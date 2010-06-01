36026808efe1090327e463d0bf374957c79a4b20
Gabi Boros <gboros@servoy.com>
2010-06-01 13:20:48 +0000

    save new column info into db when using syncWithExternalTable

--- servoy_shared/src/com/servoy/j2db/persistence/ColumnInfo.java
+++ servoy_shared/src/com/servoy/j2db/persistence/ColumnInfo.java
@@ -13,7 +13,7 @@
  You should have received a copy of the GNU Affero General Public License along
  with this program; if not, see http://www.gnu.org/licenses or write to the Free
  Software Foundation,Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301
-*/
+ */
 package com.servoy.j2db.persistence;
 
 
@@ -85,6 +85,11 @@ public class ColumnInfo implements Serializable, ISupportHTMLToolTipText
 	public boolean isStoredPersistently()
 	{
 		return storedPersistently;
+	}
+
+	public void setStoredPersistently(boolean storedPersistently)
+	{
+		this.storedPersistently = storedPersistently;
 	}
 
 	public void flagChanged()
