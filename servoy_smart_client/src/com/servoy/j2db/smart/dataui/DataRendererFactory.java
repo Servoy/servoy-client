0815ab7bc889a6b12caf819295b72a658274c1e5
Laurian Vostinar <lvostinar@servoy.com>
2010-06-03 11:15:26 +0000

    281455: Multiline portal doesnt work in smart client (merge from 5.1) Case: 281455

--- servoy_smart_client/src/com/servoy/j2db/smart/dataui/DataRendererFactory.java
+++ servoy_smart_client/src/com/servoy/j2db/smart/dataui/DataRendererFactory.java
@@ -13,7 +13,7 @@
  You should have received a copy of the GNU Affero General Public License along
  with this program; if not, see http://www.gnu.org/licenses or write to the Free
  Software Foundation,Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301
-*/
+ */
 package com.servoy.j2db.smart.dataui;
 
 
@@ -159,6 +159,7 @@ public class DataRendererFactory implements IDataRendererFactory<Component>
 			if (w > biggest_width) biggest_width = w;
 
 			if (hasRowBGColorCalc && element instanceof JComponent) ((JComponent)element).setOpaque(false);
+			element.setLocation(element.getX(), 0);
 		}
 		dr.setSize(new Dimension(biggest_width, height));
 		dr.setPreferredSize(new Dimension(biggest_width, height));
