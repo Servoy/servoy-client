fad5866df2f1ba756bfdaa45793c07e5ba120136
Jan Blok <jblok@servoy.com>
2010-06-22 21:38:21 +0000

    new version b4

--- servoy_shared/src/com/servoy/j2db/ClientVersion.java
+++ servoy_shared/src/com/servoy/j2db/ClientVersion.java
@@ -22,8 +22,8 @@ public abstract class ClientVersion
 	private static final int majorVersion = 5;
 	private static final int middleVersion = 2;
 	private static final int minorVersion = 0;
-	private static final int releaseNumber = 992;
-	private static final String versionPostfix = "b3"; //$NON-NLS-1$
+	private static final int releaseNumber = 993;
+	private static final String versionPostfix = "b4"; //$NON-NLS-1$
 	private static final String version = majorVersion + "." + middleVersion + "." + minorVersion + (versionPostfix != null ? " " + versionPostfix : ""); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ 
 
 	public static int getReleaseNumber()
