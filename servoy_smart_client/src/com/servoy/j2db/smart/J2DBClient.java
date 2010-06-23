9e03b54814868c622d416f5b6b86a5429995a18f
Rob Gansevles <rgansevles@servoy.com>
2010-06-23 16:39:43 +0000

    show solution dialog after close solution in smart client (as was with 5.1)

--- servoy_smart_client/src/com/servoy/j2db/smart/J2DBClient.java
+++ servoy_smart_client/src/com/servoy/j2db/smart/J2DBClient.java
@@ -68,11 +68,11 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Properties;
 import java.util.ResourceBundle;
 import java.util.Stack;
 import java.util.TimeZone;
-import java.util.Map.Entry;
 import java.util.concurrent.ScheduledExecutorService;
 
 import javax.security.auth.Subject;
@@ -96,6 +96,8 @@ import javax.swing.InputMap;
 import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.JDialog;
+import javax.swing.JFormattedTextField.AbstractFormatter;
+import javax.swing.JFormattedTextField.AbstractFormatterFactory;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JLayeredPane;
@@ -116,10 +118,8 @@ import javax.swing.SwingUtilities;
 import javax.swing.ToolTipManager;
 import javax.swing.UIDefaults;
 import javax.swing.UIManager;
-import javax.swing.WindowConstants;
-import javax.swing.JFormattedTextField.AbstractFormatter;
-import javax.swing.JFormattedTextField.AbstractFormatterFactory;
 import javax.swing.UIManager.LookAndFeelInfo;
+import javax.swing.WindowConstants;
 import javax.swing.border.BevelBorder;
 import javax.swing.border.Border;
 import javax.swing.plaf.FontUIResource;
@@ -1957,6 +1957,17 @@ public class J2DBClient extends ClientState implements ISmartClientApplication, 
 					transactionLabel.setIcon(empty);
 					lockLabel.setIcon(empty);
 					dataChangeLabel.setIcon(empty);
+					invokeLater(new Runnable()
+					{
+						public void run()
+						{
+							if (getSolution() == null)
+							{
+								// select new solution, run later because logout may trigger a login dialog as well
+								handleClientUserUidChanged(null, getClientInfo().getUserUid());
+							}
+						}
+					});
 				}
 			});
 
