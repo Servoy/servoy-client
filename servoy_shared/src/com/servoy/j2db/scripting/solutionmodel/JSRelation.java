39d10b00ce53548e805ff9d3da1a88540db5c078
Laurian Vostinar <lvostinar@servoy.com>
2010-06-28 13:11:59 +0000

    documentation fixes (merge from 5.2)

--- servoy_shared/src/com/servoy/j2db/scripting/solutionmodel/JSRelation.java
+++ servoy_shared/src/com/servoy/j2db/scripting/solutionmodel/JSRelation.java
@@ -13,7 +13,7 @@
  You should have received a copy of the GNU Affero General Public License along
  with this program; if not, see http://www.gnu.org/licenses or write to the Free
  Software Foundation,Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301
-*/
+ */
 package com.servoy.j2db.scripting.solutionmodel;
 
 import java.net.URI;
@@ -53,7 +53,7 @@ public class JSRelation implements IJSParent, IConstantsObject
 		{
 			public Class< ? >[] getAllReturnedTypes()
 			{
-				return new Class< ? >[] { JSRelationItem.class };
+				return null;
 			}
 		});
 	}
