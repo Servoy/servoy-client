1fa6c2fa84fac7803d81d6f79dd07a07b0f3fae2
Laurian Vostinar <lvostinar@servoy.com>
2010-07-06 14:43:17 +0000

    302818: Writing javadoc in 5.2+ (merge from 5.2) Case: 302818

--- servoy_shared/src/com/servoy/j2db/ui/IScriptScriptButtonMethods.java
+++ servoy_shared/src/com/servoy/j2db/ui/IScriptScriptButtonMethods.java
@@ -13,11 +13,16 @@
  You should have received a copy of the GNU Affero General Public License along
  with this program; if not, see http://www.gnu.org/licenses or write to the Free
  Software Foundation,Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301
-*/
+ */
 package com.servoy.j2db.ui;
 
 import com.servoy.j2db.documentation.ServoyDocumented;
 
+/***
+ * Script interface for script button
+ * 
+ *  @author lvostinar
+ */
 @ServoyDocumented(category = ServoyDocumented.RUNTIME, publicName = "Button")
 public interface IScriptScriptButtonMethods extends IScriptButtonMethods, IScriptTextLabelMethods
 {
