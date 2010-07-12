b341cd7ee64048a269631166636477d9b2ab36a5
Andrei Costache <acostache@servoy.com>
2010-07-12 08:53:45 +0000

    Making sure same font face value is returned regardless of machine locale settings. Case: 250726

--- servoy_shared/src/com/servoy/j2db/util/PersistHelper.java
+++ servoy_shared/src/com/servoy/j2db/util/PersistHelper.java
@@ -571,7 +571,7 @@ public class PersistHelper
 	{
 		if (f == null) return null;
 		StringBuffer sb = new StringBuffer();
-		sb.append(f.getFontName());
+		sb.append(f.getFontName(Locale.US));
 		sb.append(","); //$NON-NLS-1$
 		sb.append(f.getStyle());
 		sb.append(","); //$NON-NLS-1$
