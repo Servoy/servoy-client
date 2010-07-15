93bc9e3c3ec735b37f6734afd36739ceab43f68a
Laurian Vostinar <lvostinar@servoy.com>
2010-07-15 12:26:40 +0000

    document interface

--- servoy_shared/src/com/servoy/j2db/dataprocessing/IDisplayDependencyData.java
+++ servoy_shared/src/com/servoy/j2db/dataprocessing/IDisplayDependencyData.java
@@ -13,9 +13,14 @@
  You should have received a copy of the GNU Affero General Public License along
  with this program; if not, see http://www.gnu.org/licenses or write to the Free
  Software Foundation,Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301
-*/
+ */
 package com.servoy.j2db.dataprocessing;
 
+/***
+ * Interface that can notify about a possible display change even if the record object is the same. 
+ * 
+ *  @author lvostinar
+ */
 public interface IDisplayDependencyData
 {
 	public void dependencyChanged(IRecordInternal record);
