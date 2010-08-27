59cf4711bfbf1f023d7f64f4c428830a28ae1263
Gabriel Ersze <gerzse@servoy.com>
2010-08-27 11:55:03 +0000

    Added dummy class to model the Function type from JavaScript (under the JSLib node).

---
+++ servoy_shared/src/com/servoy/j2db/documentation/scripting/docs/Function.java
@@ -0,0 +1,100 @@
+/*
+ This file belongs to the Servoy development and deployment environment, Copyright (C) 1997-2010 Servoy BV
+
+ This program is free software; you can redistribute it and/or modify it under
+ the terms of the GNU Affero General Public License as published by the Free
+ Software Foundation; either version 3 of the License, or (at your option) any
+ later version.
+
+ This program is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+
+ You should have received a copy of the GNU Affero General Public License along
+ with this program; if not, see http://www.gnu.org/licenses or write to the Free
+ Software Foundation,Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301
+ */
+
+package com.servoy.j2db.documentation.scripting.docs;
+
+import com.servoy.j2db.documentation.ServoyDocumented;
+
+/**
+ * Dummy class for listing methods for JavaScript types in a manner that
+ * suits our documentation generator.
+ * 
+ * @author gerzse
+ */
+@ServoyDocumented(category = ServoyDocumented.JSLIB, publicName = "Function")
+public class Function
+{
+	/**
+	 * Specifies the function that creates an object's prototype.
+	 * 
+	 * @link https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Function/constructor
+	 */
+	public Function js_getConstructor()
+	{
+		return null;
+	}
+
+	public void js_setConstructor(Function constructor)
+	{
+
+	}
+
+	/**
+	 * Specifies the number of arguments expected by the function.
+	 * 
+	 * @link https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Function/length
+	 */
+	public Number js_getLength()
+	{
+		return null;
+	}
+
+	public void js_setLength(Number len)
+	{
+
+	}
+
+	/**
+	 * Applies the method of another object in the context of a different object (the calling object); arguments can be passed as an Array object.
+	 * 
+	 * @link https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Function/apply
+	 */
+	public void js_apply()
+	{
+
+	}
+
+	/**
+	 * Creates a new function which, when called, itself calls this function in the context of the provided value, with a given sequence of arguments preceding any provided when the new function was called.
+	 * 
+	 * @link https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Function/bind
+	 */
+	public void js_bind()
+	{
+
+	}
+
+	/**
+	 * Calls (executes) a method of another object in the context of a different object (the calling object); arguments can be passed as they are.
+	 * 
+	 * @link https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Function/call
+	 */
+	public void js_call()
+	{
+
+	}
+
+	/**
+	 * Returns a string representing the source code of the function. Overrides the Object.toString method.
+	 * 
+	 *  @link https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Function/toString
+	 */
+	public String js_toString()
+	{
+		return null;
+	}
+}
