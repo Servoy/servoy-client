31f1e84a9a179794f34c0720e0ec88016361c148
Johan Compagner <jcompagner@servoy.com>
2010-09-07 12:35:30 +0000

    nicer toString, cached hashcode

--- servoy_shared/src/com/servoy/j2db/persistence/RelationList.java
+++ servoy_shared/src/com/servoy/j2db/persistence/RelationList.java
@@ -33,6 +33,8 @@ public final class RelationList
 
 	private final short size;
 
+	private int hashcode = -1;
+
 	/**
 	 * 
 	 */
@@ -123,7 +125,15 @@ public final class RelationList
 	@Override
 	public int hashCode()
 	{
-		return relation.hashCode() + (parent == null ? 0 : parent.hashCode());
+		if (hashcode == -1)
+		{
+			hashcode = relation.hashCode();
+			if (parent != null)
+			{
+				hashcode = 31 * hashcode + parent.hashCode();
+			}
+		}
+		return hashcode;
 	}
 
 	/*
@@ -164,5 +174,168 @@ public final class RelationList
 			parentWalker = parentWalker.parent;
 		}
 		return null;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString()
+	{
+		PrependingStringBuffer sb = new PrependingStringBuffer(size * 30);
+		sb.prepend(relation.toString());
+		RelationList parentWalker = parent;
+		while (parentWalker != null)
+		{
+			sb.prepend('.');
+			sb.prepend(parentWalker.relation.toString());
+			parentWalker = parentWalker.parent;
+		}
+
+		return sb.toString();
+	}
+
+
+	public static class PrependingStringBuffer
+	{
+		private int size;
+		private int position;
+
+		private char[] buffer;
+
+		/**
+		 * Default constructor, the internal initial buffer size will be 16
+		 */
+		public PrependingStringBuffer()
+		{
+			this(16);
+		}
+
+		/**
+		 * Constructs this PrependingStringBuffer with the given buffer size.
+		 * 
+		 * @param size
+		 *            The initial size of the buffer.
+		 */
+		public PrependingStringBuffer(int size)
+		{
+			buffer = new char[size];
+			position = size;
+			this.size = 0;
+		}
+
+		/**
+		 * Constructs and direct inserts the given string. The buffer size will be string.length+16
+		 * 
+		 * @param start
+		 *            The string that is directly inserted.
+		 */
+		public PrependingStringBuffer(String start)
+		{
+			this(start.length() + 16);
+			prepend(start);
+		}
+
+		/**
+		 * Prepends one char to this PrependingStringBuffer
+		 * 
+		 * @param ch
+		 *            The char that will be prepended
+		 * @return this
+		 */
+		public PrependingStringBuffer prepend(char ch)
+		{
+			int len = 1;
+			if (position < len)
+			{
+				expandCapacity(size + len);
+			}
+			position -= len;
+			buffer[position] = ch;
+			size += len;
+			return this;
+		}
+
+		/**
+		 * Prepends the string to this PrependingStringBuffer
+		 * 
+		 * @param str
+		 *            The string that will be prepended
+		 * @return this
+		 */
+		public PrependingStringBuffer prepend(String str)
+		{
+			int len = str.length();
+			if (position < len)
+			{
+				expandCapacity(size + len);
+			}
+			str.getChars(0, len, buffer, position - len);
+			position -= len;
+			size += len;
+			return this;
+		}
+
+		private void expandCapacity(int minimumCapacity)
+		{
+			int newCapacity = (buffer.length + 1) * 2;
+			if (newCapacity < 0)
+			{
+				newCapacity = Integer.MAX_VALUE;
+			}
+			else if (minimumCapacity > newCapacity)
+			{
+				newCapacity = minimumCapacity;
+			}
+
+			char newValue[] = new char[newCapacity];
+			System.arraycopy(buffer, position, newValue, newCapacity - size, size);
+			buffer = newValue;
+			position = newCapacity - size;
+		}
+
+		/**
+		 * Returns the size of this PrependingStringBuffer
+		 * 
+		 * @return The size
+		 */
+		public int length()
+		{
+			return size;
+		}
+
+		/**
+		 * @see java.lang.Object#toString()
+		 */
+		@Override
+		public String toString()
+		{
+			return new String(buffer, position, size);
+		}
+
+		@Override
+		public boolean equals(Object obj)
+		{
+			if (obj == this)
+			{
+				return true;
+			}
+			else if (obj == null)
+			{
+				return false;
+			}
+			else
+			{
+				return toString().equals(obj.toString());
+			}
+		}
+
+		@Override
+		public int hashCode()
+		{
+			return toString().hashCode();
+		}
 	}
 }
