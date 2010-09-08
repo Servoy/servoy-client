d2e71173f62c58d210f0a9906affd8ba9310f3db
Jan Blok <jblok@servoy.com>
2010-09-08 09:11:59 +0000

    doc addition

--- servoy_shared/src/com/servoy/j2db/FormController.java
+++ servoy_shared/src/com/servoy/j2db/FormController.java
@@ -76,11 +76,11 @@ import com.servoy.j2db.persistence.IRepository;
 import com.servoy.j2db.persistence.ISupportScriptProviders;
 import com.servoy.j2db.persistence.ITable;
 import com.servoy.j2db.persistence.MethodArgument;
-import com.servoy.j2db.persistence.MethodArgument.ArgumentType;
 import com.servoy.j2db.persistence.MethodTemplate;
 import com.servoy.j2db.persistence.Part;
 import com.servoy.j2db.persistence.RepositoryException;
 import com.servoy.j2db.persistence.ScriptVariable;
+import com.servoy.j2db.persistence.MethodArgument.ArgumentType;
 import com.servoy.j2db.scripting.CreationalPrototype;
 import com.servoy.j2db.scripting.ElementScope;
 import com.servoy.j2db.scripting.FormScope;
@@ -88,11 +88,11 @@ import com.servoy.j2db.scripting.GlobalScope;
 import com.servoy.j2db.scripting.IExecutingEnviroment;
 import com.servoy.j2db.scripting.IScriptSupport;
 import com.servoy.j2db.scripting.InstanceJavaMembers;
-import com.servoy.j2db.scripting.JSApplication.FormAndComponent;
 import com.servoy.j2db.scripting.JSEvent;
 import com.servoy.j2db.scripting.ScriptEngine;
 import com.servoy.j2db.scripting.SelectedRecordScope;
 import com.servoy.j2db.scripting.SolutionScope;
+import com.servoy.j2db.scripting.JSApplication.FormAndComponent;
 import com.servoy.j2db.ui.IAccessible;
 import com.servoy.j2db.ui.IComponent;
 import com.servoy.j2db.ui.IDataRenderer;
@@ -166,6 +166,8 @@ public class FormController implements IForm, ListSelectionListener, TableModelL
 		 * //%%prefix%%controller.show('mydialog',true);
 		 * //show the form in a named window
 		 * //%%prefix%%controller.show('mywindow',null);
+		 * //show the form in the main window
+		 * //%%prefix%%controller.show(null);
 		 *
 		 * @param dialogName/windowName optional the dialog/window name
 		 * @param modal optional boolean indicating modality for dialogs or null if it should use a window; default value is false.
