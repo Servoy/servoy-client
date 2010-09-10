dd93a7e7f9891790ca1ed52e4f2c44ef00fc99cc
Rob Gansevles <rgansevles@servoy.com>
2010-09-10 16:20:37 +0000

    Fix for not-in filter with multiple values http://forum.servoy.com/viewtopic.php?f=3&t=13764&sid=200d91f583bb45716d2c12e71711ffa4#p79106

--- servoy_shared/src/com/servoy/j2db/dataprocessing/SQLGenerator.java
+++ servoy_shared/src/com/servoy/j2db/dataprocessing/SQLGenerator.java
@@ -1414,7 +1414,8 @@ public class SQLGenerator
 				}
 			}
 			int setOperator = maskedOp == ISQLCondition.IN_OPERATOR ? ISQLCondition.EQUALS_OPERATOR : ISQLCondition.NOT_OPERATOR;
-			filterWhere = new SetCondition(setOperator | (op & ~ISQLCondition.OPERATOR_MASK), new IQuerySelectValue[] { qColumn }, inValues, true);
+			filterWhere = new SetCondition(setOperator | (op & ~ISQLCondition.OPERATOR_MASK), new IQuerySelectValue[] { qColumn }, inValues,
+				maskedOp == ISQLCondition.IN_OPERATOR);
 		}
 		else if (maskedOp == ISQLCondition.BETWEEN_OPERATOR || maskedOp == ISQLCondition.NOT_BETWEEN_OPERATOR)
 		{
