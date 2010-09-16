9003cbc7aad8991585e2550a7886def640178234
Gabi Boros <gboros@servoy.com>
2010-09-16 09:17:18 +0000

    ignore margin in web size calculation when it has border, as it is include in there Case: 319942

--- servoy_headless_client/src/com/servoy/j2db/server/headlessclient/dataui/WebBaseSubmitLink.java
+++ servoy_headless_client/src/com/servoy/j2db/server/headlessclient/dataui/WebBaseSubmitLink.java
@@ -958,7 +958,7 @@ public class WebBaseSubmitLink extends SubmitLink implements ILabel, IScriptHtml
 	public void js_setSize(int width, int height)
 	{
 		size = new Dimension(width, height);
-		jsChangeRecorder.setSize(width, height, border, margin, getFontSize(), valign);
+		jsChangeRecorder.setSize(width, height, border, border != null ? null : margin, getFontSize(), valign); // border already have the margin
 	}
 
 	protected int getFontSize()
@@ -975,7 +975,7 @@ public class WebBaseSubmitLink extends SubmitLink implements ILabel, IScriptHtml
 
 	public Rectangle getWebBounds()
 	{
-		Dimension d = jsChangeRecorder.calculateWebSize(size.width, size.height, border, margin, getFontSize(), null, valign);
+		Dimension d = jsChangeRecorder.calculateWebSize(size.width, size.height, border, border != null ? null : margin, getFontSize(), null, valign); // border already have the margin
 		return new Rectangle(location, d);
 	}
 
@@ -984,7 +984,7 @@ public class WebBaseSubmitLink extends SubmitLink implements ILabel, IScriptHtml
 	 */
 	public Insets getPaddingAndBorder()
 	{
-		return jsChangeRecorder.getPaddingAndBorder(size.height, border, margin, getFontSize(), null, valign);
+		return jsChangeRecorder.getPaddingAndBorder(size.height, border, border != null ? null : margin, getFontSize(), null, valign); // border already have the margin
 	}
 
 
