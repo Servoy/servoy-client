6ab43011f5e545906ea8befb2699605c1ba1a2a5
Jan Blok <jblok@servoy.com>
2010-10-27 08:48:52 +0000

    default when serviceprovider is not yet initialized

--- servoy_headless_client/src/com/servoy/j2db/server/headlessclient/dataui/FormLayoutProviderFactory.java
+++ servoy_headless_client/src/com/servoy/j2db/server/headlessclient/dataui/FormLayoutProviderFactory.java
@@ -31,7 +31,7 @@ public class FormLayoutProviderFactory
 {
 	public static IFormLayoutProvider getFormLayoutProvider(IServiceProvider sp, Solution solution, Form f, String formInstanceName)
 	{
-		if (Utils.getAsBoolean(sp.getRuntimeProperties().get("enableAnchors")))
+		if (sp == null || Utils.getAsBoolean(sp.getRuntimeProperties().get("enableAnchors")))
 		{
 			return new AnchoredFormLayoutProvider(sp, solution, f, formInstanceName);
 		}
