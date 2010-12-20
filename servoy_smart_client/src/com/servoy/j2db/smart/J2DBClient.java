284800e79e0c3647812b686f4cc3a55cf2e91ed8
Rob Gansevles <rgansevles@servoy.com>
2010-12-20 15:02:13 +0000

    login or select-solution dialog were shown when SC could not register (case 338025: enhanced security = false with maintance mode) Case: 338025

--- servoy_smart_client/src/com/servoy/j2db/smart/J2DBClient.java
+++ servoy_smart_client/src/com/servoy/j2db/smart/J2DBClient.java
@@ -3347,7 +3347,7 @@ public class J2DBClient extends ClientState implements ISmartClientApplication, 
 		}
 		catch (final ApplicationException e)
 		{
-			invokeLater(new Runnable()
+			invokeAndWait(new Runnable()
 			{
 				public void run()
 				{
