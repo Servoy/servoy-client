c0c5faa43e535f543501c860803434d5d38c4a19
Laurian Vostinar <lvostinar@servoy.com>
2011-01-18 12:30:40 +0000

    285370: Displaying a form in dialog on a second monitor Case: 285370

--- servoy_shared/src/com/servoy/j2db/util/UIUtils.java
+++ servoy_shared/src/com/servoy/j2db/util/UIUtils.java
@@ -13,7 +13,7 @@
  You should have received a copy of the GNU Affero General Public License along
  with this program; if not, see http://www.gnu.org/licenses or write to the Free
  Software Foundation,Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301
-*/
+ */
 package com.servoy.j2db.util;
 
 import java.awt.GraphicsConfiguration;
@@ -80,7 +80,7 @@ public class UIUtils
 				if (gc != null)
 				{
 					Rectangle gcBounds = gc.getBounds();
-					if (gcBounds.contains(r))
+					if (gcBounds.contains(r.x, r.y))
 					{
 						return true;
 					}
