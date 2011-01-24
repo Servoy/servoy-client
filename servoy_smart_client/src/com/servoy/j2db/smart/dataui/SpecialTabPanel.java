bd574baf72124ee6c56c773f72947d7296af1974
Jan Blok <jblok@servoy.com>
2011-01-24 10:05:35 +0000

    prevent delegation incase of jpanel

--- servoy_smart_client/src/com/servoy/j2db/smart/dataui/SpecialTabPanel.java
+++ servoy_smart_client/src/com/servoy/j2db/smart/dataui/SpecialTabPanel.java
@@ -29,6 +29,7 @@ import java.util.List;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JComponent;
+import javax.swing.JPanel;
 import javax.swing.SwingConstants;
 import javax.swing.ToolTipManager;
 import javax.swing.event.ChangeEvent;
@@ -807,7 +808,10 @@ public class SpecialTabPanel extends EnablePanel implements IDisplayRelatedData,
 	public void js_putClientProperty(Object key, Object value)
 	{
 		putClientProperty(key, value);
-		enclosingComponent.putClientProperty(key, value);
+		if (!(enclosingComponent instanceof JPanel))
+		{
+			enclosingComponent.putClientProperty(key, value);
+		}
 	}
 
 	public Object js_getClientProperty(Object key)
