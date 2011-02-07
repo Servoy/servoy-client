6c5230d3037535f420ce49f53e52f1b40d499560
Andras Lorinz <alorincz@servoy.com>
2011-02-07 13:13:31 +0000

    The call ((FormManager)application.getFormManager()).fillScriptMenu(); can be replaced by fm.fillScriptMenu();

--- servoy_shared/src/com/servoy/j2db/FormDialog.java
+++ servoy_shared/src/com/servoy/j2db/FormDialog.java
@@ -247,7 +247,7 @@ public class FormDialog extends JEscapeDialog implements FormWindow
 					((FormManager)application.getFormManager()).setCurrentContainer(null, null);
 				}
 			}
-			((FormManager)application.getFormManager()).fillScriptMenu();
+			fm.fillScriptMenu();
 		}
 	}
 
