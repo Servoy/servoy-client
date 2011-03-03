30aac3a23867cee36bb9540dfa1cf1688812a6a1
Jan Blok <jblok@servoy.com>
2011-03-03 21:36:58 +0000

    new build files for new DLTK

--- servoy_shared/src/com/servoy/j2db/ClientVersion.java
+++ servoy_shared/src/com/servoy/j2db/ClientVersion.java
@@ -28,8 +28,8 @@ public abstract class ClientVersion
 	private static final int majorVersion = 6;
 	private static final int middleVersion = 0;
 	private static final int minorVersion = 0;
-	private static final int releaseNumber = 1204;
-	private static final String versionPostfix = "a4";
+	private static final int releaseNumber = 1205;
+	private static final String versionPostfix = "a5";
 	private static final String version = majorVersion + "." + middleVersion + "." + minorVersion + (versionPostfix != null ? " " + versionPostfix : "");
 
 	public static int getReleaseNumber()
