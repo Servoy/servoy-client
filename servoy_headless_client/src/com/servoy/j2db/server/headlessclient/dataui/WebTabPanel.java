e0563598ffa62419ffbb7bd5fef739cc0e454d41
Gabi Boros <gboros@servoy.com>
2011-04-20 10:17:14 +0000

    fix : form reuse on tabpanel breaks the webclient
    Case: 377555

--- servoy_headless_client/src/com/servoy/j2db/server/headlessclient/dataui/WebTabPanel.java
+++ servoy_headless_client/src/com/servoy/j2db/server/headlessclient/dataui/WebTabPanel.java
@@ -413,6 +413,8 @@ public class WebTabPanel extends WebMarkupContainer implements ITabPanel, IDispl
 	 */
 	private void setCurrentForm(WebTabFormLookup fl, int previousIndex, List<Runnable> invokeLaterRunnables)
 	{
+		if (fl != null && !fl.isFormReady()) return;
+
 		jsChangeRecorder.setChanged();
 		currentForm = fl;
 		if (parentData != null)
@@ -450,6 +452,11 @@ public class WebTabPanel extends WebMarkupContainer implements ITabPanel, IDispl
 		}
 	}
 
+	public WebForm getCurrentForm()
+	{
+		return currentForm != null ? currentForm.getWebForm() : null;
+	}
+
 	/**
 	 * @see org.apache.wicket.MarkupContainer#remove(org.apache.wicket.Component)
 	 */
@@ -459,8 +466,9 @@ public class WebTabPanel extends WebMarkupContainer implements ITabPanel, IDispl
 		if (currentForm != null && component == currentForm.getWebForm())
 		{
 			currentForm.setWebForm(null);
+			//replace(new Label("webform", new Model<String>("")));
 		}
-		super.remove(component);
+		else super.remove(component);
 	}
 
 	private void recomputeTabSequence()
