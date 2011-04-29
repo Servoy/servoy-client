ff6114b757d6735494688cf4c6a5a6d94cd79a6c
Gabi Boros <gboros@servoy.com>
2011-04-29 14:01:31 +0000

    disable tooltips on mobile browsers Case: 380254

--- servoy_headless_client/src/com/servoy/j2db/server/headlessclient/WebClient.java
+++ servoy_headless_client/src/com/servoy/j2db/server/headlessclient/WebClient.java
@@ -873,8 +873,28 @@ public class WebClient extends SessionClient implements IWebClientApplication
 		}
 	}
 
-	public void onEndRequest(@SuppressWarnings("unused")
-	WebClientSession webClientSession)
+	public void onEndRequest(@SuppressWarnings("unused") WebClientSession webClientSession)
 	{
+	}
+
+	@SuppressWarnings("nls")
+	public static boolean isMobile()
+	{
+		boolean isMobile = false;
+		if (Session.exists())
+		{
+			org.apache.wicket.request.ClientInfo info = Session.get().getClientInfo();
+			if (info instanceof WebClientInfo)
+			{
+				String userAgent = ((WebClientInfo)info).getProperties().getNavigatorUserAgent();
+				if (userAgent != null)
+				{
+					userAgent = userAgent.toLowerCase();
+					isMobile = userAgent.contains("android") || userAgent.contains("iphone") || userAgent.contains("ipad");
+				}
+			}
+		}
+
+		return isMobile;
 	}
 }
