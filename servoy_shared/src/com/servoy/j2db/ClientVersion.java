1b332d5ceca0aa401d92407d38b0286ea8527986
Andrei Costache <acostache@servoy.com>
2011-05-25 06:38:34 +0000

    New version

--- servoy_shared/src/com/servoy/j2db/ClientVersion.java
+++ servoy_shared/src/com/servoy/j2db/ClientVersion.java
@@ -28,8 +28,8 @@ public abstract class ClientVersion
 	private static final int majorVersion = 6;
 	private static final int middleVersion = 0;
 	private static final int minorVersion = 0;
-	private static final int releaseNumber = 1210;
-	private static final String versionPostfix = "b3";
+	private static final int releaseNumber = 1211;
+	private static final String versionPostfix = "b4";
 	private static final String version = majorVersion + "." + middleVersion + "." + minorVersion + (versionPostfix != null ? " " + versionPostfix : "");
 
 	public static int getReleaseNumber()
