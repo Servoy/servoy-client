95045d0459abd9bc6f9ceb2f7c2c6796f2100be4
Andrei Costache <acostache@servoy.com>
2011-06-21 12:15:49 +0000

    Setting the first created form as the first form for a Solution. Case: 330140

--- servoy_shared/src/com/servoy/j2db/persistence/Solution.java
+++ servoy_shared/src/com/servoy/j2db/persistence/Solution.java
@@ -139,6 +139,7 @@ public class Solution extends AbstractRootObject implements ISupportChilds, ISup
 		f.setSize(size);
 		if (style != null) f.setStyleName(style.getName());
 		addChild(f);
+		if (getForm(getFirstFormID()) == null) setFirstFormID(f.getID());
 		return f;
 	}
 
