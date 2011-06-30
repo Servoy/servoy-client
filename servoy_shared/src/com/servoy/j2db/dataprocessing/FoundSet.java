23507b754077a81f361ba2617bc180bd693798f4
Rob Gansevles <rgansevles@servoy.com>
2011-06-30 07:43:01 +0000

    add brackets around custom conditions (case 397384: 20110629 Foundsetquery corrupt) Case: 397384

--- servoy_shared/src/com/servoy/j2db/dataprocessing/FoundSet.java
+++ servoy_shared/src/com/servoy/j2db/dataprocessing/FoundSet.java
@@ -1454,7 +1454,7 @@ public abstract class FoundSet implements IFoundSetInternal, IRowListener, Scrip
 			else
 			{
 				tables = query.substring(from_index + 4, where_index);
-				sqlSelect.setCondition(SQLGenerator.CONDITION_SEARCH, new CustomCondition(query.substring(where_index + 5, order_by_index), whereArgs));
+				sqlSelect.setCondition(SQLGenerator.CONDITION_SEARCH, new CustomCondition(query.substring(where_index + 5, order_by_index).trim(), whereArgs));
 			}
 
 			// pick the foundset main table from the tables in the query (does not have to be the first one, we generate sql ourselves
