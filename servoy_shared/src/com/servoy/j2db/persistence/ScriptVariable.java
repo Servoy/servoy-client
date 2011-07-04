79d42414fbbbe7ecaca61309371b2fe97b0ba599
Johan Compagner <jcompagner@servoy.com>
2011-07-04 09:10:13 +0000

    Comment added to variable is discarded when another variable is deleted
    also copy over the comment of the script variable to the dest from the src. Flag the ScriptVariable as changed if the comment is changed. (so that it is getting kopied)
    (only 5.x, in 6 this is already a real property)
    Case: 396908

--- servoy_shared/src/com/servoy/j2db/persistence/ScriptVariable.java
+++ servoy_shared/src/com/servoy/j2db/persistence/ScriptVariable.java
@@ -13,7 +13,7 @@
  You should have received a copy of the GNU Affero General Public License along
  with this program; if not, see http://www.gnu.org/licenses or write to the Free
  Software Foundation,Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301
-*/
+ */
 package com.servoy.j2db.persistence;
 
 import java.util.ArrayList;
@@ -117,6 +117,7 @@ public class ScriptVariable extends AbstractBase implements IVariable, IDataProv
 
 	public void setComment(String arg)
 	{
+		checkForChange(comment, arg);
 		this.comment = arg;
 	}
 
