c11e6cdd224b9f39a9d96659d516cfcea6b4174f
Johan Compagner <jcompagner@servoy.com>
2011-10-06 10:56:57 +0000

    don't synchronize on the session, wicket also does that on certain places

--- servoy_debug/src/com/servoy/j2db/debug/DebugWebClient.java
+++ servoy_debug/src/com/servoy/j2db/debug/DebugWebClient.java
@@ -372,7 +372,7 @@ public class DebugWebClient extends WebClient implements IDebugWebClient
 				MainPage page = (MainPage)((WebFormManager)getFormManager()).getMainContainer(null);
 				throw new RestartResponseException(page);
 			}
-			synchronized (webClientSession)
+			synchronized (webClientSession.getWebClient())
 			{
 				executeEvents();
 			}
