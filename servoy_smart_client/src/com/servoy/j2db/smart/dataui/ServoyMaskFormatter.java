fad63080c6ecc8a54f0408c1243b53ebe4c2167d
Johan Compagner <jcompagner@servoy.com>
2012-01-30 15:20:47 +0000

    fixed the loophole found in TEXT_FIELD with a value list do not accept other values.
    So that when using a valuelist with real/displays based on text column and you attach a mask/format to it it will still check the valuelist
    Case: SVY-98

--- servoy_smart_client/src/com/servoy/j2db/smart/dataui/ServoyMaskFormatter.java
+++ servoy_smart_client/src/com/servoy/j2db/smart/dataui/ServoyMaskFormatter.java
@@ -13,7 +13,7 @@
  You should have received a copy of the GNU Affero General Public License along
  with this program; if not, see http://www.gnu.org/licenses or write to the Free
  Software Foundation,Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301
-*/
+ */
 package com.servoy.j2db.smart.dataui;
 
 import java.text.ParseException;
@@ -25,7 +25,7 @@ import com.servoy.j2db.util.gui.FixedMaskFormatter;
  * @author jcompagner
  *
  */
-public final class ServoyMaskFormatter extends FixedMaskFormatter
+public class ServoyMaskFormatter extends FixedMaskFormatter
 {
 	private final boolean displayFormatter;
 
