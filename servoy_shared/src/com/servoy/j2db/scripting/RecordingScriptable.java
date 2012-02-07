995e8ba38f06d5d93ec5b1063bf2855e35a10cb4
Rob Gansevles <rgansevles@servoy.com>
2012-02-07 09:03:16 +0000

    databaseManager.hasRecords() is the same as utils.hasRecords()

--- servoy_shared/src/com/servoy/j2db/scripting/RecordingScriptable.java
+++ servoy_shared/src/com/servoy/j2db/scripting/RecordingScriptable.java
@@ -138,7 +138,7 @@ public class RecordingScriptable implements Scriptable, IDelegate<Scriptable>, W
 		{
 			if (o instanceof Function)
 			{
-				if ("databaseManager".equals(scriptableName) && "hasRecords".equals(name)) //$NON-NLS-1$ //$NON-NLS-2$
+				if (("databaseManager".equals(scriptableName) || "utils".equals(scriptableName)) && "hasRecords".equals(name)) //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
 				{
 					// special case, databaseManager.hasRecords(record, relationName) checks existence of related foundsets 
 					return new RecordingFunction(name, (Function)o)
