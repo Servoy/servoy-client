90ab8009e12dd3902569f1c21e022cfe7013bb43
Johan Compagner <jcompagner@servoy.com>
2012-02-29 14:52:20 +0000

    up the version to i1

--- servoy_shared/src/com/servoy/j2db/ClientVersion.java
+++ servoy_shared/src/com/servoy/j2db/ClientVersion.java
@@ -28,7 +28,7 @@ public abstract class ClientVersion
 	private static final int middleVersion = 2;
 	private static final int minorVersion = 12;
 	private static final int releaseNumber = 1024;
-	private static final String versionPostfix = ""; //$NON-NLS-1$
+	private static final String versionPostfix = "i1"; //$NON-NLS-1$
 	private static final String version = majorVersion + "." + middleVersion + "." + minorVersion + (versionPostfix != null ? " " + versionPostfix : ""); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ 
 
 	public static int getReleaseNumber()
