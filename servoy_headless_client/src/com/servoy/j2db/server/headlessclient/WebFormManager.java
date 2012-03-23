0183b9fbd7f6cb42d13e50f58d9403c96720365e
Johan Compagner <jcompagner@servoy.com>
2012-03-23 10:43:37 +0000

    added 1 step more to the max loaded (192 when there is more then 300MB on memory) introduced a webclient setting so that the webclient has its own max loaded, default to 128

--- servoy_headless_client/src/com/servoy/j2db/server/headlessclient/WebFormManager.java
+++ servoy_headless_client/src/com/servoy/j2db/server/headlessclient/WebFormManager.java
@@ -39,6 +39,7 @@ import com.servoy.j2db.persistence.Form;
 import com.servoy.j2db.scripting.JSWindow;
 import com.servoy.j2db.server.headlessclient.dataui.IWebFormContainer;
 import com.servoy.j2db.smart.SwingForm;
+import com.servoy.j2db.util.Settings;
 import com.servoy.j2db.util.Utils;
 
 /**
@@ -48,9 +49,25 @@ import com.servoy.j2db.util.Utils;
  */
 public class WebFormManager extends FormManager
 {
+	private final int maxForms;
+
+	@SuppressWarnings("nls")
 	public WebFormManager(IApplication app, IMainContainer mainp)
 	{
 		super(app, mainp);
+		int max = Utils.getAsInteger(Settings.getInstance().getProperty("servoy.max.webforms.loaded", "128"), false);
+		maxForms = max == 0 ? 128 : max;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.servoy.j2db.FormManager#getMaxFormsLoaded()
+	 */
+	@Override
+	protected int getMaxFormsLoaded()
+	{
+		return maxForms;
 	}
 
 	@Override
