65e29a96f93d738db54f1406b7adff4736afb12f
Johan Compagner <jcompagner@servoy.com>
2012-04-16 15:59:41 +0000

    Exception in thread "AWT-EventQueue-0" java.lang.StackOverflowError in relation
    reverted the index-1 selected row.
    
    fixed it in the foundset by firing the right start index
    Case: SVY-1810

--- servoy_shared/src/com/servoy/j2db/dataprocessing/FoundSet.java
+++ servoy_shared/src/com/servoy/j2db/dataprocessing/FoundSet.java
@@ -1602,7 +1602,7 @@ public abstract class FoundSet implements IFoundSetInternal, IRowListener, Scrip
 				startRow = originalPKRowcount;
 				lastPkHash = null;
 			}
-			int size = getCorrectedSizeForFires();
+			int size = getSize();
 			long time = System.currentTimeMillis();
 			IDataSet newpks = fsm.getDataServer().performQuery(fsm.getApplication().getClientID(), sheet.getServerName(), transaction_id, sqlSelect,
 				fsm.getTableFilterParams(sheet.getServerName(), sqlSelect), !sqlSelect.isUnique(), startRow, correctedMaxResult,
@@ -4093,8 +4093,8 @@ public abstract class FoundSet implements IFoundSetInternal, IRowListener, Scrip
 								}
 							}
 
+							size = getSize();
 							pks.addRow(pk);
-							size = getCorrectedSizeForFires();
 
 						}
 						clearAggregates();
