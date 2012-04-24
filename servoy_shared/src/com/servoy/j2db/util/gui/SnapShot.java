6d80b83cb018c8e593bcc058296c1b5698438f7f
Gabi Boros <gboros@servoy.com>
2012-04-24 08:00:04 +0000

    changed log to error-log Case: SVY-1361

--- servoy_shared/src/com/servoy/j2db/util/gui/SnapShot.java
+++ servoy_shared/src/com/servoy/j2db/util/gui/SnapShot.java
@@ -164,7 +164,7 @@ public class SnapShot
 					}
 					catch (Exception ex)
 					{
-						Debug.log("Error saving image as png", ex);
+						Debug.error("Error saving image as png", ex);
 						return null;
 					}
 				}
