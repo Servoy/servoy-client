ce11471f710290a294154b4e52a92357a741305d
Laurian Vostinar <lvostinar@servoy.com>
2012-06-28 15:03:07 +0000

    Calling global method with headless client from jsp doesn't seem to work Case: SVY-2549

--- servoy_shared/src/com/servoy/j2db/FormController.java
+++ servoy_shared/src/com/servoy/j2db/FormController.java
@@ -4433,14 +4433,14 @@ public class FormController implements IForm, ListSelectionListener, TableModelL
 			}
 		}
 
-		if (function == null)
+		if (function == null || function == Scriptable.NOT_FOUND)
 		{
 			GlobalScope globalScope = null;
 			if (id > 0)
 			{
 				globalScope = application.getScriptEngine().getScopesScope().getGlobalScopeForFunction(new Integer(id));
 			}
-			else if (nameScope != null && nameScope.getLeft() != null)
+			else if (nameScope != null)
 			{
 				globalScope = application.getScriptEngine().getScopesScope().getGlobalScope(nameScope.getLeft());
 			}
