2a2ec293da2b49d60277f272045391531d09f10a
Haniela Hardut <hhardut@servoy.com>
2012-07-02 13:39:51 +0000

    Code sample for JSDataSet.addRow has confusing/incorrect comment Case: SVY-2562

--- servoy_shared/src/com/servoy/j2db/dataprocessing/JSDataSet.java
+++ servoy_shared/src/com/servoy/j2db/dataprocessing/JSDataSet.java
@@ -288,7 +288,7 @@ public class JSDataSet implements Wrapper, IDelegate<IDataSet>, Scriptable, Seri
 	 * @sample
 	 * //assuming the variable dataset contains a dataset
 	 * dataset.addRow(new Array(1,2,3,4,5,6,7,7)); //adds a row with 8 columns
-	 * dataset.addRow(2, new Array(1,2,3,4,5,6,7,7)); //adds a row with 8 columns after row 2
+	 * dataset.addRow(2, new Array(1,2,3,4,5,6,7,7)); //adds a row with 8 columns at row 2
 	 *
 	 * @param array row data
 	 */
@@ -303,7 +303,7 @@ public class JSDataSet implements Wrapper, IDelegate<IDataSet>, Scriptable, Seri
 	 * @sample
 	 * //assuming the variable dataset contains a dataset
 	 * dataset.addRow(new Array(1,2,3,4,5,6,7,7)); //adds a row with 8 columns
-	 * dataset.addRow(2, new Array(1,2,3,4,5,6,7,7)); //adds a row with 8 columns after row 2
+	 * dataset.addRow(2, new Array(1,2,3,4,5,6,7,7)); //adds a row with 8 columns at row 2
 	 *
 	 * @param index index to add row (1-based)
 	 * @param array row data
