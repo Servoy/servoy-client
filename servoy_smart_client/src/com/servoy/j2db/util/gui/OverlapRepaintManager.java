4af32180c7c0324e72dee5818af14c68e41058e3
Johan Compagner <jcompagner@servoy.com>
2012-07-12 10:54:18 +0000

    Severe degradation in performance when editing records (in a found set) when these records appear on a form at that time within a client
    the latest java 6 releases and java 7 have a changed repaint trigger behavior in the RepaintManager, They now have there own runnable pointing to its instance instead of going through the Swing utilities that would result in the actual RepaintManager to be used.
    
    So now the paintDirtyComponents doesn't go through our class but directly to the wrapper.
    
    Changed this so that we don't wrap it anymore but really are the repaintmanager itselfs (super calls instead of delegates)
    Case: SVY-2380

--- servoy_smart_client/src/com/servoy/j2db/util/gui/OverlapRepaintManager.java
+++ servoy_smart_client/src/com/servoy/j2db/util/gui/OverlapRepaintManager.java
@@ -18,8 +18,6 @@ package com.servoy.j2db.util.gui;
 
 import java.awt.Component;
 import java.awt.Container;
-import java.awt.Dimension;
-import java.awt.Image;
 import java.awt.Rectangle;
 import java.util.HashSet;
 import java.util.Set;
@@ -37,8 +35,6 @@ import javax.swing.SwingUtilities;
  */
 public class OverlapRepaintManager extends RepaintManager
 {
-
-	private final RepaintManager delegate;
 	private final Set<JComponent> components = new HashSet<JComponent>();
 
 	/**
@@ -46,22 +42,7 @@ public class OverlapRepaintManager extends RepaintManager
 	 */
 	public OverlapRepaintManager()
 	{
-		this(new RepaintManager());
-	}
-
-	/**
-	 * Creates a new OverlapRepaintManager that only alters the behavior of the given RepaintManager in order to support
-	 * overlapping, but keeps using it for unaltered repaint operations.
-	 * @param repaintManager the delegate repaint manager who's behavior will be altered.
-	 * @throws IllegalArgumentException if the given RepaintManager is null or an OverlapRepaintManager.
-	 */
-	public OverlapRepaintManager(RepaintManager repaintManager)
-	{
-		if (repaintManager == null || repaintManager instanceof OverlapRepaintManager)
-		{
-			throw new IllegalArgumentException("Delegate repaint manager cannot be null or of the same kind.");
-		}
-		delegate = repaintManager;
+		super();
 	}
 
 	@Override
@@ -71,7 +52,7 @@ public class OverlapRepaintManager extends RepaintManager
 		{
 			components.add(c);
 		}
-		delegate.addDirtyRegion(c, x, y, w, h); // add the dirty region
+		super.addDirtyRegion(c, x, y, w, h); // add the dirty region
 	}
 
 	private void searchOverlappingRegionsInHierarchy(Container parent, JComponent c, Rectangle repaintedArea)
@@ -92,7 +73,7 @@ public class OverlapRepaintManager extends RepaintManager
 					intersection = child[i].getBounds().intersection(parentRepaintedArea);
 					if (!intersection.isEmpty())
 					{
-						delegate.addDirtyRegion((JComponent)parent, intersection.x, intersection.y, intersection.width, intersection.height);
+						super.addDirtyRegion((JComponent)parent, intersection.x, intersection.y, intersection.width, intersection.height);
 					}
 				}
 			}
@@ -100,60 +81,6 @@ public class OverlapRepaintManager extends RepaintManager
 			// see if parents overlap on some level
 			searchOverlappingRegionsInHierarchy(parent.getParent(), (JComponent)parent, parentRepaintedArea);
 		}
-	}
-
-	@Override
-	public Rectangle getDirtyRegion(JComponent component)
-	{
-		return delegate.getDirtyRegion(component);
-	}
-
-	@Override
-	public synchronized void addInvalidComponent(JComponent invalidComponent)
-	{
-		delegate.addInvalidComponent(invalidComponent);
-	}
-
-	@Override
-	public Dimension getDoubleBufferMaximumSize()
-	{
-		return delegate.getDoubleBufferMaximumSize();
-	}
-
-	@Override
-	public Image getOffscreenBuffer(Component c, int proposedWidth, int proposedHeight)
-	{
-		return delegate.getOffscreenBuffer(c, proposedWidth, proposedHeight);
-	}
-
-	@Override
-	public Image getVolatileOffscreenBuffer(Component c, int proposedWidth, int proposedHeight)
-	{
-		return delegate.getVolatileOffscreenBuffer(c, proposedWidth, proposedHeight);
-	}
-
-	@Override
-	public boolean isCompletelyDirty(JComponent component)
-	{
-		return delegate.isCompletelyDirty(component);
-	}
-
-	@Override
-	public boolean isDoubleBufferingEnabled()
-	{
-		return delegate.isDoubleBufferingEnabled();
-	}
-
-	@Override
-	public void markCompletelyClean(JComponent component)
-	{
-		delegate.markCompletelyClean(component);
-	}
-
-	@Override
-	public void markCompletelyDirty(JComponent component)
-	{
-		delegate.markCompletelyDirty(component);
 	}
 
 	@Override
@@ -167,34 +94,9 @@ public class OverlapRepaintManager extends RepaintManager
 		}
 		for (JComponent component : tmp)
 		{
-			Rectangle dirtyRegion = delegate.getDirtyRegion(component);
+			Rectangle dirtyRegion = super.getDirtyRegion(component);
 			searchOverlappingRegionsInHierarchy(component.getParent(), component, dirtyRegion);
 		}
-		delegate.paintDirtyRegions();
+		super.paintDirtyRegions();
 	}
-
-	@Override
-	public synchronized void removeInvalidComponent(JComponent component)
-	{
-		delegate.removeInvalidComponent(component);
-	}
-
-	@Override
-	public void setDoubleBufferingEnabled(boolean flag)
-	{
-		delegate.setDoubleBufferingEnabled(flag);
-	}
-
-	@Override
-	public void setDoubleBufferMaximumSize(Dimension d)
-	{
-		delegate.setDoubleBufferMaximumSize(d);
-	}
-
-	@Override
-	public void validateInvalidComponents()
-	{
-		delegate.validateInvalidComponents();
-	}
-
 }
