f2434222b75933a9f837f154e2b5919007bac7e1
Gabi Boros <gboros@servoy.com>
2012-07-17 12:12:30 +0000

    fix : Email with more than three letter top-level domain are considered invalid Case: SVY-2536

--- servoy_shared/src/com/servoy/j2db/util/Utils.java
+++ servoy_shared/src/com/servoy/j2db/util/Utils.java
@@ -2364,7 +2364,7 @@ public class Utils
 	public static boolean isValidEmailAddress(String email)
 	{
 		return (email != null
-			? (Pattern.compile("^[_a-z0-9-+]+(\\.[_a-z0-9-+]+)*@[a-z0-9-]+(\\.[a-z0-9-]+)*(\\.[a-z]{2,3})$", Pattern.CASE_INSENSITIVE).matcher(email).matches()) //$NON-NLS-1$
+			? (Pattern.compile("^[_a-z0-9-+]+(\\.[_a-z0-9-+]+)*@[a-z0-9-]+(\\.[a-z0-9-]+)*(\\.[a-z]{2,6})$", Pattern.CASE_INSENSITIVE).matcher(email).matches()) //$NON-NLS-1$
 			: false);
 	}
 
