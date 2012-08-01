9ee0480e4f06b72d610e8709ba6d07c74e92376c
Andrei Costache <acostache@servoy.com>
2012-08-01 15:16:30 +0000

    setSelectedIndexes doesn't
    forgot docs
    Case: SVY-2783

--- servoy_shared/src/com/servoy/j2db/dataprocessing/FoundSet.java
+++ servoy_shared/src/com/servoy/j2db/dataprocessing/FoundSet.java
@@ -2325,7 +2325,7 @@ public abstract class FoundSet implements IFoundSetInternal, IRowListener, Scrip
 			selected = new Number[selectedIndexes.length];
 			for (int i = 0; i < selectedIndexes.length; i++)
 			{
-				selected[i] = new Integer(selectedIndexes[i] + 1);
+				selected[i] = Integer.valueOf(selectedIndexes[i] + 1);
 			}
 		}
 		return selected;
