70271a3243585450d550ba18de5741dc52527df7
Gabi Boros <gboros@servoy.com>
2012-08-09 10:59:55 +0000

    backport of SVY-2543 WC TableView row selection when clicking on label does not work Case: SVY-2814

--- servoy_headless_client/src/com/servoy/j2db/server/headlessclient/dataui/WebBaseButton.java
+++ servoy_headless_client/src/com/servoy/j2db/server/headlessclient/dataui/WebBaseButton.java
@@ -1046,7 +1046,8 @@ public abstract class WebBaseButton extends Button implements IButton, IResource
 		// position the <span> in the <button>. However, for centering vertically we drop this absolute positioning and
 		// rely on the fact that by default the <button> tag vertically centers its content.
 		StringBuffer instrumentedBodyText = new StringBuffer();
-		instrumentedBodyText.append("<span style='" + (bodyCursor == null ? "" : "cursor: " + (bodyCursor.getType() == Cursor.HAND_CURSOR ? "pointer" : "default") + "; ") + "display: block;"); //$NON-NLS-1$
+		// how is it possible that span receives focus when you click on it? i guess weird behavior from ie; give focus to component if that happens
+		instrumentedBodyText.append("<span onfocus='this.parentNode.focus()' style='" + (bodyCursor == null ? "" : "cursor: " + (bodyCursor.getType() == Cursor.HAND_CURSOR ? "pointer" : "default") + "; ") + "display: block;"); //$NON-NLS-1$
 		int top = 0;
 		int bottom = 0;
 		int left = 0;
