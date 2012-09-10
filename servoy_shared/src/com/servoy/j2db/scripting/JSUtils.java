85be44db474e2a5bb615c3b2fbbf2a4a6dc1c830
Laurian Vostinar <lvostinar@servoy.com>
2012-09-10 11:57:45 +0000

    The function utils.stringToNumber has been changed between Servoy 5.x and Servoy 6.x. In Servoy 6.x is now uses the locale to find out the decimalseperator. This will break existing code (adjust sample) Case: SVY-2964

--- servoy_shared/src/com/servoy/j2db/scripting/JSUtils.java
+++ servoy_shared/src/com/servoy/j2db/scripting/JSUtils.java
@@ -703,7 +703,7 @@ public class JSUtils
 	 * Filters characters out of from a string and leaves digits, returns the number. Uses locale decimal separator.
 	 *
 	 * @sample 
-	 * //returns '65567'
+	 * //returns 65567
 	 * var retval = utils.stringToNumber('fg65gf567'); 
 	 *
 	 * @param textString the text to process
@@ -718,8 +718,8 @@ public class JSUtils
 	 * Filters characters out of from a string and leaves digits, returns the number. Decimal separator is specified as parameter.
 	 *
 	 * @sample 
-	 * //returns '65567'
-	 * var retval = utils.stringToNumber('fg65gf567'); 
+	 * //returns 65.567
+	 * var retval = utils.stringToNumber('fg65gf.567','.'); 
 	 *
 	 * @param textString the text to process
 	 * @param decimalSeparator decimal separator used at parsing
