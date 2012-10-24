786ac9c533e1af383c5a78bd1e32e99c6b9307d0
Andrei Costache <acostache@servoy.com>
2012-10-24 12:47:22 +0000

    Servoy TYPE_AHEAD not working correctly when using global method for the value list
    - removing lookupListChangeListener at (globalmethod)valuelist filling, in order not to mark this as changed for (re)rendering
    Case: SVY-2914

--- servoy_shared/src/com/servoy/j2db/dataprocessing/LookupListModel.java
+++ servoy_shared/src/com/servoy/j2db/dataprocessing/LookupListModel.java
@@ -352,6 +352,7 @@ public class LookupListModel extends AbstractListModel
 		if (lookup instanceof GlobalMethodValueList)
 		{
 			GlobalMethodValueList clist = (GlobalMethodValueList)lookup;
+			clist.removeLookupListChangeListener();
 			clist.fill(realState, txt, null);
 			for (int i = 0; i < clist.getSize(); i++)
 			{
@@ -361,6 +362,7 @@ public class LookupListModel extends AbstractListModel
 				alReal.add(clist.getRealElementAt(i));
 			}
 			hadMoreRows = true;
+			clist.reAddLookupListChangeListener();
 		}
 		else if (lookup instanceof CustomValueList)
 		{
