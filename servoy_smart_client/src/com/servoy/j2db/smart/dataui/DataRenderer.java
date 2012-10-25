3e7ce0adffce62127996ef6fd3490393741111a5
Gabi Boros <gboros@servoy.com>
2012-10-25 15:57:30 +0000

    fix SC combobox copy/paste
    Case: SVY-3250

--- servoy_smart_client/src/com/servoy/j2db/smart/dataui/DataRenderer.java
+++ servoy_smart_client/src/com/servoy/j2db/smart/dataui/DataRenderer.java
@@ -175,6 +175,7 @@ public class DataRenderer extends StyledEnablePanel implements ListCellRenderer,
 						child.addMouseMotionListener(dragTester);
 						child.addMouseListener(dragTester);
 						if (child instanceof ISupportDragNDropTextTransfer) ((ISupportDragNDropTextTransfer)child).clearTransferHandler();
+						else if (child instanceof DataComboBox) ((DataComboBox)child).disableEditorTransferHandler();
 						else ((JComponent)child).setTransferHandler(null);
 					}
 				}
