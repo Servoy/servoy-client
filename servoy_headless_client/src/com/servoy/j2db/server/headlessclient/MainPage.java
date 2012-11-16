23def0db30c018b1689176fdd49ae90a0f213f56
Andrei Costescu <acostescu@servoy.com>
2012-11-16 12:24:42 +0000

    Fixed SVY-3380 - Modal dialog does no longer show up after logging in, only after refresh by reverting the fix for SVY-3262. In FF a timer req. generated before a form switch that was now executing immediately after the form switch wanted to call the show JS before the new form response was completely parsed (DOM ready). It seems that only FF allows such requests in the time interval between receiving a response and DOM ready. Case: SVY-3380

--- servoy_headless_client/src/com/servoy/j2db/server/headlessclient/MainPage.java
+++ servoy_headless_client/src/com/servoy/j2db/server/headlessclient/MainPage.java
@@ -275,20 +275,6 @@ public class MainPage extends WebPage implements IMainContainer, IAjaxIndicatorA
 			return generateCallbackScript("wicketAjaxGet('" + getCallbackUrl(onlyTargetActivePage) + "&ignoremp=true'");
 		}
 
-		/*
-		 * (non-Javadoc)
-		 * 
-		 * @see org.apache.wicket.behavior.AbstractAjaxBehavior#getCallbackUrl(boolean)
-		 */
-		@Override
-		public CharSequence getCallbackUrl(boolean onlyTargetActivePage)
-		{
-			if (getComponent() == null)
-			{
-				throw new IllegalArgumentException("Behavior must be bound to a component to create the URL"); //$NON-NLS-1$
-			}
-			return getComponent().urlFor(this, AlwaysLastPageVersionRequestListenerInterface.INTERFACE);
-		}
 	}
 
 
@@ -419,20 +405,6 @@ public class MainPage extends WebPage implements IMainContainer, IAjaxIndicatorA
 					return !client.getFlattenedSolution().isInDesign(null) && ((getController() != null && getController().isFormVisible()) || closingAsWindow);
 				}
 
-				/*
-				 * (non-Javadoc)
-				 * 
-				 * @see org.apache.wicket.behavior.AbstractAjaxBehavior#getCallbackUrl(boolean)
-				 */
-				@Override
-				public CharSequence getCallbackUrl(boolean onlyTargetActivePage)
-				{
-					if (getComponent() == null)
-					{
-						throw new IllegalArgumentException("Behavior must be bound to a component to create the URL"); //$NON-NLS-1$
-					}
-					return getComponent().urlFor(this, AlwaysLastPageVersionRequestListenerInterface.INTERFACE);
-				}
 			});
 
 		}
