15d15f2fb67a880f55b6b60c631613830e23b795
Rob Gansevles <rgansevles@servoy.com>
2012-11-21 11:37:58 +0000

    small doc improve

--- servoy_shared/src/com/servoy/j2db/dataprocessing/JSDatabaseManager.java
+++ servoy_shared/src/com/servoy/j2db/dataprocessing/JSDatabaseManager.java
@@ -3581,6 +3581,7 @@ public class JSDatabaseManager implements IJSDatabaseManager
 	 * Create a QueryBuilder object for a datasource.
 	 * @sample
 	 * 
+	 * /** @type {QBSelect<db:/example_data/book_nodes>} *&#47;
 	 * var q = databaseManager.createSelect('db:/example_data/book_nodes')
 	 * q.result.addPk()
 	 * q.where.add(q.columns.label_text.not.isin(null))
