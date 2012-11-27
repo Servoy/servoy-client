b13635eaf78393d20c1ec47f16dfdd96d1322278
Andrei Costache <acostache@servoy.com>
2012-11-27 16:27:33 +0000

    Version bump for 61x branch for new released build (to mirror tag version): needed for cc builds and update zips

--- servoy_shared/src/com/servoy/j2db/ClientVersion.java
+++ servoy_shared/src/com/servoy/j2db/ClientVersion.java
@@ -28,8 +28,8 @@ public abstract class ClientVersion
 	private static final int majorVersion = 6;
 	private static final int middleVersion = 1;
 	private static final int minorVersion = 3;
-	private static final int releaseNumber = 1423;
-	private static final String versionPostfix = "rc";
+	private static final int releaseNumber = 1424;
+	private static final String versionPostfix = "";
 
 	// make sure you keep this the same format, or make it work with extensions version comparing & xml schema
 	private static final String version = majorVersion + "." + middleVersion + "." + minorVersion + (versionPostfix != null ? " " + versionPostfix : "");
