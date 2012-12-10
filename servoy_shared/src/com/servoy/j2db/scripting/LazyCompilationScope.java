e1dec8ae6d4b3ed7b0d974f237e1f2b1a69427af
Tiberiu Popovici <tpopovici@servoy.com>
2012-12-10 11:17:21 +0000

    onshow event in scope does not fire when client is first loaded. Reverted changes, becuase cc tests did not passed Case: SVY-3495

--- servoy_shared/src/com/servoy/j2db/scripting/LazyCompilationScope.java
+++ servoy_shared/src/com/servoy/j2db/scripting/LazyCompilationScope.java
@@ -63,15 +63,7 @@ public abstract class LazyCompilationScope extends DefaultScope implements LazyI
 		while (it.hasNext())
 		{
 			IScriptProvider sm = it.next();
-			Iterator< ? > iterator = sm.getParent().getAllObjects();
-			while (iterator.hasNext())
-			{
-				Object o = iterator.next();
-				if (o instanceof IScriptProvider)
-				{
-					put((IScriptProvider)o, o, overwriteInitialValue);
-				}
-			}
+			put(sm, sm, overwriteInitialValue);
 		}
 	}
 
