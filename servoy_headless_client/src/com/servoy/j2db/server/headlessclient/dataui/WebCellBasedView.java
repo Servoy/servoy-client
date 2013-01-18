02657111729373613e8ac221f4f4e0883a530ff8
Ovidiu Buligan <obuligan@servoy.com>
2013-01-18 12:52:49 +0000

    Row CSS and onRender does not work correctly in webclient Case: SVY-3665

--- servoy_headless_client/src/com/servoy/j2db/server/headlessclient/dataui/WebCellBasedView.java
+++ servoy_headless_client/src/com/servoy/j2db/server/headlessclient/dataui/WebCellBasedView.java
@@ -1082,8 +1082,11 @@ public class WebCellBasedView extends WebMarkupContainer implements IView, IPort
 								((IScriptableProvider)innerComponent).getScriptObject() instanceof IRuntimeComponent &&
 								((IRuntimeComponent)((IScriptableProvider)innerComponent).getScriptObject()).isTransparent() && bgColor != null)
 							{
-								// apply the bg color even if transparent
-								if (innerComponent instanceof IProviderStylePropertyChanges &&
+								IScriptable s = ((IScriptableProvider)innerComponent).getScriptObject();
+								boolean hasOnRender = ((ISupportOnRenderCallback)s).getRenderEventExecutor().hasRenderCallback();
+
+								// apply the bg color even if transparent and does not have  onRender
+								if (innerComponent instanceof IProviderStylePropertyChanges && !hasOnRender &&
 									((IProviderStylePropertyChanges)innerComponent).getStylePropertyChanges() instanceof IStylePropertyChangesRecorder)
 								{
 									((IStylePropertyChangesRecorder)(((IProviderStylePropertyChanges)innerComponent).getStylePropertyChanges())).setBgcolor(bgColor);
@@ -1095,7 +1098,7 @@ public class WebCellBasedView extends WebMarkupContainer implements IView, IPort
 						if (((updateComponentRenderState(c, isSelected)) || (!ignoreStyles && (bgColor != null || fgColor != null || compFont != null || compBorder != null))) &&
 							target != null)
 						{
-							target.addComponent(innerComponent);
+							target.addComponent(innerComponent.getParent());
 							WebEventExecutor.generateDragAttach(innerComponent, target.getHeaderResponse());
 							if (!innerComponent.isVisible())
 							{
@@ -3808,7 +3811,7 @@ public class WebCellBasedView extends WebMarkupContainer implements IView, IPort
 
 	private void applyStyleOnComponent(Component comp, Object bgColor, Object fgColor, Object compFont, Object compBorder)
 	{
-		if (comp instanceof IScriptableProvider)
+		if (comp instanceof IScriptableProvider && !hasOnRender)
 		{
 			IScriptable s = ((IScriptableProvider)comp).getScriptObject();
 
