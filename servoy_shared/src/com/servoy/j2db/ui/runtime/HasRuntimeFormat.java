2b43d667414d2ea2f722809449f7b24930e6f21f
Ovidiu Buligan <obuligan@servoy.com>
2013-02-06 09:59:14 +0000

    Font is not returned on element level when using default font +merge of documentation of a feature that is not in 6.1 Case: SVY-3819

--- servoy_shared/src/com/servoy/j2db/ui/runtime/HasRuntimeFormat.java
+++ servoy_shared/src/com/servoy/j2db/ui/runtime/HasRuntimeFormat.java
@@ -31,17 +31,6 @@ public interface HasRuntimeFormat
 	/**
 	 * Gets or sets the display formatting of an element for number and text values; does not affect the actual value stored in the database column.
 	 *
-
-	 * There are different options for the different dataprovider types that is assigned to this field.
-	 * <p>For Integer fields, there is a display and edit format, using http://docs.oracle.com/javase/7/docs/api/java/text/DecimalFormat.html and the max (string) length can be set.
-	 * <p>For Text/String fields, there are options to force uppercase,lowercase or only numbers. Or a mask can be set that restrict the input the pattern chars can be found here: http://docs.oracle.com/javase/7/docs/api/javax/swing/text/MaskFormatter.html
-	 * A mask can have a placehoder (what is shown when there is no data) and if the data must be stored raw (without literals of the mask). A max text length can also be set to force
-	 * the max text length input, this doesn't work on mask because that max length is controlled with the mask.
-	 * <p>For Date fields a display and edit format can be set by using a pattern from here: http://docs.oracle.com/javase/7/docs/api/java/text/SimpleDateFormat.html, you can also say this must behave like a mask (the edit format)
-	 * A mask only works with when the edit format is exactly that mask (1 char is 1 number/char), because for example MM then only 2 numbers are allowed MMM that displays the month as a string is not supported as a mask.
-	 * Some examples are "dd-MM-yyyy", "MM-dd-yyyy", etc.
-	 * <p>The format property is also used to set the UI Converter, this means that you can convert the value object to something else before it gets set into the field, this can also result in a type change of the data. 
-	 * So a string in scripting/db is converted to a integer in the ui, then you have to set an integer format.
 	 * <p>It only returns it's correct value if it was explicitly set, otherwise null.
 	 * 
 	 * @sample
