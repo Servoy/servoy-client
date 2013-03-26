8411de6fa1de8e99ec46b73caf6b3d99a5d837f6
Laurian Vostinar <lvostinar@servoy.com>
2013-03-26 10:28:52 +0000

    Form (sub) class name leaks through to form elements Case: SVY-4164

--- servoy_shared/src/com/servoy/j2db/util/ServoyAttributeResolver.java
+++ servoy_shared/src/com/servoy/j2db/util/ServoyAttributeResolver.java
@@ -13,7 +13,7 @@
  You should have received a copy of the GNU Affero General Public License along
  with this program; if not, see http://www.gnu.org/licenses or write to the Free
  Software Foundation,Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301
-*/
+ */
 
 package com.servoy.j2db.util;
 
@@ -51,7 +51,7 @@ public class ServoyAttributeResolver implements AttributeResolver
 	{
 		String elem = (String)e;
 		int index = elem.lastIndexOf('.');
-		if (index != -1)
+		if (index != -1 && index > elem.lastIndexOf(' '))
 		{
 			return elem.substring(index + 1);
 		}
