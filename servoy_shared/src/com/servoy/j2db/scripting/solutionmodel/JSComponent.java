33482e5218710f7682b13f167315a155119e9c3c
Haniela Hardut <hhardut@servoy.com>
2013-04-15 10:58:54 +0000

    SVY-4291 - Error in sample code for borderType JSLabel, JSField and others Case: SVY-4291

--- servoy_shared/src/com/servoy/j2db/scripting/solutionmodel/JSComponent.java
+++ servoy_shared/src/com/servoy/j2db/scripting/solutionmodel/JSComponent.java
@@ -78,7 +78,7 @@ public class JSComponent<T extends BaseComponent> extends JSBase<T> implements I
 	 * @sample
 	 * //HINT: To know exactly the notation of this property set it in the designer and then read it once out through the solution model.
 	 * var field = form.newField('my_table_text', JSField.TEXT_FIELD, 10, 10, 100, 20);
-	 * field.borderType = solutionModel.createBorder(1,'#ff0000');;
+	 * field.borderType = solutionModel.createLineBorder(1,'#ff0000');
 	 */
 	public String js_getBorderType()
 	{
