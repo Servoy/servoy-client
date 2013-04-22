e25a8da9dcc49bdf666e9ded2eb044d642817640
Johan Compagner <jcompagner@servoy.com>
2013-04-22 15:24:40 +0000

    Relation cannot be used in find, general issue with lack of documentation...
    the message "Relation paymentoption_to_paymentoptiondefault cannot be used in a find" 
    
    will now have extra info:
    
    "Relation paymentoption_to_paymentoptiondefault cannot be used in a find (REASON)"
    
    where REASON will be one of these:
    
    "global relation"
    "multi server"
    "server/table not valid/loaded"
    "relation primary datasource: 'relationDS' != findsate primary datasource: 'findStateDS'"
    Case: SVY-19

--- servoy_shared/src/com/servoy/j2db/dataprocessing/FindState.java
+++ servoy_shared/src/com/servoy/j2db/dataprocessing/FindState.java
@@ -499,9 +499,17 @@ public class FindState implements Scriptable, IRecordInternal, Serializable, IJS
 				{
 					if (!getValidSearchRelations().contains(r))
 					{
-						Debug.warn("Find: skip related search for '" + partName + "', relation cannot be used in search");
+						String reason = "";
+						if (r.isGlobal()) reason = "global relation";
+						else if (r.isMultiServer()) reason = "multi server";
+						else if (r.isValid()) reason = "server/table not valid/loaded";
+						else
+						{
+							reason = "relation primary datasource: " + r.getPrimaryDataSource() + " != findsate primary datasource: " + parent.getDataSource();
+						}
+						Debug.warn("Find: skip related search for '" + partName + "', relation cannot be used in search, because: " + reason);
 						parent.getFoundSetManager().getApplication().reportJSError(
-							Messages.getString("servoy.relation.find.unusable", new Object[] { partName }), null);
+							Messages.getString("servoy.relation.find.unusable", new Object[] { partName }) + " (" + reason + ')', null); //$NON-NLS-2$
 						return null;
 					}
 					SQLSheet sheet = parent.getSQLSheet().getRelatedSheet(
