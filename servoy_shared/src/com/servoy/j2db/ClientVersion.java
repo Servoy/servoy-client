6195305aaf3ee6c8992eb30a05395e29794973f5
Laurian Vostinar <lvostinar@servoy.com>
2013-04-26 10:33:28 +0000

    Make sure 7.y.x (where x > 0, so minor versions) also check for needed updates in the marketplace Case: SVY-4336

--- servoy_shared/src/com/servoy/j2db/ClientVersion.java
+++ servoy_shared/src/com/servoy/j2db/ClientVersion.java
@@ -16,6 +16,8 @@
  */
 package com.servoy.j2db;
 
+import com.servoy.j2db.util.Utils;
+
 /**
  * Servoy version info class
  * 
@@ -47,6 +49,19 @@ public abstract class ClientVersion
 	public static String getBundleVersion()
 	{
 		return majorVersion + "." + middleVersion + "." + minorVersion + "." + releaseNumber;
+	}
+
+	public static int[] parseBundleVersion(String bundleVersion)
+	{
+		if (bundleVersion != null)
+		{
+			String[] parts = bundleVersion.split("\\.");
+			if (parts != null && parts.length == 4)
+			{
+				return new int[] { Utils.getAsInteger(parts[0]), Utils.getAsInteger(parts[1]), Utils.getAsInteger(parts[2]), Utils.getAsInteger(parts[3]) };
+			}
+		}
+		return null;
 	}
 
 	public static int getMajorVersion()
