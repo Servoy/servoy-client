d945e77d40b25d8324971042e8a6801c3fc2267c
Rob Gansevles <rgansevles@servoy.com>
2013-06-11 12:44:51 +0000

    Move all SolutionModel stuff currently located under the Mobile plugin to the SolutionModel node
    Deprecated classes were still shown in solex tree and code completion
    Case: SVY-4155

--- servoy_shared/src/com/servoy/j2db/scripting/IDeprecated.java
+++ servoy_shared/src/com/servoy/j2db/scripting/IDeprecated.java
@@ -13,16 +13,18 @@
  You should have received a copy of the GNU Affero General Public License along
  with this program; if not, see http://www.gnu.org/licenses or write to the Free
  Software Foundation,Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301
-*/
+ */
 package com.servoy.j2db.scripting;
 
 /**
  * Interface for tagging ScriptObjects as deprecated.
+ * No longer needed, use Deprecated annotation instead.
  * 
  * @author jcompagner
  * 
  * @since 5.0
  */
+@Deprecated
 public interface IDeprecated
 {
 
