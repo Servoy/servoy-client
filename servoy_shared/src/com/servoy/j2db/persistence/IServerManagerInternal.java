d2ef4ac30874ec036a0f37dc37cda1868b80f0a3
Gabi Boros <gboros@servoy.com>
2013-08-05 10:23:01 +0000

    more fixes for server clone cycles handling
    Case: SVY-4860

--- servoy_shared/src/com/servoy/j2db/persistence/IServerManagerInternal.java
+++ servoy_shared/src/com/servoy/j2db/persistence/IServerManagerInternal.java
@@ -84,4 +84,6 @@ public interface IServerManagerInternal extends IServerManager
 	void setGlobalColumnInfoProviders(IServerInternal server, IDeveloperRepository rep, String clientId);
 
 	void setGlobalSequenceProvider(ISequenceProvider sm);
+
+	public boolean isServerDataModelCloneCycling(IServerInternal server);
 }
