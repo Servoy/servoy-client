f822de7274d2a824da6d5aeb8b7dfd7a672287b8
Andrei Costache <acostache@servoy.com>
2013-08-26 12:28:35 +0000

    New version: 7.2.0 - build 2015 - i2

--- servoy_shared/src/com/servoy/j2db/ClientVersion.java
+++ servoy_shared/src/com/servoy/j2db/ClientVersion.java
@@ -29,8 +29,8 @@ public abstract class ClientVersion
 	private static final int majorVersion = 7;
 	private static final int middleVersion = 2;
 	private static final int minorVersion = 0;
-	private static final int releaseNumber = 2014;
-	private static final String versionPostfix = "i1";
+	private static final int releaseNumber = 2015;
+	private static final String versionPostfix = "i2";
 
 	// make sure you keep this the same format, or make it work with extensions version comparing & xml schema
 	private static final String version = majorVersion + "." + middleVersion + "." + minorVersion + (versionPostfix != null ? " " + versionPostfix : "");
