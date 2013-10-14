582e676ebb769fd615d6524dd8cddba21cd12250
Laurian Vostinar <lvostinar@servoy.com>
2013-10-14 08:21:49 +0000

    [ Solution im/export ] Scale in column type is not exported to .servoy file Case: SVY-3072

--- servoy_shared/src/com/servoy/j2db/util/xmlxport/ColumnInfoDef.java
+++ servoy_shared/src/com/servoy/j2db/util/xmlxport/ColumnInfoDef.java
@@ -45,6 +45,7 @@ public final class ColumnInfoDef implements Serializable
 	public static final String AUTO_ENTER_TYPE = "autoEnterType"; //$NON-NLS-1$
 	public static final String ALLOW_NULL = "allowNull"; //$NON-NLS-1$
 	public static final String LENGTH = "length"; //$NON-NLS-1$
+	public static final String SCALE = "scale"; //$NON-NLS-1$
 	public static final String DATA_TYPE = "dataType"; //$NON-NLS-1$
 	public static final String COMPATIBLE_COLUMN_TYPES = "compatibleColumnTypes"; //$NON-NLS-1$
 	public static final String CREATION_ORDER_INDEX = "creationOrderIndex"; //$NON-NLS-1$
