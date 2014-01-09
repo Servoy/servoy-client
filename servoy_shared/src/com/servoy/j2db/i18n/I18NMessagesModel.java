ab5b5c80c2f02d88c851718b5f7120cf22f3a301
Andrei Costescu <acostescu@servoy.com>
2014-01-09 13:29:12 +0000

    Related to SVY-5655: Removed bad dependency. Case: SVY-5655

--- servoy_shared/src/com/servoy/j2db/i18n/I18NMessagesModel.java
+++ servoy_shared/src/com/servoy/j2db/i18n/I18NMessagesModel.java
@@ -14,7 +14,7 @@
  with this program; if not, see http://www.gnu.org/licenses or write to the Free
  Software Foundation,Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301
  */
-package com.servoy.j2db.property;
+package com.servoy.j2db.i18n;
 
 import java.util.Collection;
 import java.util.HashMap;
