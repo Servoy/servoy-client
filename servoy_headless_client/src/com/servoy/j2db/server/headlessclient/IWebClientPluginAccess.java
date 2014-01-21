cd8f83d9fdc8ab8fc8e6d9d99726c36f2d0ea227
Andrei Costescu <acostescu@servoy.com>
2014-01-21 14:29:11 +0000

    For SVY-5263 : Css stylesheets/js files added using WebClientUtils plugin lost. This part is to allow removal/addition of behaviors to a form even if it's showing. Case: SVY-5263

--- servoy_headless_client/src/com/servoy/j2db/server/headlessclient/IWebClientPluginAccess.java
+++ servoy_headless_client/src/com/servoy/j2db/server/headlessclient/IWebClientPluginAccess.java
@@ -29,7 +29,7 @@ import com.servoy.j2db.plugins.IClientPluginAccess;
 public interface IWebClientPluginAccess extends IClientPluginAccess
 {
 	/**
-	 * Server a resource, returns the url under which the resource is served, can be shown with showURL. After that it is served the content will be cleaned.
+	 * Serve a resource, returns the url under which the resource is served, can be shown with showURL. After that it is served the content will be cleaned.
 	 * The "Content-disposition" header parameter is set to "attachment", so the clients will know this resource is for downloading.
 	 * 
 	 * @param filename
@@ -40,7 +40,7 @@ public interface IWebClientPluginAccess extends IClientPluginAccess
 	public String serveResource(String filename, byte[] array, String mimetype);
 
 	/**
-	 * Server a resource, returns the url under which the resource is served, can be shown with showURL. After that it is served the content will be cleaned.
+	 * Serve a resource, returns the url under which the resource is served, can be shown with showURL. After that it is served the content will be cleaned.
 	 * 
 	 * @param filename
 	 * @param array the resource as byte array
