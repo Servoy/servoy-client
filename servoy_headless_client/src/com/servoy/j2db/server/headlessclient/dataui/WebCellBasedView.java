ccc573c5e1aaae5b227497ffd7b60d6a3563322c
Ovidiu Buligan <obuligan@servoy.com>
2014-03-10 14:21:46 +0000

    servoy.webclient.scrolling.tableview.multiplier is not used while loading the first entries
    Case: SVY-6109

--- servoy_headless_client/src/com/servoy/j2db/server/headlessclient/dataui/WebCellBasedView.java
+++ servoy_headless_client/src/com/servoy/j2db/server/headlessclient/dataui/WebCellBasedView.java
@@ -199,7 +199,7 @@ public class WebCellBasedView extends WebMarkupContainer implements IView, IPort
 {
 	private static final int SCROLLBAR_SIZE = 17;
 	private static final long serialVersionUID = 1L;
-
+	private final double NEW_PAGE_MULITPLIER;
 	public final ResourceReference R_ARROW_OFF = new ResourceReference(IApplication.class, "images/arrow_off.png"); //$NON-NLS-1$
 	public final ResourceReference R_ARROW_DOWN = new ResourceReference(IApplication.class, "images/arrow_down.png"); //$NON-NLS-1$
 	public final ResourceReference R_ARROW_UP = new ResourceReference(IApplication.class, "images/arrow_up.png"); //$NON-NLS-1$
@@ -1485,6 +1485,15 @@ public class WebCellBasedView extends WebMarkupContainer implements IView, IPort
 		this.isListViewMode = viewType == IForm.LIST_VIEW || viewType == FormController.LOCKED_LIST_VIEW;
 		this.bodyWidthHint = form.getWidth();
 
+		double multiplier = Utils.getAsDouble(Settings.getInstance().getProperty("servoy.webclient.scrolling.tableview.multiplier", "2"));
+		if (multiplier > 0)
+		{
+			NEW_PAGE_MULITPLIER = multiplier;
+		}
+		else
+		{
+			NEW_PAGE_MULITPLIER = 2;
+		}
 		useAJAX = Utils.getAsBoolean(application.getRuntimeProperties().get("useAJAX")); //$NON-NLS-1$
 		useAnchors = Utils.getAsBoolean(application.getRuntimeProperties().get("enableAnchors")); //$NON-NLS-1$
 
@@ -3005,7 +3014,7 @@ public class WebCellBasedView extends WebMarkupContainer implements IView, IPort
 					if (isScrollFirstShow)
 					{
 						table.setStartIndex(0);
-						table.setViewSize(2 * maxRowsPerPage);
+						table.setViewSize((int)(NEW_PAGE_MULITPLIER * maxRowsPerPage));
 						isScrollFirstShow = false;
 					}
 					else
@@ -5226,22 +5235,11 @@ public class WebCellBasedView extends WebMarkupContainer implements IView, IPort
 
 	private class ScrollBehavior extends ServoyAjaxEventBehavior implements IIgnoreDisabledComponentBehavior
 	{
-		private final double NEW_PAGE_MULITPLIER;
-
 		private boolean isGettingRows;
 
 		public ScrollBehavior(String event)
 		{
 			super(event);
-			double multiplier = Utils.getAsDouble(Settings.getInstance().getProperty("servoy.webclient.scrolling.tableview.multiplier", "2"));
-			if (multiplier > 0)
-			{
-				NEW_PAGE_MULITPLIER = multiplier;
-			}
-			else
-			{
-				NEW_PAGE_MULITPLIER = 2;
-			}
 		}
 
 		private static final long serialVersionUID = 1L;
