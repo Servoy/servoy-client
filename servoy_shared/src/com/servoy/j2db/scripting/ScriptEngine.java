e71c80b7e54d82779ccb6d065573642ef8033706
Gabi Boros <gboros@servoy.com>
2014-04-28 13:40:43 +0000

    fix : Exceptions thrown in the "after" table events are not propagated
    Case: SVY-6249

--- servoy_shared/src/com/servoy/j2db/scripting/ScriptEngine.java
+++ servoy_shared/src/com/servoy/j2db/scripting/ScriptEngine.java
@@ -630,7 +630,7 @@ public class ScriptEngine implements IScriptSupport
 			}
 			catch (Exception ex)
 			{
-				if (scope instanceof TableScope || throwException || ex instanceof JavaScriptException)
+				if (scope instanceof TableScope || throwException)
 				{
 					throw ex;//is calc report back via other way
 				}
