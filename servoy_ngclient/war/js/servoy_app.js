7f68b7836b447125454e9edca56072570abf900a
jcompagner <jcompagner@servoy.com>
2014-09-09 12:40:46 +0200

    do the conversion a bit later because applyBeanData can be called first

--- servoy_ngclient/war/js/servoy_app.js
+++ servoy_ngclient/war/js/servoy_app.js
@@ -43,14 +43,17 @@ angular.module('servoyApp', ['servoy','webStorageModule','ngGrid','servoy-compon
 				   }
 			   }
 			   if (changed) {
-				   if (beanConversionInfo && beanConversionInfo[prop]) changes[prop] = $sabloConverters.convertFromClientToServer(now[prop], beanConversionInfo[prop], prev ? prev[prop] : undefined);
-				   else changes[prop] = $sabloUtils.convertClientObject(now[prop])
+				   changes[prop] = now[prop];
 			   }
 		   }
 		   if (changes.location || changes.size || changes.visible || changes.anchors) {
 			   if (beanLayout) {
 				   applyBeanData(now /*formStates[formname].model[beanname]*/, beanLayout, changes, parentSize, changeNotifier);
 			   }
+		   }
+		   for (prop in changes) {
+			   if (beanConversionInfo && beanConversionInfo[prop]) changes[prop] = $sabloConverters.convertFromClientToServer(changes[prop], beanConversionInfo[prop], prev ? prev[prop] : undefined);
+			   else changes[prop] = $sabloUtils.convertClientObject(changes[prop])
 		   }
 		   return changes;
 	   };
