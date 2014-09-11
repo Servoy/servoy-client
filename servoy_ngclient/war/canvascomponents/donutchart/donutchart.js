ed5670ca6aa7f52ab2810be8b7b478c8f0148a0d
Gabi Boros <gboros@servoy.com>
2014-09-11 11:29:16 +0300

    SVY-7139 donut uses percentage for display

--- servoy_ngclient/war/canvascomponents/donutchart/donutchart.js
+++ servoy_ngclient/war/canvascomponents/donutchart/donutchart.js
@@ -6,13 +6,22 @@ angular.module('donutchart',['servoy']).directive('donutchart', function($window
     	  model: "=svyModel",
       },      
       controller: function($scope, $element, $attrs) {
+    	  $scope.formatter = function(value, data) {
+    		  var total = 0;
+    		  for(var e in $scope.model.dataProviderID.data) {
+    			  total += $scope.model.dataProviderID.data[e].value;
+    		  }
+    		  return (value * 100 / total).toFixed(0) + '%';
+    	  };
+    	  
     	  $scope.$watch('model.dataProviderID', function(newVal, oldVal) {
     		  if($scope.model.dataProviderID) {
 	    		  Morris.Donut({
 	    			  element: 'donut-chart',
 	        		  data: $scope.model.dataProviderID.data,
 	        		  colors: $scope.model.dataProviderID.colors,  
-	        		  resize: false
+	        		  resize: false,
+	        		  formatter: $scope.formatter
 	    		  });
     		  }
     	  }, true);
@@ -23,7 +32,8 @@ angular.module('donutchart',['servoy']).directive('donutchart', function($window
 	    			  element: 'donut-chart',
 	        		  data: $scope.model.dataProviderID.data,
 	        		  colors: $scope.model.dataProviderID.colors,  
-	        		  resize: false
+	        		  resize: false,
+	        		  formatter: $scope.formatter
 	    		  });
     		  }
     	  });
