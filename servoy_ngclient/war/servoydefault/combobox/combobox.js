13d2dbf0dc740914da3f13a43e77083f2a3afee4
lvostinar@servoy.com <lvostinar@servoy.com>
2014-10-20 13:03:24 +0300

    SVY-7042 combobox uses select2 that overrides background settings that ng-invalid class provides

--- servoy_ngclient/war/servoydefault/combobox/combobox.js
+++ servoy_ngclient/war/servoydefault/combobox/combobox.js
@@ -9,7 +9,8 @@ angular.module('servoydefaultCombobox',['servoy','ui.select'])
         handlers: "=svyHandlers"
       },
       controller: function($scope, $element, $attrs) {
-    	   $scope.style = {width:'100%',height:'100%',overflow:'hidden'}
+    	  var minHeight =  $scope.model.size.height+'px';
+    	   $scope.style = {'min-height':minHeight,height:'100%'};
     	   $scope.findMode = false;
       },
       link: function(scope, element, attr) {
