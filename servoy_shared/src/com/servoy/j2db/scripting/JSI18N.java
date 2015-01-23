23e0af76edb932865a4afd72137579e3bf67e87e
Andrei Costescu <costescuandrei@gmail.com>
2015-01-23 17:53:39 +0200

    JSDoc - updated some obsolete URLs.

--- servoy_shared/src/com/servoy/j2db/scripting/JSI18N.java
+++ servoy_shared/src/com/servoy/j2db/scripting/JSI18N.java
@@ -65,18 +65,18 @@ public class JSI18N implements IJSI18N
 	/**
 	 * Set/Overwrite the locale for this client.
 	 * All forms not yet loaded will change (execute this in solution startup or first form).
-	 * 
+	 *
 	 * The language must be a lowercase 2 letter code defined by ISO-639.
-	 * see http://www.ics.uci.edu/pub/ietf/http/related/iso639.txt
-	 * The country must be an upper case 2 letter code defined by IS-3166
-	 * see http://www.chemie.fu-berlin.de/diverse/doc/ISO_3166.html 
+	 * see ISO 639-1 codes at http://en.wikipedia.org/wiki/List_of_ISO_639-1_code
+	 * The country must be an upper case 2 letter code defined by ISO-3166
+	 * see ISO-3166-1 codes at http://en.wikipedia.org/wiki/ISO_3166-1_alpha-2
 	 *
 	 * NOTE: For more information on i18n, see the chapter on Internationalization in the Servoy Developer User's Guide, and the chapter on Internationalization-I18N in the Programming Guide.
-	 * 
+	 *
 	 * @sample
 	 * //Warning: already created form elements with i18n text lookup will not change,
 	 * //so call this method in the solution startup method or in methods from first form
-	 * 
+	 *
 	 * i18n.setLocale('en','US');
 	 *
 	 * @param language The lowercase 2 letter code
@@ -90,12 +90,12 @@ public class JSI18N implements IJSI18N
 	}
 
 	/**
-	 * Gets the current language; based on the current locale settings in the Servoy Client Locale preferences. 
-	 * 
+	 * Gets the current language; based on the current locale settings in the Servoy Client Locale preferences.
+	 *
 	 * NOTE: For more information on i18n, see the chapter on Internationalization in the Servoy Developer User's Guide, and the chapter on Internationalization-I18N in the Programming Guide.
 	 * @sample
 	 * var currLang = i18n.getCurrentLanguage();
-	 * 
+	 *
 	 * @return a String representing the current language.
 	 */
 	@JSFunction
@@ -105,13 +105,13 @@ public class JSI18N implements IJSI18N
 	}
 
 	/**
-	 * Gets the current country; based on the current locale settings in the Servoy Client Locale preferences. 
-	 * 
+	 * Gets the current country; based on the current locale settings in the Servoy Client Locale preferences.
+	 *
 	 * NOTE: For more information on i18n, see the chapter on Internationalization in the Servoy Developer User's Guide, and the chapter on Internationalization-I18N in the Programming Guide.
-	 * 
+	 *
 	 * @sample
 	 * var currCountry = i18n.getCurrentCountry();
-	 * 
+	 *
 	 * @return a String representing the current country.
 	 */
 	@JSFunction
@@ -125,7 +125,7 @@ public class JSI18N implements IJSI18N
 	 *
 	 * @sample
 	 * var currTimeZone = i18n.getCurrentTimeZone();
-	 * 
+	 *
 	 * @return a String representing the current time zone.
 	 */
 	@JSFunction
@@ -136,10 +136,10 @@ public class JSI18N implements IJSI18N
 
 	/**
 	 * Gets the current default date format; based on the current locale settings in the Servoy Client Locale preferences.
-	 *  
+	 *
 	 * @sample
 	 * var defaultDateFormat = i18n.getDefaultDateFormat();
-	 * 
+	 *
 	 * @return a String representing the default date format.
 	 */
 	@JSFunction
@@ -150,10 +150,10 @@ public class JSI18N implements IJSI18N
 
 	/**
 	 * Gets the current default number format; based on the current locale settings in the Servoy Client Locale preferences.
-	 * 
+	 *
 	 * @sample
 	 * var defaultNumberFormat = i18n.getDefaultNumberFormat();
-	 * 
+	 *
 	 * @return a String representing the default number format.
 	 */
 	@JSFunction
@@ -166,16 +166,16 @@ public class JSI18N implements IJSI18N
 	 * Returns a dataset with rows that contains a language key (en) and the displayname (English) column.
 	 *
 	 * See http://www.ics.uci.edu/pub/ietf/http/related/iso639.txt for a list that could be returned.
-	 * 
+	 *
 	 * @sample
 	 * var set = i18n.getLanguages();
 	 * for(var i=1;i<=set.getMaxRowIndex();i++)
 	 * {
 	 * 	application.output(set.getValue(i, 1) + " " + set.getValue(i, 2));
 	 * }
-	 * 
+	 *
 	 * @return a JSDataSet with all the languages.
-	 * 
+	 *
 	 * @link http://www.ics.uci.edu/pub/ietf/http/related/iso639.txt
 	 */
 	@JSFunction
@@ -203,9 +203,9 @@ public class JSI18N implements IJSI18N
 	/**
 	 * Returns an array of known timezones.
 	 *
-	 * @sample 
+	 * @sample
 	 * var timeZones = i18n.getTimeZones();
-	 * 
+	 *
 	 * @return an Array with all the timezones.
 	 */
 	@JSFunction
@@ -222,7 +222,7 @@ public class JSI18N implements IJSI18N
 	 * var timeZoneOffset = i18n.getTimeZoneOffset('America/Los_Angeles');
 	 *
 	 * @param timeZoneId The time zone to get the offset for.
-	 * 
+	 *
 	 * @return an int representing the time zone's offset from UTC.
 	 */
 	@JSFunction
@@ -234,10 +234,10 @@ public class JSI18N implements IJSI18N
 	/**
 	 * @clonedesc getTimeZoneOffset(String)
 	 * @sampleas getTimeZoneOffset(String)
-	 * 
+	 *
 	 * @param timeZoneId The time zone to get the offset for.
 	 * @param date The date in the time zone (default current date). Needed in case daylight saving time/GMT offset changes are used in the time zone.
-	 * 
+	 *
 	 * @return an int representing the time zone's offset from UTC.
 	 */
 	@JSFunction
@@ -252,7 +252,7 @@ public class JSI18N implements IJSI18N
 	}
 
 	/**
-	 * Gets the real message (for the clients locale) for a specified message key. 
+	 * Gets the real message (for the clients locale) for a specified message key.
 	 *
 	 * @sample
 	 * // returns 'Welcome my_name in my solution'
@@ -270,7 +270,7 @@ public class JSI18N implements IJSI18N
 	}
 
 	/**
-	 * Gets the real message (for the clients locale) for a specified message key. 
+	 * Gets the real message (for the clients locale) for a specified message key.
 	 * You can use parameter substitution by using {n}, where n is a index number of the value thats in the arguments array.
 	 *
 	 * @sample
@@ -281,7 +281,7 @@ public class JSI18N implements IJSI18N
 	 * @param i18nKey The message key
 	 *
 	 * @param dynamicValues Arguments array when using parameter substitution.
-	 * 
+	 *
 	 * @return a String that is the message for the message key.
 	 */
 	@JSFunction
@@ -317,7 +317,7 @@ public class JSI18N implements IJSI18N
 	}
 
 	/**
-	 * Sets the value of i18n key for client scope,if value null the setting is removed. 
+	 * Sets the value of i18n key for client scope,if value null the setting is removed.
 	 * All forms not yet loaded will change (execute this in solution startup or first form)
 	 *
 	 * @sample
@@ -325,7 +325,7 @@ public class JSI18N implements IJSI18N
 	 * //Warning: already created form elements with i18n text lookup will not change,
 	 * //so call this method in the solution startup method or in methods from first form
 	 * //this method saves message for current locale, so if locale is changed with setLocale, all messages set from scripting will be lost
-	 * 
+	 *
 	 * i18n.setI18NMessage('mykey.username.text','my_name')
 	 *
 	 * @param i18nKey The message key
@@ -342,9 +342,9 @@ public class JSI18N implements IJSI18N
 	 * Call this if you want to add a filter for a column (created by you) in the i18n table.
 	 * So that you can have multiple default values and multiple values per locale for one key.
 	 * // NOTE: this function is only working on runtime clients when the i18n values are read from the database
-	 * 
+	 *
 	 * @deprecated use table filters on the i18n table using databaseManager.addTableFilterParam
-	 * 
+	 *
 	 * @sample
 	 * // Puts i18n in filter mode - this allows you to have multiple default/per locale
 	 * // values for one key and to use one of them based on the filter parameters.
@@ -353,18 +353,18 @@ public class JSI18N implements IJSI18N
 	 * // For example you have 2 rows in you table for key X, language EN, different values and different "message_variant" (let's say 1 and 2)
 	 * // If you want the solution to use the first variant, you will have to call:
 	 * i18n.setI18NMessagesFilter('message_variant', '1')
-	 * 
+	 *
 	 * // Using filter fall back : if there is no key for 'message_variant' with value '1', it will
 	 * // search for value '2'
 	 * var filterValue = new Array();
 	 * filterValue[0] = '1';
 	 * filterValue[1] = '2';
 	 * i18n.setI18NMessagesFilter('message_variant', filterValue)
-	 * 
+	 *
 	 * // ATTENTION: if you use setI18NMessagesFilter(...) it is not recommended to use the i18n Dialog (especially before the filter is applied through JS).
 	 * @param columnName The column name that is the filter column in the i18n table.
 	 *
-	 * @param value The filter value.  
+	 * @param value The filter value.
 	 */
 	@Deprecated
 	public void js_setI18NMessagesFilter(String columnName, String value)
@@ -380,12 +380,12 @@ public class JSI18N implements IJSI18N
 	 *
 	 * @deprecated use table filters on the i18n table using databaseManager.addTableFilterParam, for example: </br>
 	 * databaseManager.addTableFilterParam('database', 'your_i18n_table', 'message_variant', 'in', [1, 2])
-	 * 
+	 *
 	 * @sampleas js_setI18NMessagesFilter(String, String)
 	 *
 	 * @param columnName The column name that is the filter column in the i18n table.
 	 *
-	 * @param value The filter value.  
+	 * @param value The filter value.
 	 */
 	@Deprecated
 	public void js_setI18NMessagesFilter(String columnName, String[] value)
@@ -394,7 +394,7 @@ public class JSI18N implements IJSI18N
 	}
 
 	/**
-	 * Returns a dataset with rows that contains 3 columns: 'key' (i18n key), 'reference' (reference text for that key) and 'locale ([CURRENT_LOCALE])' (where [CURRENT_LOCALE] is the current language) - with the system messages of servoy. 
+	 * Returns a dataset with rows that contains 3 columns: 'key' (i18n key), 'reference' (reference text for that key) and 'locale ([CURRENT_LOCALE])' (where [CURRENT_LOCALE] is the current language) - with the system messages of servoy.
 	 * This means all servoy messages, with all available translations.
 	 *
 	 * @sample
@@ -403,7 +403,7 @@ public class JSI18N implements IJSI18N
 	 * {
 	 * 	application.output(set.getValue(i, 1) + " " + set.getValue(i, 2)+ " " + set.getValue(i, 3));
 	 * }
-	 * 
+	 *
 	 * @return a JSDataSet with all the system messages.
 	 */
 	@JSFunction
@@ -463,13 +463,13 @@ public class JSI18N implements IJSI18N
 	/**
 	 * Sets the current time zone of the client.
 	 * The parameter should be a string having the format which can be retrieved via i18n.getAvailableTimeZones or
-	 * can be seen in the SmartClient Edit -> Preferences -> Locale at the "Default Timezone" combobox. 
-	 * For instance the time zone for Netherlands is set using the ID "Europe/Amsterdam". 
+	 * can be seen in the SmartClient Edit -> Preferences -> Locale at the "Default Timezone" combobox.
+	 * For instance the time zone for Netherlands is set using the ID "Europe/Amsterdam".
 	 *
 	 * @sample
 	 * // This will set the default time zone to Central European Time
 	 * i18n.setTimeZone("Europe/Amsterdam");
-	 * 
+	 *
 	 * @param timezone the client's time zone
 	 */
 	@JSFunction
@@ -481,9 +481,9 @@ public class JSI18N implements IJSI18N
 
 	/**
 	 * Get the list of available time zones.
-	 * 
+	 *
 	 * @sample
-	 * //Get the list of available time zones 
+	 * //Get the list of available time zones
 	 * var timezones = i18n.getAvailableTimeZoneIDs();
 	 */
 	@JSFunction
