3cfe489aa1aadfdc4a673d729bd5a3ba0c2d6814
georgeganea <georgeganea@gmail.com>
2015-02-25 15:40:27 +0200

    SVY-6002 added watch to change the style if formatting has text-transform. This is needed for uppercase/lowercase formatting

--- servoy_ngclient/war/servoydefault/textfield/textfield.js
+++ servoy_ngclient/war/servoydefault/textfield/textfield.js
@@ -11,11 +11,14 @@ angular.module('servoydefaultTextfield',['servoy']).directive('servoydefaultText
 			$scope.findMode = false;
 			$scope.style = {width:'100%',height:'100%',overflow:'hidden'}
 			
-			if ($scope.model.format["text-transform"])
-				$scope.style["text-transform"] = $scope.model.format["text-transform"];
-			
 			var storedTooltip = false;
 			// fill in the api defined in the spec file
+
+			$scope.$watch($attrs['svyFormat'], function(newVal, oldVal){
+				if (newVal && newVal["text-transform"])
+					$scope.style["text-transform"] = newVal["text-transform"];
+			});
+
 			$scope.api.onDataChangeCallback = function(event, returnval) {
 				var stringValue = typeof returnval == 'string'
 				if(!returnval || stringValue) {
