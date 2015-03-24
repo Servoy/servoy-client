cd7d463ec7768d59ce62f547de1f652275db2cc9
emera <emera@servoy.com>
2015-03-24 10:44:42 +0200

    SVY-7682 document the default components and services

--- servoy_ngclient/war/servoydefault/textarea/textarea.js
+++ servoy_ngclient/war/servoydefault/textarea/textarea.js
@@ -31,27 +31,80 @@ angular.module('servoydefaultTextarea',['servoy']).directive('servoydefaultTexta
 						storedTooltip = false;
 					}
 			}
+		   /**
+	    	* Request the focus to this textaarea.
+	    	* @example %%prefix%%%%elementName%%.requestFocus();
+	    	*/
 			$scope.api.requestFocus = function() { 
 				$element[0].focus()
 			}
 
-
+			/**
+	     	 * Sets the scroll location of an element. It takes as input the X (horizontal) and Y (vertical) coordinates - starting from the TOP LEFT side of the screen - only for an element where the height of the element is greater than the height of element content
+	     	 * NOTE: getScrollX() can be used with getScrollY() to return the current scroll location of an element; then use the X and Y coordinates with the setScroll function to set a new scroll location. 
+	     	 * For Example:
+	     	 * //returns the X and Y coordinates
+	     	 * var x = forms.company.elements.mytextarea.getScrollX();
+	     	 * var y = forms.company.elements.mytextarea.getScrollY();
+	    	 * //sets the new location
+	     	 * forms.company.elements.mytextarea.setScroll(x+10,y+10);
+	     	 * @example %%prefix%%%%elementName%%.setScroll(200,200);
+	     	 * @param x the X coordinate of the textarea scroll location in pixels
+	     	 * @param y the Y coordinate of the textarea scroll location in pixels
+	     	 */
 			$scope.api.setScroll = function(x, y) {
 				$element.scrollLeft(x);
 				$element.scrollTop(y);
 			}
 
+			 /**
+	    	  * Returns the x scroll location of specified element - only for an element where height of element is less than the height of element content. 
+	    	  * NOTE: getScrollX() can be used with getScrollY() to set the scroll location of an element using the setScroll function. 
+	    	  * For Example:
+	    	  * //returns the X and Y scroll coordinates
+	    	  * var x = forms.company.elements.mytextarea.getScrollX();
+	    	  * var y = forms.company.elements.mytextarea.getScrollY(); 
+	    	  * //sets the new scroll location
+	    	  * forms.company.elements.mytextarea.setScroll(x+10,y+10);
+	    	  * @example var x = %%prefix%%%%elementName%%.getScrollX();
+	     	  * @return The x scroll location in pixels.
+	     	  */
 			$scope.api.getScrollX = function() {
 				return $element.scrollLeft();
 			}
 
+			/**
+		     * Returns the y scroll location of specified element - only for an element where height of element is less than the height of element content.
+	         * NOTE: getScrollY() can be used with getScrollX() to set the scroll location of an element using the setScroll function. For Example:
+		     * //returns the X and Y scroll coordinates
+		     * var x = forms.company.elements.mytextarea.getScrollX();
+		     * var y = forms.company.elements..getScmytextarearollY();
+		     * //sets the new scroll location
+		     * forms.company.elements.mytextarea.setScroll(x+10,y+10);
+		     * @example var y = %%prefix%%%%elementName%%.getScrollY(); 
+		     * @return The y scroll location in pixels.
+		     */
 			$scope.api.getScrollY = function() {
 				return $element.scrollTop();
 			}
 
+			/**
+			 * Returns the currently selected text in the specified text area. 
+			 * @example var my_text = %%prefix%%%%elementName%%.getSelectedText();
+			 * @return {String} The selected text from the component.
+			 */
 			$scope.api.getSelectedText = $apifunctions.getSelectedText($element[0]);
 			$scope.api.setSelection = $apifunctions.setSelection($element[0]);
+			/**
+			 * Replaces the selected text; if no text has been selected, the replaced value will be inserted at the last cursor position.
+			 * @example %%prefix%%%%elementName%%.replaceSelectedText('John');
+			 * @param s The replacement text.
+			 */
 			$scope.api.replaceSelectedText = $apifunctions.replaceSelectedText($element[0]);
+			/**
+			 * Selects all the contents of the textarea.
+			 * @example %%prefix%%%%elementName%%.selectAll();
+			 */
 			$scope.api.selectAll = $apifunctions.selectAll($element[0]);
 
 		},
--- servoy_ngclient/war/servoydefault/textfield/textfield.js
+++ servoy_ngclient/war/servoydefault/textfield/textfield.js
@@ -36,15 +36,47 @@ angular.module('servoydefaultTextfield',['servoy']).directive('servoydefaultText
 						storedTooltip = false;
 					}
 			}
+			/**
+	    	* Request the focus to this textaarea.
+	    	* @example %%prefix%%%%elementName%%.requestFocus();
+	    	*/
 			$scope.api.requestFocus = function() { 
 				$element[0].focus()
 			}
 
+			/**
+			 * Returns the currently selected text in the specified text field. 
+			 * @example var my_text = %%prefix%%%%elementName%%.getSelectedText();
+			 * @return {String} The selected text in the text field.
+			 */
 			$scope.api.getSelectedText = $apifunctions.getSelectedText($element[0]);
 			$scope.api.setSelection = $apifunctions.setSelection($element[0]);
+			/**
+			 * Replaces the selected text; if no text has been selected, the replaced value will be inserted at the last cursor position.
+			 * @example %%prefix%%%%elementName%%.replaceSelectedText('John');
+			 * @param s The replacement text.
+			 */
 			$scope.api.replaceSelectedText = $apifunctions.replaceSelectedText($element[0]);
+			/**
+			 * Selects all the contents of the text field.
+			 * @example %%prefix%%%%elementName%%.selectAll();
+			 */
 			$scope.api.selectAll = $apifunctions.selectAll($element[0]);
 
+			/**
+	         * Sets the display/real values to the custom valuelist of the element (if element has custom valuelist).
+	         * This does not affect the value list with same name list on other elements or value lists at application level.
+	         * Should receive a dataset parameter, first column is for display values, second column (optional) is for real values.
+	         * @example
+	         * var dataset = databaseManager.createEmptyDataSet(0,new Array('display_values','optional_real_values'));
+	         * dataset.addRow(['aa',1]);
+	         * dataset.addRow(['bb',2]);
+	         * dataset.addRow(['cc',3]);
+	         * // %%prefix%%%%elementName%% should have a valuelist attached
+	         * %%prefix%%%%elementName%%.setValueListItems(dataset);
+	         *
+	         * @param value first column is display value, second column is real value
+	         */
 			$scope.api.setValueListItems = function(values) 
 			{
 				var valuelistItems = [];
