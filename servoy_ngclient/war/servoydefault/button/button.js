09cefd97dda4202e0409f9e32442f57d7c853396
emera <emera@servoy.com>
2015-03-25 10:14:03 +0200

    SVY-8036 requestFocus not implemented for some elements in NG

--- servoy_ngclient/war/servoydefault/button/button.js
+++ servoy_ngclient/war/servoydefault/button/button.js
@@ -3,11 +3,20 @@ angular.module('servoydefaultButton',['servoy']).directive('servoydefaultButton'
       restrict: 'E',
       scope: {
        	model: "=svyModel",
-       	handlers: "=svyHandlers"
+       	handlers: "=svyHandlers",
+        api: "=svyApi"
       },
       controller: function($scope, $element, $attrs) {
     	  $scope.containerstyle = {overflow:'hidden',position:'absolute'}
           $scope.contentstyle = {width:'100%',overflow:'hidden',position:'relative',whiteSpace:'nowrap'}
+
+    	  /**
+    	   * Set the focus to this button.
+    	   * @example %%prefix%%%%elementName%%.requestFocus();
+    	   */
+    	  $scope.api.requestFocus = function() { 
+    		  $element.find('button')[0].focus();
+    	  }
       },
       templateUrl: 'servoydefault/button/button.html'
     };
