49f731069a37ded8cc93d8dae0dac370654e2741
jcompagner <jcompagner@servoy.com>
2015-04-23 17:06:57 +0200

    SVY-7524 Foundset property's foundset available in Rhino scripting + correct separate foundset usage
    added foundset and dataproviders as properties of a foundset type
    foundset can be set and the dataproviders can be set as a whole or per
    item

--- servoy_ngclient/src/com/servoy/j2db/server/ngclient/property/FoundsetPropertyType.java
+++ servoy_ngclient/src/com/servoy/j2db/server/ngclient/property/FoundsetPropertyType.java
@@ -31,6 +31,8 @@ import org.sablo.websocket.utils.DataConversion;
 import org.sablo.websocket.utils.JSONUtils;
 
 import com.servoy.j2db.FlattenedSolution;
+import com.servoy.j2db.dataprocessing.IFoundSetInternal;
+import com.servoy.j2db.scripting.DefaultScope;
 import com.servoy.j2db.server.ngclient.DataAdapterList;
 import com.servoy.j2db.server.ngclient.FormElement;
 import com.servoy.j2db.server.ngclient.FormElementContext;
@@ -50,7 +52,6 @@ public class FoundsetPropertyType extends CustomJSONPropertyType<FoundsetTypeSab
 	IConvertedPropertyType<FoundsetTypeSabloValue>, ISabloComponentToRhino<FoundsetTypeSabloValue>, ISupportsGranularUpdates<FoundsetTypeSabloValue>,
 	IDataLinkedType<JSONObject, FoundsetTypeSabloValue>
 {
-
 	public static final FoundsetPropertyType INSTANCE = new FoundsetPropertyType(null);
 
 	public static final String TYPE_NAME = "foundset";
@@ -134,7 +135,7 @@ public class FoundsetPropertyType extends CustomJSONPropertyType<FoundsetTypeSab
 	@Override
 	public Object toRhinoValue(FoundsetTypeSabloValue webComponentValue, PropertyDescription pd, BaseWebObject componentOrService, Scriptable startScriptable)
 	{
-		return Scriptable.NOT_FOUND;
+		return new FoundsetTypeSableValueWrapper(startScriptable, webComponentValue, pd);
 	}
 
 	@Override
@@ -142,5 +143,107 @@ public class FoundsetPropertyType extends CustomJSONPropertyType<FoundsetTypeSab
 	{
 		return TargetDataLinks.LINKED_TO_ALL; // if you change this you should call this method in FoundsetTypeSabloValue.attach as well when registering as listener to DAL
 	}
+
+
+	/**
+	 * @author jcompagner
+	 */
+	private final class FoundsetTypeSableValueWrapper extends DefaultScope
+	{
+		private final FoundsetTypeSabloValue webComponentValue;
+		private final PropertyDescription pd;
+
+		/**
+		 * @param parent
+		 * @param webComponentValue
+		 * @param pd
+		 */
+		private FoundsetTypeSableValueWrapper(Scriptable parent, FoundsetTypeSabloValue webComponentValue, PropertyDescription pd)
+		{
+			super(parent);
+			this.webComponentValue = webComponentValue;
+			this.pd = pd;
+		}
+
+		@Override
+		public Object get(String name, Scriptable start)
+		{
+			switch (name)
+			{
+				case "foundset" :
+				{
+					return webComponentValue.foundset;
+				}
+				case "dataproviders" :
+				{
+					if (webComponentValue.elementsToDataproviders.size() > 0)
+					{
+						return "Foundset bound to a components with dataproviders, can't be set through the foundset property";
+					}
+					DefaultScope dataproviders = new DefaultScope(start)
+					{
+						@Override
+						public Object get(String nm, Scriptable strt)
+						{
+							return webComponentValue.dataproviders.get(nm);
+						}
+
+						@Override
+						public void put(String nm, Scriptable strt, Object value)
+						{
+							webComponentValue.dataproviders.put(nm, (String)value);
+							webComponentValue.changeMonitor.allChanged();
+						}
+
+						@Override
+						public Object[] getIds()
+						{
+							return webComponentValue.dataproviders.keySet().toArray();
+						};
+					};
+					return dataproviders;
+				}
+			}
+			return Scriptable.NOT_FOUND;
+		}
+
+		@Override
+		public void put(String name, Scriptable start, Object value)
+		{
+			switch (name)
+			{
+				case "foundset" :
+				{
+					if (value instanceof IFoundSetInternal) webComponentValue.foundset = (IFoundSetInternal)value;
+					else throw new RuntimeException("illegal value '" + value + "' to set on the foundset property " + pd.getName());
+				}
+				case "dataproviders" :
+				{
+					if (webComponentValue.elementsToDataproviders.size() > 0)
+					{
+						throw new RuntimeException("Foundset bound to a components with dataproviders, can't be set through the foundset property");
+					}
+					if (value instanceof Scriptable)
+					{
+						webComponentValue.dataproviders.clear();
+						Object[] ids = ((Scriptable)value).getIds();
+						for (Object id : ids)
+						{
+							webComponentValue.dataproviders.put((String)id, (String)((Scriptable)value).get((String)id, ((Scriptable)value)));
+						}
+						webComponentValue.changeMonitor.allChanged();
+					}
+					else throw new RuntimeException("illegal value '" + value + "' to set on the dataprovides property " + pd.getName());
+				}
+			}
+		}
+
+		@Override
+		public Object[] getIds()
+		{
+			return new Object[] { "foundset", "dataproviders" };
+		}
+	}
+
 
 }
