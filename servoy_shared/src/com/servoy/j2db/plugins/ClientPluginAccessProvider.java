d128f55c36d34608823efc2e6a099f7e99c10f4d
lvostinar@servoy.com <lvostinar@servoy.com>
2015-05-07 09:23:13 +0300

    SVY-8203 Timer on Data Change

--- servoy_shared/src/com/servoy/j2db/plugins/ClientPluginAccessProvider.java
+++ servoy_shared/src/com/servoy/j2db/plugins/ClientPluginAccessProvider.java
@@ -592,7 +592,7 @@ public class ClientPluginAccessProvider implements IClientPluginAccess
 									Debug.error(ex);
 									application.handleException(application.getI18NMessage("servoy.formPanel.error.formData"), ex); //$NON-NLS-1$
 								}
-								retval = fp.executeFunction(methodname, arguments, true, null, true, null, true, false, true);
+								retval = fp.executeFunction(methodname, arguments, stopUIEditing, null, true, null, true, false, true);
 							}
 							else
 							{
@@ -668,7 +668,7 @@ public class ClientPluginAccessProvider implements IClientPluginAccess
 
 	/*
 	 * (non-Javadoc)
-	 * 
+	 *
 	 * @see com.servoy.j2db.plugins.IClientPluginAccess#getStyleSheet(java.lang.String)
 	 */
 	public IStyleSheet getStyleSheet(String name)
@@ -685,7 +685,7 @@ public class ClientPluginAccessProvider implements IClientPluginAccess
 
 	/*
 	 * (non-Javadoc)
-	 * 
+	 *
 	 * @see com.servoy.j2db.plugins.IClientPluginAccess#getRuntimeWindow(java.lang.String)
 	 */
 	public IRuntimeWindow getRuntimeWindow(String name)
@@ -695,7 +695,7 @@ public class ClientPluginAccessProvider implements IClientPluginAccess
 
 	/*
 	 * (non-Javadoc)
-	 * 
+	 *
 	 * @see com.servoy.j2db.plugins.IClientPluginAccess#getCurrentRuntimeWindow()
 	 */
 	public IRuntimeWindow getCurrentRuntimeWindow()
@@ -710,7 +710,7 @@ public class ClientPluginAccessProvider implements IClientPluginAccess
 
 	/*
 	 * (non-Javadoc)
-	 * 
+	 *
 	 * @see com.servoy.j2db.plugins.IClientPluginAccess#setValueListItems(java.lang.String, java.lang.Object[], java.lang.Object[])
 	 */
 	public void setValueListItems(String name, Object[] displayValues, Object[] realValues)
@@ -720,7 +720,7 @@ public class ClientPluginAccessProvider implements IClientPluginAccess
 
 	/*
 	 * (non-Javadoc)
-	 * 
+	 *
 	 * @see com.servoy.j2db.plugins.IClientPluginAccess#setValueListItems(java.lang.String, java.lang.Object[])
 	 */
 	public void setValueListItems(String name, Object[] displayValues)
