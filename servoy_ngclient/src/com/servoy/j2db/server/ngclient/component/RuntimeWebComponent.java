8950a550b2fa211e9baa74cdca8f1fe79e0e7a5e
Gabi Boros <gboros@servoy.com>
2015-05-12 13:02:33 +0300

    SVY-7236 re-added optimization of api call for bounds

--- servoy_ngclient/src/com/servoy/j2db/server/ngclient/component/RuntimeWebComponent.java
+++ servoy_ngclient/src/com/servoy/j2db/server/ngclient/component/RuntimeWebComponent.java
@@ -34,8 +34,11 @@ import org.sablo.specification.WebComponentApiDefinition;
 import org.sablo.specification.WebComponentSpecification;
 import org.sablo.specification.property.IPropertyType;
 
+import com.servoy.j2db.FormController;
+import com.servoy.j2db.persistence.ISupportAnchors;
 import com.servoy.j2db.scripting.IInstanceOf;
 import com.servoy.j2db.server.ngclient.ComponentFactory;
+import com.servoy.j2db.server.ngclient.FormElement;
 import com.servoy.j2db.server.ngclient.IWebFormUI;
 import com.servoy.j2db.server.ngclient.WebFormComponent;
 import com.servoy.j2db.server.ngclient.property.types.NGConversions;
@@ -74,17 +77,20 @@ public class RuntimeWebComponent implements Scriptable, IInstanceOf
 		{
 			for (WebComponentApiDefinition def : webComponentSpec.getApiFunctions().values())
 			{
-				Function func = null;
-				if (apiObject != null)
+				if (isApiFunctionEnabled(def.getName()))
 				{
-					Object serverSideFunction = apiObject.get(def.getName(), apiObject);
-					if (serverSideFunction instanceof Function)
+					Function func = null;
+					if (apiObject != null)
 					{
-						func = (Function)serverSideFunction;
+						Object serverSideFunction = apiObject.get(def.getName(), apiObject);
+						if (serverSideFunction instanceof Function)
+						{
+							func = (Function)serverSideFunction;
+						}
 					}
+					if (func != null) apiFunctions.put(def.getName(), func);
+					else apiFunctions.put(def.getName(), new WebComponentFunction(component, def));
 				}
-				if (func != null) apiFunctions.put(def.getName(), func);
-				else apiFunctions.put(def.getName(), new WebComponentFunction(component, def));
 			}
 			Map<String, PropertyDescription> specs = webComponentSpec.getProperties();
 			for (String propName : specs.keySet())
@@ -97,6 +103,21 @@ public class RuntimeWebComponent implements Scriptable, IInstanceOf
 				}
 			}
 		}
+	}
+
+	protected boolean isApiFunctionEnabled(String functionName)
+	{
+		FormElement fe = component.getFormElement();
+		if (fe.isLegacy() && fe.getPersistIfAvailable() instanceof ISupportAnchors)
+		{
+			int anchor = ((ISupportAnchors)fe.getPersistIfAvailable()).getAnchors();
+			if ((anchor == 0 || (fe.getForm().getView() == FormController.TABLE_VIEW || fe.getForm().getView() == FormController.LOCKED_TABLE_VIEW)) &&
+				(("getLocationX").equals(functionName) || ("getLocationY").equals(functionName) || ("getWidth").equals(functionName) || ("getHeight").equals(functionName)))
+			{
+				return false;
+			}
+		}
+		return true;
 	}
 
 	@Override
