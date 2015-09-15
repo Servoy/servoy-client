6b34e5278c9d1797c52bb9ef368a848155130ff9
Gabi Boros <gboros@servoy.com>
2015-09-15 08:25:56 +0300

    SVY-7715 allow load records by query for querys that starts with 'declare'

--- servoy_shared/src/com/servoy/j2db/dataprocessing/SQLGenerator.java
+++ servoy_shared/src/com/servoy/j2db/dataprocessing/SQLGenerator.java
@@ -1325,7 +1325,7 @@ public class SQLGenerator
 	{
 		if (value != null)
 		{
-			String lc = value.toLowerCase();
+			String lc = value.toLowerCase().trim();
 			return lc.startsWith("select") || lc.startsWith("with") || lc.startsWith("declare"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
 		}
 		return false;
