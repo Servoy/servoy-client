98b5e38b19c95ae765273dae818c3b634912e07a
emera <emera@servoy.com>
2015-11-16 13:27:41 +0200

    SVY-8856 spike: can we have the actual labelFor component in the table header? (remove the "null" string check)

--- servoy_ngclient/src/com/servoy/j2db/server/ngclient/property/ComponentTypeSabloValue.java
+++ servoy_ngclient/src/com/servoy/j2db/server/ngclient/property/ComponentTypeSabloValue.java
@@ -312,8 +312,8 @@ public class ComponentTypeSabloValue implements ISmartPropertyValue
 
 		if (foundsetPropValue != null)
 		{
-			viewPortChangeMonitor = new ViewportDataChangeMonitor(monitor, new ComponentViewportRowDataProvider((FoundsetDataAdapterList)dal, childComponent,
-				recordBasedProperties, this));
+			viewPortChangeMonitor = new ViewportDataChangeMonitor(monitor,
+				new ComponentViewportRowDataProvider((FoundsetDataAdapterList)dal, childComponent, recordBasedProperties, this));
 			foundsetPropValue.addViewportDataChangeMonitor(viewPortChangeMonitor);
 			setDataproviderNameToFoundset();
 		}
@@ -470,7 +470,8 @@ public class ComponentTypeSabloValue implements ISmartPropertyValue
 		removeRecordDependentProperties(changes);
 
 		boolean modelChanged = (changes.content.size() > 0);
-		boolean viewPortChanged = (forFoundsetTypedPropertyName != null && (viewPortChangeMonitor.shouldSendWholeViewport() || viewPortChangeMonitor.getViewPortChanges().size() > 0));
+		boolean viewPortChanged = (forFoundsetTypedPropertyName != null &&
+			(viewPortChangeMonitor.shouldSendWholeViewport() || viewPortChangeMonitor.getViewPortChanges().size() > 0));
 
 		destinationJSON.object();
 		if (modelChanged || viewPortChanged)
@@ -677,8 +678,8 @@ public class ComponentTypeSabloValue implements ISmartPropertyValue
 						boolean selectionOk = true;
 						if (update.has("rowId"))
 						{
-							String rowId = update.getString("rowId");
-							if (!"null".equals(rowId))
+							String rowId = update.optString("rowId");
+							if (rowId != null)
 							{
 								FoundsetTypeSabloValue foundsetValue = getFoundsetValue();
 								if (foundsetValue != null)
@@ -864,8 +865,8 @@ public class ComponentTypeSabloValue implements ISmartPropertyValue
 				}
 				catch (JSONException e)
 				{
-					Debug.error("Setting value for record dependent property '" + propertyName + "' in foundset linked component to value: " + value +
-						" failed.", e);
+					Debug.error(
+						"Setting value for record dependent property '" + propertyName + "' in foundset linked component to value: " + value + " failed.", e);
 				}
 				finally
 				{
