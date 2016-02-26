8cd2e9035e26c6a2c7c7f0ff867e4e32c5bb5d4d
Rob Gansevles <rgansevles@gmail.com>
2016-02-26 11:30:12 +0100

    SVY-9210 added getOpenSolutionName() to JSClientInformation

--- servoy_shared/src/com/servoy/j2db/server/shared/IClientInformation.java
+++ servoy_shared/src/com/servoy/j2db/server/shared/IClientInformation.java
@@ -20,71 +20,78 @@ import java.util.Date;
 
 /**
  * Interface to receive some client information at the server side
- * 
+ *
  * @author gerze
  */
 public interface IClientInformation extends com.servoy.j2db.server.IClientInformation
 {
 	/**
 	 * Gets the clientId of this client.
-	 * 
+	 *
 	 * @return A String which holds the id of the client.
 	 */
 	String getClientID();
 
 	/**
 	 * Gets the identified of the host computer where this client has connected from.
-	 * 
+	 *
 	 * @return A String which holds the identifier of the host computer.
 	 */
 	String getHostIdentifier();
 
 	/**
 	 * Gets the name of the host computer where this client has connected from.
-	 * 
+	 *
 	 * @return A String which holds the name of the host computer.
 	 */
 	String getHostName();
 
 	/**
 	 * Gets the address of the host computer where this client has connected from.
-	 * 
+	 *
 	 * @return A String which holds the address of the host computer.
 	 */
 	String getHostAddress();
 
 	/**
 	 * Gets the type of the application that was started by this client (smart client, web client, ...).
-	 * 
+	 *
 	 * @return An int which encodes the application type.
 	 */
 	int getApplicationType();
 
 	/**
 	 * Gets the uid of the user who logged in at this client.
-	 * 
+	 *
 	 * @return A String holding the uid of the user. If the solution that is running on this client does not require a login, then null is returned.
 	 */
 	String getUserUID();
 
 	/**
 	 * Gets the name of the user who logged in at this client.
-	 * 
+	 *
 	 * @return A String holding the name of the user. If the solution that is running on the client does not require a login, then null is returned.
 	 */
 	String getUserName();
 
 	/**
 	 * Gets the time and date this client logged in.
-	 * 
+	 *
 	 * @return A Date when the client logged in. If the solution that is running on the client does not require a login, then null is returned.
 	 */
 	Date getLoginTime();
 
 	/**
 	 * Gets the time and date since this client has been idle.
-	 * 
+	 *
 	 * @return A Date representing since when the client is idle. If the solution that is running on the client does not require a login, then null is returned.
 	 */
 	Date getIdleTime();
+
+	/**
+	 * Gets the name of the solution that is currently open by the client.
+	 *
+	 * @return solution name, or null when no solution is open.
+	 */
+	String getOpenSolutionName();
 }
