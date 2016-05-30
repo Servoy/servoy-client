48518a664e9e83f7c7f133faeced628a5cb84497
Johan Compagner <jcompagner@servoy.com>
2016-05-30 16:40:57 +0200

    SVY-9921 Issue with node components in resource project SVY-9892 Exported component with different package name fails to load
    name of a package reader should default to package name (when service
    resources the package name is in the url, not the file name of the zip)

--- servoy_ngclient/src/com/servoy/j2db/server/ngclient/startup/resourceprovider/ResourceProvider.java
+++ servoy_ngclient/src/com/servoy/j2db/server/ngclient/startup/resourceprovider/ResourceProvider.java
@@ -38,6 +38,7 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.jar.Attributes;
 import java.util.jar.JarFile;
 import java.util.jar.Manifest;
 import java.util.zip.ZipEntry;
@@ -87,6 +88,7 @@ public class ResourceProvider implements Filter
 
 	private static String getName(IPackageReader reader)
 	{
+		if (reader.getPackageName() != null) return reader.getPackageName();
 		String name = reader.getName();
 		int index = name.lastIndexOf('/');
 		if (index == -1) index = name.lastIndexOf('\\');
