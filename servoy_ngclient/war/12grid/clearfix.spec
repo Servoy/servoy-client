4f85930a21257353ef2389e7d4cca5d30289e594
Johan Compagner <jcompagner@servoy.com>
2016-06-03 14:41:46 +0200

    SVY-9931 extract 12grid and bootstrap components to be installable in with WPM
    deleted bootstrap and 12 grid packages from the war

--- servoy_ngclient/war/12grid/clearfix.spec
+++
@@ -1,9 +0,0 @@
-{
-	"name": "clearfix",
-	"displayName": "ClearFix",
-	"version": 1,
-	"definition": "clearfix.json",
-	"categoryName":"Templates",
-	"contains": [],
-	"topContainer": false
-}
--- servoy_ngclient/war/12grid/lib/2screens.js
+++
@@ -1,4 +0,0 @@
-function screenToggle(element) {
-		$(element.parentElement.parentElement.firstElementChild).toggleClass('in');
-		$(element.parentElement.parentElement.lastElementChild).toggleClass('in');
-}
