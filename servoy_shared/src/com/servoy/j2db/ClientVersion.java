7cb3460636f008d84ca70407dc669e4774ca1c5d
servoytest <test@servoy.com>
2016-08-10 17:30:28 +0200

    version bumb to 3027 (8.1)

--- servoy_shared/src/com/servoy/j2db/ClientVersion.java
+++ servoy_shared/src/com/servoy/j2db/ClientVersion.java
@@ -29,8 +29,8 @@ public abstract class ClientVersion
 	private static final int majorVersion = 8;
 	private static final int middleVersion = 1;
 	private static final int minorVersion = 0;
-	private static final int releaseNumber = 3026;
-	private static final String versionPostfix = "rc1";
+	private static final int releaseNumber = 3027;
+	private static final String versionPostfix = "";
 
 	// make sure you keep this the same format, or make it work with extensions version comparing & xml schema
 	private static final String version = majorVersion + "." + middleVersion + "." + minorVersion + (versionPostfix != null ? " " + versionPostfix : "");
