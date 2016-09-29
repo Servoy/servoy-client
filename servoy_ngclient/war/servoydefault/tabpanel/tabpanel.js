df0ee770137eb2f9815db6e9b574f8dac697f448
Johan Compagner <jcompagner@servoy.com>
2016-09-29 22:43:02 +0200

    SVY-10186 SPIKE: improve lazy loading of component's controller SVY-9947 Selected tab in tabpanel is not persisted if the tabs were not set at design time.
    reverted partly the changes to wait for data, because now the data is
    always there directly through the template
    webStorage is because of this not needed anymore

--- servoy_ngclient/war/servoydefault/tabpanel/tabpanel.js
+++ servoy_ngclient/war/servoydefault/tabpanel/tabpanel.js
@@ -26,20 +26,17 @@ angular.module('servoydefaultTabpanel',['servoy']).directive('servoydefaultTabpa
 			}
 			$scope.bgstyle = {}
 			$scope.waitingForServerVisibility = {}
-			$scope.realTabs = [];
 
-		     // if the selected tab is already set then this could be a cached value of the previous state
-			// we need to reinitialize it with the correct tabIndex that should be coming from the server.
 			if ($scope.model.selectedTab) {
+			     // if the selected tab is already set then this is a reload of the form and we need to call formWillShow
 				delete $scope.model.selectedTab;
 			}
 			
 			function refresh() {
-				// if svyMarkupId is not set then the data is not here yet, wait for the tabIndex to come from the server.
-				if (! $scope.model.svyMarkupId) return;
 				var i = 0;
 				var realTabIndex = 1;
 				if($scope.model.tabIndex == undefined) $scope.model.tabIndex = 1; // default it is 1
+				
 				if (typeof $scope.model.tabIndex === 'number')
 					realTabIndex = $scope.model.tabIndex - 1;
 				else if (!isNaN(parseInt($scope.model.tabIndex)) && parseInt($scope.model.tabIndex) > 0)
@@ -54,7 +51,7 @@ angular.module('servoydefaultTabpanel',['servoy']).directive('servoydefaultTabpa
 							break;
 						}
 					}
-				
+			    
 				if ($scope.model.tabs){
 					var selectedTabNotFound = true;
 					
@@ -79,21 +76,15 @@ angular.module('servoydefaultTabpanel',['servoy']).directive('servoydefaultTabpa
 				}
 			}
 
-			// wait for the svyMarkupId to be set (then the form data from the server iis there. only from that one work with the tabs)
-			var unreg = $scope.$watch("model.svyMarkupId", function(newVal) {
-				if (!newVal) return;
-				unreg();
-				$scope.$watch("model.tabIndex", function(newValue) {
-					if ($log.debugEnabled) $log.debug("svy * model.tabIndex = " + $scope.model.tabIndex + " -- " + new Date().getTime());
-					refresh();
-				});
-	
-				$scope.$watch("model.tabs", function(newValue) {
-					$scope.realTabs = $scope.model.tabs;
-					if ($log.debugEnabled) $log.debug("svy * model.tabs reference updated; length = " + ($scope.model.tabs ? $scope.model.tabs.length : undefined) + " -- " + new Date().getTime());
-					refresh();
-				});   
+			$scope.$watch("model.tabIndex", function(newValue) {
+				if ($log.debugEnabled) $log.debug("svy * model.tabIndex = " + $scope.model.tabIndex + " -- " + new Date().getTime());
+				refresh();
 			});
+
+			$scope.$watch("model.tabs", function(newValue) {
+				if ($log.debugEnabled) $log.debug("svy * model.tabs reference updated; length = " + ($scope.model.tabs ? $scope.model.tabs.length : undefined) + " -- " + new Date().getTime());
+				refresh();
+			});        
 
 			$scope.$watch("model.visible", function(newValue,oldValue) {
 	    	  		if ($scope.model.selectedTab && newValue !== oldValue && $scope.model.selectedTab.containsFormId)
@@ -478,8 +469,3 @@ angular.module('servoydefaultTabpanel',['servoy']).directive('servoydefaultTabpa
 		template: "<div style='height:100%;width:100%;position:absolute;' svy-border='model.borderType'svy-font='model.fontType'><div ng-include='getTemplateUrl()'></div></div>"
 	};
 })
-
-
-
-
-
