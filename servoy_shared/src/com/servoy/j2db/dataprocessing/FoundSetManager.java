0ff9f9e04a95b78fef7fa32b8df1d3d239d1d3c8
Rob Gansevles <rgansevles@gmail.com>
2016-12-16 15:41:03 +0100

    SVY-10056 foundset.loadAllRecords causes resorting of relations.
    Do not invalidate related foundsets during execution of current script,
    selected record may not be stable

--- servoy_shared/src/com/servoy/j2db/dataprocessing/FoundSetManager.java
+++ servoy_shared/src/com/servoy/j2db/dataprocessing/FoundSetManager.java
@@ -473,7 +473,7 @@ public class FoundSetManager implements IFoundSetManagerInternal
 			Map.Entry<String, SoftReference<RelatedFoundSet>>[] array = hm.entrySet().toArray(EMPTY_ENTRY_ARRAY);
 			for (Map.Entry<String, SoftReference<RelatedFoundSet>> entry : array)
 			{
-				SoftReference<RelatedFoundSet> sr = entry.getValue();
+				final SoftReference<RelatedFoundSet> sr = entry.getValue();
 				RelatedFoundSet element = sr.get();
 				if (element != null)
 				{
@@ -484,7 +484,19 @@ public class FoundSetManager implements IFoundSetManagerInternal
 						{
 							if (!element.creationSqlSelect.equals(element.getSqlSelect()))
 							{
-								element.invalidateFoundset();
+								// Do not invalidate related foundsets during execution of current script, selected record may not be stable
+								getApplication().invokeLater(new Runnable()
+								{
+									@Override
+									public void run()
+									{
+										RelatedFoundSet rfs = sr.get();
+										if (rfs != null)
+										{
+											rfs.invalidateFoundset();
+										}
+									}
+								});
 							}
 						}
 						catch (Exception e)
