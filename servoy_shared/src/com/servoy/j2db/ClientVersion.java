99907ff42cc19235abe3bd79c5a1f8dc2ef66672
Johan Compagner <jcompagner@servoy.com>
2017-01-19 11:52:20 +0100

    version bumb 7410i1 2048

--- servoy_shared/src/com/servoy/j2db/ClientVersion.java
+++ servoy_shared/src/com/servoy/j2db/ClientVersion.java
@@ -28,9 +28,9 @@ public abstract class ClientVersion
 	// these fields are private intentionally, to prevent final class member copy in other classes! 
 	private static final int majorVersion = 7;
 	private static final int middleVersion = 4;
-	private static final int minorVersion = 9;
-	private static final int releaseNumber = 2047;
-	private static final String versionPostfix = "";
+	private static final int minorVersion = 10;
+	private static final int releaseNumber = 2048;
+	private static final String versionPostfix = "i1";
 
 	// make sure you keep this the same format, or make it work with extensions version comparing & xml schema
 	private static final String version = majorVersion + "." + middleVersion + "." + minorVersion + (versionPostfix != null ? " " + versionPostfix : "");
