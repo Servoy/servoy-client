1e0aa13787369a5cf06c9ee0fcd863a9acae3f24
Johan Compagner <jcompagner@servoy.com>
2017-05-16 10:52:25 +0200

    upped the version to 8.2.0.3100

--- servoy_shared/src/com/servoy/j2db/ClientVersion.java
+++ servoy_shared/src/com/servoy/j2db/ClientVersion.java
@@ -27,9 +27,9 @@ public abstract class ClientVersion
 {
 	// these fields are private intentionally, to prevent final class member copy in other classes!
 	private static final int majorVersion = 8;
-	private static final int middleVersion = 1;
-	private static final int minorVersion = 3;
-	private static final int releaseNumber = 3033;
+	private static final int middleVersion = 2;
+	private static final int minorVersion = 0;
+	private static final int releaseNumber = 3100;
 	private static final String versionPostfix = "";
 
 	// make sure you keep this the same format, or make it work with extensions version comparing & xml schema
