cf6642d052f653fe2ce40160d1e67f92ce92e8d7
emera <emera@servoy.com>
2017-05-30 13:23:58 +0300

    SVY-11195 Anchored elements in form component is forced to "TOP,RIGHT,LEFT"

--- servoy_ngclient/war/js/servoy_app.ts
+++ servoy_ngclient/war/js/servoy_app.ts
@@ -375,7 +375,7 @@ angular.module('servoyApp', ['sabloApp', 'servoy','webStorageModule','servoy-com
 
 					for (var beanName in beanDatas) {
 						// initialize with design data
-						if (formProperties.absoluteLayout) layout[beanName] = { position: 'absolute' }
+						if (formProperties.absoluteLayout || beanName.indexOf("$") !== -1 && beanDatas[beanName.slice(0,beanName.indexOf("$"))].absoluteLayout) layout[beanName] = { position: 'absolute' }
 
 						var newBeanConversionInfo = beanDatas[beanName][$sabloConverters.TYPES_KEY];
 						var beanConversionInfo = newBeanConversionInfo ? $sabloUtils.getOrCreateInDepthProperty($sabloApplication.getFormStatesConversionInfo(), formName, beanName) : $sabloUtils.getInDepthProperty($sabloApplication.getFormStatesConversionInfo(), formName, beanName);
