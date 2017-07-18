6443ec4b912e1449ec0cb201fdeae2b082db782a
lvostinar <lvostinar@servoy.com>
2017-07-18 12:14:58 +0300

    SVY-11320 Error in typeahead.js when entering find mode in table view

--- servoy_ngclient/war/servoydefault/typeahead/typeahead.js
+++ servoy_ngclient/war/servoydefault/typeahead/typeahead.js
@@ -77,9 +77,12 @@ angular.module('servoydefaultTypeahead', ['servoy'])
 					if(!found)
 					{
 						$scope.value = null;
-						$scope.model.valuelistID.getDisplayValue($scope.model.dataProviderID).then(function(displayValue) {
+						if (angular.isDefined($scope.model.valuelistID)) 
+						{
+							$scope.model.valuelistID.getDisplayValue($scope.model.dataProviderID).then(function(displayValue) {
 								$scope.value = displayValue;
-						});
+							});
+						}
 					}	
 				}	 
 			}
