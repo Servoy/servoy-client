7af48532b4b9ffb9fa3f77683e916dce6bdd7173
lvostinar <lvostinar@servoy.com>
2017-07-19 14:10:00 +0300

    SVY-11305 JSDoc specification for utils.dateFormat

--- servoy_shared/src/com/servoy/j2db/scripting/JSUtils.java
+++ servoy_shared/src/com/servoy/j2db/scripting/JSUtils.java
@@ -234,6 +234,7 @@ public class JSUtils implements IJSUtils
 
 	/**
 	 * Parse a string to a date object.
+	 * Format can be a string like: 'dd-MM-yyyy' , 'dd-MM-yyyy HH:mm' , 'MM/dd/yyyy', 'MM/dd/yyyy hh:mm aa', 'dd.MM.yyyy'
 	 *
 	 * @sample
 	 * var parsedDate = utils.parseDate(datestring,'EEE, d MMM yyyy HH:mm:ss');
@@ -280,6 +281,8 @@ public class JSUtils implements IJSUtils
 	 * This will format with the system timezone, so for web and ng clients it will use the server timezone to format
 	 * see {@link #dateFormat(Date, String, String)} for using the actual clients timezone.
 	 *
+	 * Format can be a string like: 'dd-MM-yyyy' , 'dd-MM-yyyy HH:mm' , 'MM/dd/yyyy', 'MM/dd/yyyy hh:mm aa', 'dd.MM.yyyy'
+	 *
 	 * @sample
 	 * var formattedDateString = utils.dateFormat(dateobject,'EEE, d MMM yyyy HH:mm:ss');
 	 *
@@ -298,6 +301,8 @@ public class JSUtils implements IJSUtils
 	 * Format a date object to a text representation using the format and timezone given.
 	 * If the timezone is not given the timezone of the client itself will be used.
 	 * see i18n.getAvailableTimeZoneIDs() to get a timezone string that can be used.
+	 *
+	 * Format can be a string like: 'dd-MM-yyyy' , 'dd-MM-yyyy HH:mm' , 'MM/dd/yyyy', 'MM/dd/yyyy hh:mm aa', 'dd.MM.yyyy'
 	 *
 	 * @sample
 	 * var formattedDateString = utils.dateFormat(dateobject,'EEE, d MMM yyyy HH:mm:ss', "UTC");
@@ -1190,9 +1195,9 @@ public class JSUtils implements IJSUtils
 	}
 
 	private final Character[] CONVERSIONS = new Character[] { Character.valueOf('b'), Character.valueOf('B'), Character.valueOf('h'), Character.valueOf('H'), //
-		Character.valueOf('s'), Character.valueOf('S'), Character.valueOf('c'), Character.valueOf('C'), Character.valueOf('d'), Character.valueOf('o'), //
-		Character.valueOf('x'), Character.valueOf('X'), Character.valueOf('e'), Character.valueOf('E'), Character.valueOf('g'), Character.valueOf('G'), //
-		Character.valueOf('a'), Character.valueOf('A'), Character.valueOf('t'), Character.valueOf('T'), Character.valueOf('n') };
+	Character.valueOf('s'), Character.valueOf('S'), Character.valueOf('c'), Character.valueOf('C'), Character.valueOf('d'), Character.valueOf('o'), //
+	Character.valueOf('x'), Character.valueOf('X'), Character.valueOf('e'), Character.valueOf('E'), Character.valueOf('g'), Character.valueOf('G'), //
+	Character.valueOf('a'), Character.valueOf('A'), Character.valueOf('t'), Character.valueOf('T'), Character.valueOf('n') };
 
 	/**
 	 * Returns the number of words in the text string.
