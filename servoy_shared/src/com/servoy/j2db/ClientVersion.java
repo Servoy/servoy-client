ec37471b65428f0a9458a3cb82677ea772a2320a
Johan Compagner <jcompagner@servoy.com>
2017-07-24 12:21:15 +0200

    SVY-11337 Version bumb for jenkins 8.2b1 -> 8.2rc with buildnr increase
    poms updated so version push works
    updated ClientVersion

--- servoy_shared/src/com/servoy/j2db/ClientVersion.java
+++ servoy_shared/src/com/servoy/j2db/ClientVersion.java
@@ -29,8 +29,8 @@ public abstract class ClientVersion
 	private static final int majorVersion = 8;
 	private static final int middleVersion = 2;
 	private static final int minorVersion = 0;
-	private static final int releaseNumber = 3100;
-	private static final String versionPostfix = "b1";
+	private static final int releaseNumber = 3101;
+	private static final String versionPostfix = "rc1";
 
 	// make sure you keep this the same format, or make it work with extensions version comparing & xml schema
 	private static final String version = majorVersion + "." + middleVersion + "." + minorVersion + (versionPostfix != null ? " " + versionPostfix : "");
