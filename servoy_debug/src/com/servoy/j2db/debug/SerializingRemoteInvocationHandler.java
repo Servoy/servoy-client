933ad6d91af0e65dd0c3c40e773d3dd158b575bb
Johan Compagner <jcompagner@servoy.com>
2017-07-25 16:22:41 +0200

    make sure that all objects implementing Remote (rmi) will be serialized just fine.

--- servoy_debug/src/com/servoy/j2db/debug/SerializingRemoteInvocationHandler.java
+++ servoy_debug/src/com/servoy/j2db/debug/SerializingRemoteInvocationHandler.java
@@ -19,12 +19,18 @@ package com.servoy.j2db.debug;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.ObjectStreamClass;
+import java.io.OutputStream;
+import java.io.Serializable;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 import java.rmi.Remote;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 import org.apache.wicket.util.io.ByteArrayOutputStream;
 
@@ -35,9 +41,9 @@ import com.servoy.j2db.util.Debug;
 /**
  * Proxy invocationHandler that serializes/deserializes arguments to method calls and the result.
  * This can be used to mimic rmi access from debug SC in developer.
- * 
+ *
  * @author rgansevles
- * 
+ *
  * @since 6.0
  *
  */
@@ -81,27 +87,12 @@ public class SerializingRemoteInvocationHandler<T extends Remote> extends Abstra
 	public <O> O serializeAndDeserialize(O o) throws IOException, ClassNotFoundException
 	{
 		ByteArrayOutputStream baos = new ByteArrayOutputStream();
-		ObjectOutputStream os = new ObjectOutputStream(baos);
+		Map<ReplacedObject, Object> map = new HashMap<>();
+		ObjectOutputStream os = new ReplaceObjectOutputStream(baos, map);
 		os.writeObject(o);
 		os.close();
 
-		ObjectInputStream is = new ObjectInputStream(new ByteArrayInputStream(baos.toByteArray()))
-		{
-			@Override
-			protected Class< ? > resolveClass(ObjectStreamClass desc) throws IOException, ClassNotFoundException
-			{
-				String name = desc.getName();
-				try
-				{
-					return Class.forName(name, false, application.getBeanManager().getClassLoader());
-				}
-				catch (ClassNotFoundException e)
-				{
-					Debug.error("Could not load class using beanmanager classloader, trying default", e);
-					return super.resolveClass(desc);
-				}
-			}
-		};
+		ObjectInputStream is = new ResolveObjectInputStream(new ByteArrayInputStream(baos.toByteArray()), map);
 		@SuppressWarnings("unchecked")
 		O o2 = (O)is.readObject();
 		is.close();
@@ -118,4 +109,99 @@ public class SerializingRemoteInvocationHandler<T extends Remote> extends Abstra
 		Object res = invokeMethod(method, serializedArgs);
 		return serializeAndDeserialize(res);
 	}
+
+	/**
+	 * @author jcompagner
+	 *
+	 */
+	private final class ResolveObjectInputStream extends ObjectInputStream
+	{
+		private final Map<ReplacedObject, Object> map;
+
+		/**
+		 * @param in
+		 */
+		private ResolveObjectInputStream(InputStream in, Map<ReplacedObject, Object> map) throws IOException
+		{
+			super(in);
+			this.map = map;
+			enableResolveObject(true);
+		}
+
+		@Override
+		protected Class< ? > resolveClass(ObjectStreamClass desc) throws IOException, ClassNotFoundException
+		{
+			String name = desc.getName();
+			if (name.endsWith("SerializingRemoteInvocationHandler$ReplacedObject"))
+			{
+				return super.resolveClass(desc);
+			}
+			try
+			{
+				return Class.forName(name, false, application.getBeanManager().getClassLoader());
+			}
+			catch (ClassNotFoundException e)
+			{
+				Debug.error("Could not load class using beanmanager classloader, trying default", e);
+				return super.resolveClass(desc);
+			}
+		}
+
+		@Override
+		protected Object resolveObject(Object obj) throws IOException
+		{
+			Object o = map.get(obj);
+			if (o != null) return o;
+			return super.resolveObject(obj);
+		}
+	}
+
+	private static final class ReplaceObjectOutputStream extends ObjectOutputStream
+	{
+		private final Map<ReplacedObject, Object> replacedCache;
+
+		/**
+		 * @param out
+		 */
+		private ReplaceObjectOutputStream(OutputStream out, Map<ReplacedObject, Object> replacedCache) throws IOException
+		{
+			super(out);
+			this.replacedCache = replacedCache;
+			enableReplaceObject(true);
+		}
+
+		@Override
+		protected Object replaceObject(Object obj) throws IOException
+		{
+			if (obj instanceof Remote)
+			{
+				ReplacedObject ro = new ReplacedObject();
+				replacedCache.put(ro, obj);
+				return ro;
+			}
+			return super.replaceObject(obj);
+		}
+	}
+
+	private static final class ReplacedObject implements Serializable
+	{
+		private final UUID uuid = UUID.randomUUID();
+
+		@Override
+		public boolean equals(Object obj)
+		{
+			if (obj instanceof ReplacedObject)
+			{
+				return uuid.equals(((ReplacedObject)obj).uuid);
+			}
+			return false;
+		}
+
+		@Override
+		public int hashCode()
+		{
+			return uuid.hashCode();
+		}
+	}
+
 }
