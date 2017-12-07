572095e633b3e9190de8a566e004824b74d2e359
Johan Compagner <jcompagner@servoy.com>
2017-12-07 11:37:18 +0100

    SVY-11525 Handler translate clientside foundset's record to JSRecord
    added synchornized keyword

--- servoy_shared/src/com/servoy/j2db/dataprocessing/FoundSetManager.java
+++ servoy_shared/src/com/servoy/j2db/dataprocessing/FoundSetManager.java
@@ -1474,7 +1474,7 @@ public class FoundSetManager implements IFoundSetManagerInternal
 	}
 
 	@Override
-	public int getNextFoundSetID()
+	public synchronized int getNextFoundSetID()
 	{
 		return foundsetCounter++;
 	}
