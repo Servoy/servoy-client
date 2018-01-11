63d8c3bcd6a7bd65124c5c4a4f272bedbd5a7e89
emera <emera@servoy.com>
2018-01-11 18:04:06 +0200

    SVY-12006 security.getElementUUIDs(formName) for responsive forms

--- servoy_shared/src/com/servoy/j2db/scripting/JSSecurity.java
+++ servoy_shared/src/com/servoy/j2db/scripting/JSSecurity.java
@@ -40,6 +40,7 @@ import com.servoy.j2db.persistence.IRepository;
 import com.servoy.j2db.persistence.IServer;
 import com.servoy.j2db.persistence.ISupportName;
 import com.servoy.j2db.persistence.ITable;
+import com.servoy.j2db.persistence.NameComparator;
 import com.servoy.j2db.persistence.RepositoryException;
 import com.servoy.j2db.persistence.SolutionMetaData;
 import com.servoy.j2db.persistence.Table;
@@ -956,7 +957,8 @@ public class JSSecurity implements IReturnedTypesProvider, IConstantsObject, IJS
 		}
 		catch (Exception e)
 		{
-			application.reportJSError("Can't remove user  " + a_userUID + " from group " + groupName + ", only admin users can create/change security stuff", e);
+			application.reportJSError("Can't remove user  " + a_userUID + " from group " + groupName + ", only admin users can create/change security stuff",
+				e);
 		}
 		return false;
 	}
@@ -1473,13 +1475,13 @@ public class JSSecurity implements IReturnedTypesProvider, IConstantsObject, IJS
 		{
 			List elements = new ArrayList();
 			elements.add(new Object[] { null, f.getUUID() });
-			Iterator it = f.getAllObjects();
+			Iterator< ? extends IPersist> it = f.isResponsiveLayout() ? f.getFlattenedObjects(NameComparator.INSTANCE).iterator() : f.getAllObjects();
 			while (it.hasNext())
 			{
-				IPersist elem = (IPersist)it.next();
+				IPersist elem = it.next();
 				int type = elem.getTypeID();
 				if (type == IRepository.GRAPHICALCOMPONENTS || type == IRepository.FIELDS || type == IRepository.PORTALS || type == IRepository.RECTSHAPES ||
-					type == IRepository.SHAPES || type == IRepository.BEANS || type == IRepository.TABPANELS)
+					type == IRepository.SHAPES || type == IRepository.BEANS || type == IRepository.TABPANELS || type == IRepository.WEBCOMPONENTS)
 				{
 					if (elem instanceof ISupportName && ((ISupportName)elem).getName() != null)
 					{
