9bd589c9fb3abd4d49db52f8b25de6db7478805a
Johan Compagner <jcompagner@servoy.com>
2018-02-12 17:11:47 +0100

    SVY-12172 ConcurrentModificationException in DAL
    every iteration through the visible forms will use a copy now

--- servoy_ngclient/src/com/servoy/j2db/server/ngclient/DataAdapterList.java
+++ servoy_ngclient/src/com/servoy/j2db/server/ngclient/DataAdapterList.java
@@ -290,7 +290,8 @@ public class DataAdapterList implements IModificationListener, ITagResolver, IDa
 
 		if (relation != null)
 		{
-			for (Entry<IWebFormController, String> relatedFormEntry : visibleChildForms.entrySet())
+			HashMap<IWebFormController, String> childFormsCopy = getVisibleChildFormCopy();
+			for (Entry<IWebFormController, String> relatedFormEntry : childFormsCopy.entrySet())
 			{
 				IWebFormController relatedForm = relatedFormEntry.getKey();
 				String relatedFormRelation = relatedFormEntry.getValue();
@@ -533,11 +534,13 @@ public class DataAdapterList implements IModificationListener, ITagResolver, IDa
 		}
 		if (record != null)
 		{
-			for (IWebFormController form : visibleChildForms.keySet())
+			for (Entry<IWebFormController, String> entry : getVisibleChildFormCopy().entrySet())
 			{
-				if (visibleChildForms.get(form) != null)
+				String relation = entry.getValue();
+				if (relation != null)
 				{
-					form.loadRecords(record.getRelatedFoundSet(visibleChildForms.get(form), ((BasicFormController)form).getDefaultSortColumns()));
+					IWebFormController form = entry.getKey();
+					form.loadRecords(record.getRelatedFoundSet(relation, ((BasicFormController)form).getDefaultSortColumns()));
 				}
 			}
 		}
@@ -640,8 +643,7 @@ public class DataAdapterList implements IModificationListener, ITagResolver, IDa
 	{
 		if (record != null && e != null && e.getName() != null)
 		{
-			HashMap<IWebFormController, String> visibleChildFormsCopy = new HashMap<>(visibleChildForms);
-			for (Entry<IWebFormController, String> relatedFormEntry : visibleChildFormsCopy.entrySet())
+			for (Entry<IWebFormController, String> relatedFormEntry : getVisibleChildFormCopy().entrySet())
 			{
 				IWebFormController relatedForm = relatedFormEntry.getKey();
 				String relatedFormRelation = relatedFormEntry.getValue();
@@ -965,7 +967,7 @@ public class DataAdapterList implements IModificationListener, ITagResolver, IDa
 
 	public void notifyVisible(boolean b, List<Runnable> invokeLaterRunnables, Set<IWebFormController> childFormsThatWereAlreadyNotified)
 	{
-		HashMap<IWebFormController, String> childFormsCopy = new HashMap<IWebFormController, String>(visibleChildForms);
+		HashMap<IWebFormController, String> childFormsCopy = getVisibleChildFormCopy();
 		for (IWebFormController relatedController : childFormsCopy.keySet())
 		{
 			updateParentContainer(relatedController, childFormsCopy.get(relatedController), b);
@@ -973,9 +975,17 @@ public class DataAdapterList implements IModificationListener, ITagResolver, IDa
 		}
 	}
 
+	/**
+	 * @return
+	 */
+	private HashMap<IWebFormController, String> getVisibleChildFormCopy()
+	{
+		return new HashMap<IWebFormController, String>(visibleChildForms);
+	}
+
 	public boolean stopUIEditing(boolean looseFocus)
 	{
-		for (IWebFormController relatedController : visibleChildForms.keySet())
+		for (IWebFormController relatedController : getVisibleChildFormCopy().keySet())
 		{
 			if (!relatedController.stopUIEditing(looseFocus)) return false;
 		}
