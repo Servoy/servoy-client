13ce26c65e2bba392d5b56a819ee80156226c70f
Rob Gansevles <rgansevles@gmail.com>
2018-06-29 10:57:27 +0200

    SVY-12517 Bad connection not evicted from database connection pool.

--- servoy_shared/src/com/servoy/j2db/util/Utils.java
+++ servoy_shared/src/com/servoy/j2db/util/Utils.java
@@ -52,7 +52,6 @@ import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.sql.Connection;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.sql.Statement;
 import java.sql.Timestamp;
 import java.sql.Types;
@@ -1724,7 +1723,7 @@ public final class Utils
 				connection.rollback();
 			}
 		}
-		catch (SQLException e)
+		catch (Exception e)
 		{
 			Debug.error(e);
 		}
@@ -1774,7 +1773,7 @@ public final class Utils
 				}
 			}
 		}
-		catch (SQLException e)
+		catch (Exception e)
 		{
 			Debug.error(e);
 		}
@@ -1790,7 +1789,7 @@ public final class Utils
 				statement.close();
 			}
 		}
-		catch (SQLException e)
+		catch (Exception e)
 		{
 			Debug.error(e);
 		}
@@ -1806,7 +1805,7 @@ public final class Utils
 				resultSet.close();
 			}
 		}
-		catch (SQLException e)
+		catch (Exception e)
 		{
 			Debug.error(e);
 		}
