13519f94bb0754391754d219867dde5b151d0028
Gabi Boros <gboros@servoy.com>
2018-08-03 13:51:22 +0300

    SVY-12634 add converter to map property type

--- servoy_ngclient/src/com/servoy/j2db/server/ngclient/property/types/MapPropertyType.java
+++ servoy_ngclient/src/com/servoy/j2db/server/ngclient/property/types/MapPropertyType.java
@@ -17,18 +17,31 @@
 
 package com.servoy.j2db.server.ngclient.property.types;
 
+import java.util.Map;
+
+import org.json.JSONException;
 import org.json.JSONObject;
+import org.json.JSONWriter;
+import org.sablo.specification.PropertyDescription;
+import org.sablo.specification.property.IBrowserConverterContext;
+import org.sablo.specification.property.IConvertedPropertyType;
 import org.sablo.specification.property.types.DefaultPropertyType;
+import org.sablo.util.ValueReference;
+import org.sablo.websocket.utils.DataConversion;
+
+import com.servoy.j2db.server.ngclient.FormElementContext;
+import com.servoy.j2db.server.ngclient.property.types.NGConversions.IFormElementToTemplateJSON;
 
 /**
  * @author gboros
  *
  */
-public class MapPropertyType extends DefaultPropertyType<JSONObject>
+public class MapPropertyType extends DefaultPropertyType<Map<String, ? >>
+	implements IConvertedPropertyType<Map<String, ? >>, IFormElementToTemplateJSON<JSONObject, Map<String, ? >>
 {
 
 	public static final MapPropertyType INSTANCE = new MapPropertyType();
-	public static final String TYPE_NAME = "map";
+	public static final String TYPE_NAME = "map"; //$NON-NLS-1$
 
 	private MapPropertyType()
 	{
@@ -43,5 +56,102 @@ public class MapPropertyType extends DefaultPropertyType<JSONObject>
 	public String getName()
 	{
 		return TYPE_NAME;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see org.sablo.specification.property.IPropertyConverter#fromJSON(java.lang.Object, java.lang.Object, org.sablo.specification.PropertyDescription,
+	 * java.lang.Object, org.sablo.util.ValueReference)
+	 */
+	@Override
+	public Map<String, ? > fromJSON(Object newJSONValue, Map<String, ? > previousSabloValue, PropertyDescription propertyDescription,
+		IBrowserConverterContext context, ValueReference<Boolean> returnValueAdjustedIncommingValue)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see org.sablo.specification.property.IPropertyConverter#toJSON(org.json.JSONWriter, java.lang.String, java.lang.Object,
+	 * org.sablo.specification.PropertyDescription, org.sablo.websocket.utils.DataConversion, java.lang.Object)
+	 */
+	@Override
+	public JSONWriter toJSON(JSONWriter writer, String key, Map<String, ? > sabloValue, PropertyDescription propertyDescription,
+		DataConversion clientConversion, IBrowserConverterContext dataConverterContext) throws JSONException
+	{
+		if (sabloValue != null)
+		{
+			JSONObject jsonMap = new JSONObject();
+			writer.key(key);
+			for (Map.Entry<String, ? > mapEntry : sabloValue.entrySet())
+			{
+				jsonMap.put(mapEntry.getKey(), mapEntry.getValue());
+			}
+			writer.value(jsonMap);
+		}
+		return writer;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see com.servoy.j2db.server.ngclient.property.types.NGConversions.IFormElementToTemplateJSON#toTemplateJSONValue(org.json.JSONWriter, java.lang.String,
+	 * java.lang.Object, org.sablo.specification.PropertyDescription, org.sablo.websocket.utils.DataConversion,
+	 * com.servoy.j2db.server.ngclient.FormElementContext)
+	 */
+	@Override
+	public JSONWriter toTemplateJSONValue(JSONWriter writer, String key, JSONObject formElementValue, PropertyDescription pd,
+		DataConversion browserConversionMarkers, FormElementContext formElementContext) throws JSONException
+	{
+		if (formElementValue != null)
+		{
+			JSONObject fixedTypeMap = new JSONObject();
+			for (String jsonKey : formElementValue.keySet())
+			{
+				Object v = formElementValue.get(jsonKey);
+				if (v instanceof String)
+				{
+					String sV = (String)v;
+					if (sV.length() > 1 && sV.startsWith("'") && sV.endsWith("'"))
+					{
+						v = sV.substring(1, sV.length() - 1);
+					}
+					else if (sV.toLowerCase().equals("true") || sV.toLowerCase().equals("false"))
+					{
+						fixedTypeMap.put(jsonKey, Boolean.parseBoolean(sV));
+						continue;
+					}
+					else
+					{
+						try
+						{
+							long lV = Long.parseLong(sV);
+							fixedTypeMap.put(jsonKey, lV);
+							continue;
+						}
+						catch (NumberFormatException ex)
+						{
+							try
+							{
+								double dV = Double.parseDouble(sV);
+								fixedTypeMap.put(jsonKey, dV);
+								continue;
+							}
+							catch (NumberFormatException ex1)
+							{
+							}
+						}
+					}
+
+				}
+				fixedTypeMap.put(jsonKey, v);
+			}
+			writer.key(key);
+			writer.value(fixedTypeMap);
+		}
+		return writer;
 	}
 }
