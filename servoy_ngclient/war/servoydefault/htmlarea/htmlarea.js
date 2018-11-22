9ba93628bfe86b00a0777aca956cc322c9af851c
Costin Chiulan <costinchiulan@gmail.com>
2018-11-22 10:05:48 +0200

    SVY-13025 HTML editor (anchored form) does not re-enable editable

--- servoy_ngclient/war/servoydefault/htmlarea/htmlarea.js
+++ servoy_ngclient/war/servoydefault/htmlarea/htmlarea.js
@@ -22,7 +22,14 @@ angular.module('servoydefaultHtmlarea',['servoy','ui.tinymce']).directive('servo
 						$scope.editor = editor;
 						editor.on('init', function() {
 							$scope.init = true;
-							$scope.model.editable ? editor.setMode('design'):editor.setMode('readonly')
+							if($scope.model.editable){
+								editor.setMode('design');
+								editor.settings.readonly = 0;				
+							}
+							else{
+								editor.setMode('readonly');
+								editor.settings.readonly = 1;
+							}							
 							ed.getBody().setAttribute('contenteditable', $scope.model.editable);
 							
 							if (!$scope.svyServoyapi.isInDesigner())
@@ -315,7 +322,7 @@ angular.module('servoydefaultHtmlarea',['servoy','ui.tinymce']).directive('servo
 	var ServoyTinyMCESettings = {
 			menubar : false,
 			statusbar : false,
-			readonly: 1,
+			readonly: 0,
 			plugins: 'tabindex resizetocontainer',
 			tabindex: 'element',
 			toolbar: 'fontselect fontsizeselect | bold italic underline | superscript subscript | undo redo |alignleft aligncenter alignright alignjustify | styleselect | outdent indent bullist numlist'
