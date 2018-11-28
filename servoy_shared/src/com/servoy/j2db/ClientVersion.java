8774393a7fe4548b281ceaabb4ef63eb871e8972
Johan Compagner <jcompagner@servoy.com>
2018-11-28 16:07:48 +0100

    SVY-13055 build and test Servoy 8.4 rc
    version bumb to 8.4.0.3400_rc1

--- servoy_shared/src/com/servoy/j2db/ClientVersion.java
+++ servoy_shared/src/com/servoy/j2db/ClientVersion.java
@@ -30,7 +30,7 @@ public abstract class ClientVersion
 	private static final int middleVersion = 4;
 	private static final int minorVersion = 0;
 	private static final int releaseNumber = 3400;
-	private static final String versionPostfix = "b1";
+	private static final String versionPostfix = "rc1";
 
 	// make sure you keep this the same format, or make it work with extensions version comparing & xml schema
 	private static final String version = majorVersion + "." + middleVersion + "." + minorVersion + (versionPostfix != null ? " " + versionPostfix : "");
