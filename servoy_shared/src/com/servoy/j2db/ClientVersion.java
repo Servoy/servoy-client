9e44278fad63d2d91f4f16ed4c693abc732e472f
Johan Compagner <jcompagner@servoy.com>
2018-12-27 16:20:28 +0100

    version bumb to 2019.3.0.3410_rc

--- servoy_shared/src/com/servoy/j2db/ClientVersion.java
+++ servoy_shared/src/com/servoy/j2db/ClientVersion.java
@@ -26,11 +26,11 @@ package com.servoy.j2db;
 public abstract class ClientVersion
 {
 	// these fields are private intentionally, to prevent final class member copy in other classes!
-	private static final int majorVersion = 8;
-	private static final int middleVersion = 4;
+	private static final int majorVersion = 2019;
+	private static final int middleVersion = 3;
 	private static final int minorVersion = 0;
-	private static final int releaseNumber = 3401;
-	private static final String versionPostfix = "rc2";
+	private static final int releaseNumber = 3410;
+	private static final String versionPostfix = "rc";
 
 	// make sure you keep this the same format, or make it work with extensions version comparing & xml schema
 	private static final String version = majorVersion + "." + middleVersion + "." + minorVersion + (versionPostfix != null ? " " + versionPostfix : "");
