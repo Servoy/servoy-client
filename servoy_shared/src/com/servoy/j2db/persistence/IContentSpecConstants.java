7dadd1fda6c5cb92dc291c646f5a79561ae931f3
Costin Chiulan <cchiulan@servoy.com>
2019-03-14 17:31:24 +0200

    Revert "SVY-13421 Autofill to allow generic values."
    This reverts commit 4b6a664405d293eb999ee8d7e032344eb93d4175.

--- servoy_shared/src/com/servoy/j2db/persistence/IContentSpecConstants.java
+++ servoy_shared/src/com/servoy/j2db/persistence/IContentSpecConstants.java
@@ -213,7 +213,6 @@ public interface IContentSpecConstants extends IContentSpecConstantsBase
 	public static final String PROPERTY_JSON = "json";
 	public static final String PROPERTY_TYPENAME = "typeName";
 	public static final String PROPERTY_NG_READONLY_MODE = "ngReadOnlyMode";
-	public static final String PROPERTY_AUTOCOMPLETE = "autocomplete";
 	public static final String PROPERTY_ONELEMENTDATACHANGEMETHODID = "onElementDataChangeMethodID";
 	public static final String PROPERTY_FORM_COMPONENT = "formComponent";
 	public static final String PROPERTY_DESIGNTIME = "design";
