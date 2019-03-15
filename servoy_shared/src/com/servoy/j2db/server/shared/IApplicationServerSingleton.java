bf9720a3440fc6798adb61901d63629531bf592a
emera <emera@servoy.com>
2019-03-15 18:23:43 +0200

    SVY-13476  WarExporter saveLicense should auto upgrade the license (if it is not valid)

--- servoy_shared/src/com/servoy/j2db/server/shared/IApplicationServerSingleton.java
+++ servoy_shared/src/com/servoy/j2db/server/shared/IApplicationServerSingleton.java
@@ -37,6 +37,7 @@ import com.servoy.j2db.persistence.RepositoryException;
 import com.servoy.j2db.persistence.SolutionMetaData;
 import com.servoy.j2db.plugins.IPluginManagerInternal;
 import com.servoy.j2db.plugins.IServerAccess;
+import com.servoy.j2db.util.Pair;
 import com.servoy.j2db.util.xmlxport.IXMLExportUserChannel;
 import com.servoy.j2db.util.xmlxport.IXMLExporter;
 import com.servoy.j2db.util.xmlxport.IXMLImportEngine;
@@ -152,4 +153,6 @@ public interface IApplicationServerSingleton
 	void checkClientRepositoryAccess(String serverName) throws RepositoryException;
 
 	boolean checkClientLicense(String companyName, String license, String numberOfLicenses);
+
+	Pair<Boolean, String> upgradeLicense(String companyName, String licensecode, String licenses);
 }
