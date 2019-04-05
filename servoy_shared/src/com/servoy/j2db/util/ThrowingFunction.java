eba0ea39ae0590d5941f811e3a9893995b944ebc
Johan Compagner <jcompagner@servoy.com>
2019-04-05 11:26:57 +0200

    SVY-13562 ViewFoundset getTable() implementation should generate a table that really reflects all the columns
    helper classes for lamba's to wrap around checked exceptions

---
+++ servoy_shared/src/com/servoy/j2db/util/ThrowingFunction.java
@@ -0,0 +1,23 @@
+package com.servoy.j2db.util;
+
+import java.util.function.Function;
+
+@FunctionalInterface
+public interface ThrowingFunction<T, R, E extends Throwable>
+{
+	R apply(T t) throws E;
+
+	static <T, R, E extends Throwable> Function<T, R> unchecked(ThrowingFunction<T, R, E> f)
+	{
+		return t -> {
+			try
+			{
+				return f.apply(t);
+			}
+			catch (Throwable e)
+			{
+				throw new RuntimeException(e);
+			}
+		};
+	}
+}
