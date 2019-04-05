54dda80019c4a6efe99aa0086af95e78b39d73d0
Johan Compagner <jcompagner@servoy.com>
2019-04-05 13:11:32 +0200

    updated doc when getting a view foundset that is not registered

--- servoy_shared/src/com/servoy/j2db/dataprocessing/FoundSetManager.java
+++ servoy_shared/src/com/servoy/j2db/dataprocessing/FoundSetManager.java
@@ -1715,7 +1715,7 @@ public class FoundSetManager implements IFoundSetManagerInternal
 		{
 			ViewFoundSet vfs = viewFoundSets.get(dataSource);
 			if (vfs == null) throw new IllegalStateException("The view datasource " + dataSource +
-				" is not registered yet on the form manager, please use databaseManager.regiserViewFoundset() first before showing a form");
+				" is not registered yet on the form manager, please use databaseManager.getViewFoundSet(name, query, register)  with the register boolean true, or get at design time view through datasourcs.view.xxx.getFoundSet() first before showing a form");
 			return vfs;
 		}
 
