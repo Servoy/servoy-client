8c70dc53df77416ed8e2d6a499945ea3fd498525
Johan Compagner <jcompagner@servoy.com>
2019-07-04 10:59:31 +0200

    SVY-13875 build and test Servoy 2019.06 final
    version bumb to 3502

--- servoy_shared/src/com/servoy/j2db/ClientVersion.java
+++ servoy_shared/src/com/servoy/j2db/ClientVersion.java
@@ -29,8 +29,8 @@ public abstract class ClientVersion
 	private static final int majorVersion = 2019;
 	private static final int middleVersion = 6;
 	private static final int minorVersion = 0;
-	private static final int releaseNumber = 3501;
-	private static final String versionPostfix = "rc2";
+	private static final int releaseNumber = 3502;
+	private static final String versionPostfix = "";
 	private static final boolean lts = false;
 
 	// make sure you keep this the same format, or make it work with extensions version comparing & xml schema
