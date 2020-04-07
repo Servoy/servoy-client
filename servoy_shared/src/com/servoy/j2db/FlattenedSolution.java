c2e19bda5eb0fe6c5612e09af0655d41170521ce
Johan Compagner <jcompagner@servoy.com>
2020-04-07 10:30:41 +0200

    SVY-14765 False positive warnings in developer
    flush the PersistHelper super cache that we have after we did load the
    index, because the index could have filled it already at creation with
    NULL values because the extended form was not set yet. (that can only be
    done after the index is created)

--- servoy_shared/src/com/servoy/j2db/FlattenedSolution.java
+++ servoy_shared/src/com/servoy/j2db/FlattenedSolution.java
@@ -112,6 +112,7 @@ import com.servoy.j2db.util.DataSourceUtils;
 import com.servoy.j2db.util.Debug;
 import com.servoy.j2db.util.IteratorChain;
 import com.servoy.j2db.util.Pair;
+import com.servoy.j2db.util.PersistHelper;
 import com.servoy.j2db.util.ScopesUtils;
 import com.servoy.j2db.util.UUID;
 import com.servoy.j2db.util.Utils;
@@ -356,6 +357,8 @@ public class FlattenedSolution implements IItemChangeListener<IPersist>, IDataPr
 			index = createPersistIndex();
 
 			// refresh all the extends forms, TODO this is kind of bad, because form instances are shared over clients.
+			// flush first the persist helpers cach that could already been filled with null values in creating the index.
+			PersistHelper.flushSuperPersistCache();
 			Iterator<Form> it = getForms(false);
 			while (it.hasNext())
 			{
