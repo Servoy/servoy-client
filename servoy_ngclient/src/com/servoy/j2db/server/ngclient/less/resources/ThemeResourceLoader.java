748213cacb36456270dc69524feb5523cde4df32
Johan Compagner <jcompagner@servoy.com>
2020-05-26 09:06:57 +0200

    SVY-15003 Get new version of Servoy Theme for 2020.6

--- servoy_ngclient/src/com/servoy/j2db/server/ngclient/less/resources/ThemeResourceLoader.java
+++ servoy_ngclient/src/com/servoy/j2db/server/ngclient/less/resources/ThemeResourceLoader.java
@@ -38,7 +38,7 @@ public class ThemeResourceLoader
 	public static final String CUSTOM_PROPERTIES_LESS = "custom_servoy_theme_properties.less";
 	public static final String PROPERTIES_LESS = "servoy_theme_properties.less";
 	public static final String THEME_LESS = "servoy_theme.less";
-	public static final String[] VERSIONS = new String[] { "latest", "2019.12.0", "2019.6.0", "2019.3.0", "8.4.0" };
+	public static final String[] VERSIONS = new String[] { "latest", "2020.6.0", "2019.12.0", "2019.6.0", "2019.3.0", "8.4.0" };
 
 	private static SortedMap<Version, String> themePropertyResource = new TreeMap<>();
 	private static SortedMap<Version, String> themeResource = new TreeMap<>();
@@ -48,11 +48,13 @@ public class ThemeResourceLoader
 		themeResource.put(new Version("2019.3.0"), "servoy_theme_2019.3.0.less");
 		themeResource.put(new Version("2019.6.0"), "servoy_theme_2019.6.0.less");
 		themeResource.put(new Version("2019.12.0"), "servoy_theme_2019.12.0.less");
+		themeResource.put(new Version("2020.6.0"), "servoy_theme_2020.6.0.less");
 
 		themePropertyResource.put(new Version("8.4.0"), "servoy_theme_properties_8.4.0.less");
 		themePropertyResource.put(new Version("2019.3.0"), "servoy_theme_properties_2019.3.0.less");
 		themePropertyResource.put(new Version("2019.6.0"), "servoy_theme_properties_2019.6.0.less");
 		themePropertyResource.put(new Version("2019.12.0"), "servoy_theme_properties_2019.12.0.less");
+		themePropertyResource.put(new Version("2020.6.0"), "servoy_theme_properties_2020.6.0.less");
 	}
 
 	public static byte[] getDefaultSolutionLess()
