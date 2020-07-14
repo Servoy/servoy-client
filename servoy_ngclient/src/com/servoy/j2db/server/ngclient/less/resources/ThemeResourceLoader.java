58f07280e27e751aefd9eccf632dd69a5593494c
Johan Compagner <jcompagner@servoy.com>
2020-07-14 17:58:46 +0200

    In a regexp the . matching (really on a .) should be escaped, else it is just a special regexp char

--- servoy_ngclient/src/com/servoy/j2db/server/ngclient/less/resources/ThemeResourceLoader.java
+++ servoy_ngclient/src/com/servoy/j2db/server/ngclient/less/resources/ThemeResourceLoader.java
@@ -145,7 +145,7 @@ public class ThemeResourceLoader
 
 		public Version(String versionString)
 		{
-			String[] parts = versionString.split(".");
+			String[] parts = versionString.split("\\.");
 			if (parts.length > 2)
 			{
 				major = Utils.getAsInteger(parts[0], false);
