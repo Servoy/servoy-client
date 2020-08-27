a69d9366f7574b9b61e54b7dc63c647da3b62a3a
Diana Bunaciu <dtimut@servoy.com>
2020-08-27 17:16:02 +0300

    SVY-15367 Can't close form popup if opened 1 time after the other If the formpopup is opened and the user wants to open it again, the method closeFormPopup will not be called any more, because the form popup is viewed as "hidden" (scope.model.popupform == null) to allow the form popup to close the second time, we need to ensure that scope.model.popupform will have a value the second time the popup is opened.

--- servoy_ngclient/war/servoyservices/window/window.js
+++ servoy_ngclient/war/servoyservices/window/window.js
@@ -320,7 +320,7 @@ angular.module('window',['servoy'])
 		 * plugins.window.cancelFormPopup();
 		 * 
 		 */
-		cancelFormPopup : function()
+		cancelFormPopup : function(enableCallServerSideApi)
 		{
 			$('body').off('mouseup',formPopupBodyListener);
 			if (scope.formPopupShown)
@@ -349,6 +349,13 @@ angular.module('window',['servoy'])
 			if (scope.model.popupform === scope.formPopupShown) {
 				scope.model.popupform = null;
 				scope.formPopupShown = null;
+				/* Only callServerSideApi clearPopupForm when the popup si closed.
+				 * If the form is already opened, and the user wants to open it again ,we need scope.model.popupform to have a value, in order to allow the popup to be closed.
+				 * This will set scope.model.popupform again to null only when necessary.
+				 * */
+				if(!enableCallServerSideApi){
+					$services.callServerSideApi("window","clearPopupForm",[]);
+				}
 			}
 			else {
 				_this.showFormPopup(scope.model.popupform.component,scope.model.popupform.form,scope.model.popupform.width,scope.model.popupform.height,scope.model.popupform.x,scope.model.popupform.y,scope.model.popupform.showBackdrop);
