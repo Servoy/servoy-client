0e3282ade26f69aecd731acdc3f8897e0040ffc2
lvostinar <lvostinar@servoy.com>
2020-12-08 11:51:01 +0200

    SVY-15681 Regression: improper escaping in the values of attributes on components

--- servoy_ngclient/src/com/servoy/j2db/server/ngclient/PersistBasedFormElementImpl.java
+++ servoy_ngclient/src/com/servoy/j2db/server/ngclient/PersistBasedFormElementImpl.java
@@ -25,7 +25,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
-import org.apache.commons.lang3.StringEscapeUtils;
+import org.apache.commons.text.StringEscapeUtils;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.sablo.specification.PropertyDescription;
