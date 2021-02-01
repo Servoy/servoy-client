4f90b058ff8c3ed47dd89c0d8893a6de7220e118
Andrei Costescu <costescuandrei@gmail.com>
2021-02-01 11:51:41 +0200

    For SVYX-165: Latest version of FullCalendar causes many log entries due invalid (servoy) api usage
    Deep watch on root 'object' typed property would not work correctly. I
    did watch it an send it to server but then server ignored it because of
    some regression introuced by SVY-8479. (JSONObject.equals was not
    actually doing anything except a reference check which was unexpected)

--- servoy_shared/src/com/servoy/j2db/util/Utils.java
+++ servoy_shared/src/com/servoy/j2db/util/Utils.java
@@ -2453,6 +2453,8 @@ public final class Utils
 		return oldObj.equals(obj);
 	}
 
+	// THESE METHODS FOR JSON comparison are copy pasted to sablo's JSONUtils as well; if you alter them please update code in both places!
+	// BEGIN -------------------------
 	/**
 	 * Identical to {@link #areJSONEqual(Object, Object, String)} where keysToIgnoreRegex is null (so it doesn't skip any keys during comparison).
 	 */
@@ -2506,6 +2508,8 @@ public final class Utils
 			return elem;
 		}
 	}
+
+	// END -------------------------
 
 	/**
 	 * Convert to string representation, remove trailing '.0' for numbers.
