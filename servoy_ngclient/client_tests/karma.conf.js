91d704ea69d762bb7e2128b6b0f75076d32a8b1a
Johan Compagner <jcompagner@servoy.com>
2021-04-22 10:50:56 +0200

    update frontend-maven-plugin to 1.11.3 and the npm version to 16
    updated karma test to use now Chrome or Edge browsers instead of the
    depricated PhantomJS

--- servoy_ngclient/client_tests/karma.conf.js
+++ servoy_ngclient/client_tests/karma.conf.js
@@ -1,88 +1,89 @@
 module.exports = function(config) {
-	config.set({
-		basePath: '.',
+    config.set({
+        basePath: '.',
 
-		preprocessors: {
-			'../war/servoycore/**/*.html': ['ng-html2js'],
-			'../war/servoydefault/**/*.html': ['ng-html2js']
-		},
-		files: [
-			{ pattern: 'fileResources/**/*', watched: true, included: false, served: true },
+        preprocessors: {
+            '../war/servoycore/**/*.html': ['ng-html2js'],
+            '../war/servoydefault/**/*.html': ['ng-html2js']
+        },
+        files: [
+            { pattern: 'fileResources/**/*', watched: true, included: false, served: true },
 
-			// libraries for testing and angular
-			'lib/jquery.js',
-			'lib/phantomjs.polyfill.js',
-			'../war/js/angular.js',
-			'lib/angular-mocks.js',
+            // libraries for testing and angular
+            'lib/jquery.js',
+            'lib/phantomjs.polyfill.js',
+            '../war/js/angular.js',
+            'lib/angular-mocks.js',
 
-			// sablo scripts
-			'sablo/META-INF/resources/sablo/js/*.js', /* use this when running from Git */
-			'sablo/META-INF/resources/sablo/*.js', /* use this when running from Git */
+            // sablo scripts
+            'sablo/META-INF/resources/sablo/js/*.js', /* use this when running from Git */
+            'sablo/META-INF/resources/sablo/*.js', /* use this when running from Git */
 
-			// ngclient scripts
-			'../war/js/numeral.js',
-			'../war/js/**/*.js',
+            // ngclient scripts
+            '../war/js/numeral.js',
+            '../war/js/**/*.js',
 
-			// core components         
-			'../war/servoycore/*/*.js',
-			'../war/servoycore/*/*/*.js',
+            // core components         
+            '../war/servoycore/*/*.js',
+            '../war/servoycore/*/*/*.js',
 
-			// components
-			'../war/servoydefault/*/*.js',
-			'../war/servoydefault/*/*/*.js',
-			'../war/servoyservices/component_custom_property/*.js',
-			'../war/servoyservices/custom_json_array_property/*.js',
-			'../war/servoyservices/custom_json_object_property/*.js',
-			'../war/servoyservices/foundset_linked_property/*.js',
-			'../war/servoyservices/foundset_custom_property/*.js',
-			'../war/servoyservices/foundset_viewport_module/*.js',
+            // components
+            '../war/servoydefault/*/*.js',
+            '../war/servoydefault/*/*/*.js',
+            '../war/servoyservices/component_custom_property/*.js',
+            '../war/servoyservices/custom_json_array_property/*.js',
+            '../war/servoyservices/custom_json_object_property/*.js',
+            '../war/servoyservices/foundset_linked_property/*.js',
+            '../war/servoyservices/foundset_custom_property/*.js',
+            '../war/servoyservices/foundset_viewport_module/*.js',
 
-			// templates
-			'../war/servoycore/**/*.html',
-			'../war/servoydefault/**/*.html',
+            // templates
+            '../war/servoycore/**/*.html',
+            '../war/servoydefault/**/*.html',
 
-			// tests
-			'test/**/*.js'
-		],
-		exclude: [
-			'../war/servoycore/**/*_server.js',
-			'../war/servoydefault/**/*_server.js',
-			'../war/js/**/*.min.js',
-			'../war/js/debug.js'
-		],
-		ngHtml2JsPreprocessor: {
-			// setting this option will create only a single module that contains templates
-			// from all the files, so you can load them all with module('foo')
-			moduleName: 'servoy-components',
+            // tests
+            'test/**/*.js'
+        ],
+        exclude: [
+            '../war/servoycore/**/*_server.js',
+            '../war/servoydefault/**/*_server.js',
+            '../war/js/**/*.min.js',
+            '../war/js/debug.js'
+        ],
+        ngHtml2JsPreprocessor: {
+            // setting this option will create only a single module that contains templates
+            // from all the files, so you can load them all with module('foo')
+            moduleName: 'servoy-components',
 
-			cacheIdFromPath: function(filepath) {
-				return filepath.replace( /.*?\/servoydefault\/(.*)/, "servoydefault/$1" ).replace( /.*?\/servoycore\/(.*)/, "servoycore/$1" );
-			},
-		},
+            cacheIdFromPath: function(filepath) {
+                return filepath.replace(/.*?\/servoydefault\/(.*)/, "servoydefault/$1").replace(/.*?\/servoycore\/(.*)/, "servoycore/$1");
+            },
+        },
 
-		frameworks: ['jasmine'],
-		client: {
-			jasmine: { random: false }
-		},
-		browsers: ['PhantomJS'],//
+        frameworks: ['jasmine'],
+        client: {
+            jasmine: { random: false }
+        },
+        browsers: ['ChromeHeadless'],//
 
-		/*plugins : [    <- not needed since karma loads by default all sibling plugins that start with karma-*
-				'karma-junit-reporter',
-				'karma-chrome-launcher',
-				'karma-firefox-launcher',
-				'karma-script-launcher',
-				'karma-jasmine'
-				],*/
-		singleRun: true,
-		//autoWatch : true,
-		reporters: ['dots', 'junit'],
-		junitReporter: {
-			outputFile: '../../target/TEST-phantomjs-karma.xml'
-		}
-		/*,  alternative format
-		  junitReporter : {
-			outputFile: 'test_out/unit.xml',
-			suite: 'unit'
-		  }*/
-	});
+        plugins: [
+            require('karma-jasmine'),
+            require('karma-chrome-launcher'),
+            require('@chiragrupani/karma-chromium-edge-launcher'),
+            require('karma-coverage'),
+            require('karma-junit-reporter'),
+            require('karma-ng-html2js-preprocessor')
+        ],
+        singleRun: true,
+        //autoWatch : true,
+        reporters: ['dots', 'junit'],
+        junitReporter: {
+            outputFile: '../../target/TEST-phantomjs-karma.xml'
+        }
+        /*,  alternative format
+          junitReporter : {
+            outputFile: 'test_out/unit.xml',
+            suite: 'unit'
+          }*/
+    });
 };
