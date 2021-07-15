1b3f01367a7eec39a2fe068e50594e968a2dcd7b
Johan Compagner <jcompagner@servoy.com>
2021-07-15 12:10:06 +0200

    SVY-16249 Make PerformanceData fully concurrent
    Disabling the performance data will keep the instance but that instance
    will not report anything anymore because it now always ask the
    PerformanceRegistry for the max entries per context (instead of having
    that final integer itself)

--- servoy_ngclient.tests/src/test/java/com/servoy/j2db/server/ngclient/property/TestNGClient.java
+++ servoy_ngclient.tests/src/test/java/com/servoy/j2db/server/ngclient/property/TestNGClient.java
@@ -531,7 +531,7 @@ public class TestNGClient extends NGClient
 							@Override
 							public PerformanceData getPerformanceData(String context)
 							{
-								return new PerformanceData(PerformanceAggregator.DEFAULT_MAX_ENTRIES_TO_KEEP_IN_PRODUCTION, null, "",
+								return new PerformanceData(this, null, "",
 									new PerformanceAggregator(PerformanceAggregator.DEFAULT_MAX_ENTRIES_TO_KEEP_IN_PRODUCTION));
 							}
 
