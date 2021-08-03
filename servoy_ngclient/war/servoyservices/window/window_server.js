d25e945acfbb9aebe262dbe0500b5bc9422bb7df
Diana Bunaciu <dtimut@servoy.com>
2021-08-03 16:38:25 +0300

    SVY-16203 Method arguments of popup menu items cannot be sent to client in some cases set methodarguments every time a menuitem is created to ensure the callback method can be called

--- servoy_ngclient/war/servoyservices/window/window_server.js
+++ servoy_ngclient/war/servoyservices/window/window_server.js
@@ -132,6 +132,7 @@ var Menu = {
 		newItem.mnemonic = mnemonic;
 		newItem.align = align;
 		newItem.enabled = enabled;
+		newItem.methodArguments = null;
 		return this.items[this.items.push(newItem) - 1]; // we set and get it back to return as that instruments the value and makes it change-aware (be able to send granular updates to browser);
 //		return newItem;
 	},
