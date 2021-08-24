024fb169e0feead00dab2fea2c18338e1f33b4fb
lvostinar <lvostinar@servoy.com>
2021-08-24 14:51:03 +0300

    SVY-16330 plugins.ngclientutils.replaceHeaderTag should not reload all headers

--- servoy_ngclient/war/servoy_ng_only_services/ngutils/ngutils.js
+++ servoy_ngclient/war/servoy_ng_only_services/ngutils/ngutils.js
@@ -296,10 +296,58 @@ angular.module('ngclientutils', [ 'servoy' ])
 			// bind-once watch to wait for the server value to come (become defined) in case of a browser page refresh
 			scope.$watch("model.contributedTags", function(newVal, oldVal) {
 				if (newVal) {
-					var tags = scope.model.contributedTags;
-
-					// remove old ones if any
-					angular.element("head > [hhsManagedTag]").remove();
+					var tags = scope.model.contributedTags.slice();
+                    var uitags = angular.element("head > [hhsManagedTag]");
+					
+                    // remove old ones if any
+                    if (oldVal){
+                        for (var i = 0; i < oldVal.length; i++) {
+                            var oldTag = oldVal[i];
+                            var newIndex = -1;
+                            for (var j = 0; j < tags.length; j++) {
+                                var tag = tags[j];
+                                if (oldTag.tagName == tag.tagName && oldTag.attrs.length == tag.attrs.length){
+                                     var sameAttributes = true;
+                                     for (var k = 0; k < tag.attrs.length; k++) {
+                                         if (oldTag.attrs[k].name != tag.attrs[k].name || oldTag.attrs[k].value != tag.attrs[k].value){
+                                            sameAttributes = false;
+                                            break;
+                                        }
+                                     }
+                                     if (sameAttributes){
+                                        newIndex = j;
+                                        break;
+                                    }
+                                }
+                            }
+                            if (newIndex >= 0) {
+                                // element was found unchanged, just leave it alone
+                                tags.splice(newIndex, 1);
+                            }
+                            else{
+                                // element was deleted or changed, we must remove it from dom
+                                 for (var j = 0; j < uitags.length; j++) {
+                                    var uitag = uitags[j];
+                                    if (uitag.tagName.toLowerCase() == oldTag.tagName.toLowerCase()){
+                                         var sameAttributes = true;
+                                         for (var k = 0; k < oldTag.attrs.length; k++) {
+                                            if ( uitag.getAttribute(oldTag.attrs[k].name) != oldTag.attrs[k].value){
+                                                sameAttributes = false;
+                                                break;
+                                            }
+                                        }
+                                        if (sameAttributes){
+                                            uitag.remove();
+                                            break;
+                                        }
+                                    }
+                                 }
+                            }
+                        }
+                    }
+                    else{
+                        uitags.remove();
+                    }
 
 					// create new ones
 					if (tags) {
