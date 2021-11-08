2174b7685049e68fa7f3d54df09f682efdcf5b79
Johan Compagner <jcompagner@servoy.com>
2021-11-08 12:20:31 +0100

    SVY-15705 inMemory tables events for API backend.
    added the templatename for generating the methodtemplates.xml correctly

--- servoy_shared/src/com/servoy/j2db/persistence/TableNode.java
+++ servoy_shared/src/com/servoy/j2db/persistence/TableNode.java
@@ -271,6 +271,7 @@ public class TableNode extends AbstractBase implements ISupportChilds
 	 * @templatedescription
 	 * Return the next chunk of data for an inmemory datasource, when there is no more data, return nothing or an empty dataset.
 	 * @templatetype JSDataSet
+	 * @templatename onFoundsetNextChunk
 	 * @templateparam inmemDataSourceName name of the inmemory datasource
 	 * @templateparam sizeHint preferred number of records to be retrieved
 	 * @templateaddtodo
