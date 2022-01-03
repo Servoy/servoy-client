7974a939cb017701f07dc505cc6389f4c743985a
Johan Compagner <jcompagner@servoy.com>
2022-01-03 13:54:04 +0100

    SVY-16737 build and test Servoy 2021.12 final

--- servoy_shared/src/com/servoy/j2db/ClientVersion.java
+++ servoy_shared/src/com/servoy/j2db/ClientVersion.java
@@ -29,8 +29,8 @@ public abstract class ClientVersion
 	private static final int majorVersion = 2021;
 	private static final int middleVersion = 12;
 	private static final int minorVersion = 0;
-	private static final int releaseNumber = 3721;
-	private static final String versionPostfix = "rc2";
+	private static final int releaseNumber = 3722;
+	private static final String versionPostfix = "";
 	private static final boolean lts = false;
 
 	// make sure you keep this the same format, or make it work with extensions version comparing & xml schema
