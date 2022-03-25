d593ab4edb1055096edecf07f79840eed195fda2
Andrei Costescu <costescuandrei@gmail.com>
2022-03-25 17:11:34 +0100

    For hackathon epsilon 2022 phabricator T1.

--- servoy_ngclient/src/com/servoy/j2db/server/ngclient/NGClientWebsocketSession.java
+++ servoy_ngclient/src/com/servoy/j2db/server/ngclient/NGClientWebsocketSession.java
@@ -52,6 +52,7 @@ import org.sablo.websocket.WebsocketSessionManager;
 
 import com.servoy.j2db.FlattenedSolution;
 import com.servoy.j2db.IApplication;
+import com.servoy.j2db.IDesignerCallback;
 import com.servoy.j2db.J2DBGlobals;
 import com.servoy.j2db.Messages;
 import com.servoy.j2db.persistence.Form;
@@ -82,6 +83,8 @@ public class NGClientWebsocketSession extends BaseWebsocketSession implements IN
 	public static final String CLIENT_FUNCTION_SERVICE = "clientFunctionService"; //$NON-NLS-1$
 
 	private int clientType = 1;
+
+	IDesignerCallback designerCallback;
 
 	private static final class WindowServiceSpecification extends WebObjectSpecification
 	{
@@ -114,7 +117,7 @@ public class NGClientWebsocketSession extends BaseWebsocketSession implements IN
 
 	private NGClient client;
 
-	public NGClientWebsocketSession(WebsocketSessionKey sessionKey)
+	public NGClientWebsocketSession(WebsocketSessionKey sessionKey, IDesignerCallback designerCallback)
 	{
 		super(sessionKey);
 		registerClientService(new ServoyClientService(NGRuntimeWindowManager.WINDOW_SERVICE, WINDOWS_SERVICE_SPEC, this, false));
@@ -126,7 +129,7 @@ public class NGClientWebsocketSession extends BaseWebsocketSession implements IN
 	{
 		if (client == null)
 		{
-			setClient(new NGClient(this));
+			setClient(new NGClient(this, designerCallback));
 		}
 	}
 
--- servoy_ngclient/src/com/servoy/j2db/server/ngclient/WebsocketSessionFactory.java
+++ servoy_ngclient/src/com/servoy/j2db/server/ngclient/WebsocketSessionFactory.java
@@ -38,6 +38,6 @@ public class WebsocketSessionFactory implements IWebsocketSessionFactory
 	@Override
 	public IWebsocketSession createSession(WebsocketSessionKey sessionKey) throws Exception
 	{
-		return new NGClientWebsocketSession(sessionKey);
+		return new NGClientWebsocketSession(sessionKey, null);
 	}
 }
