d9efb29f41e415c84719781bbfe811bc64f54e54
Johan Compagner <jcompagner@servoy.com>
2022-05-24 13:01:00 +0200

    2021.3.3.3645_LTS

--- servoy_shared/src/com/servoy/j2db/ClientVersion.java
+++ servoy_shared/src/com/servoy/j2db/ClientVersion.java
@@ -28,8 +28,8 @@ public abstract class ClientVersion
 	// these fields are private intentionally, to prevent final class member copy in other classes!
 	private static final int majorVersion = 2021;
 	private static final int middleVersion = 03;
-	private static final int minorVersion = 2;
-	private static final int releaseNumber = 3644;
+	private static final int minorVersion = 3;
+	private static final int releaseNumber = 3645;
 	private static final String versionPostfix = "";
 	private static final boolean lts = true;
 
