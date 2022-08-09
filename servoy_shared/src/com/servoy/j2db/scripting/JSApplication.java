9c2783f8d883cae61cb5ffbd2d7d5400fb4226a7
Johan Compagner <jcompagner@servoy.com>
2022-08-09 11:13:43 +0200

    SVY-17258 Upgrade Rhino to the latest release
    rhino now has its own version, use this as a maven property
    some small api updates for the latest rhino

--- servoy_shared/src/com/servoy/j2db/scripting/JSApplication.java
+++ servoy_shared/src/com/servoy/j2db/scripting/JSApplication.java
@@ -2181,7 +2181,7 @@ public class JSApplication implements IReturnedTypesProvider, IJSApplication
 		}
 		else if (msg instanceof NativeError)
 		{
-			application.output(msg.toString() + '\n' + ((NativeError)msg).getStackDelegated((NativeError)msg), level);
+			application.output(msg.toString() + '\n' + ((NativeError)msg).getStackDelegated(), level);
 		}
 		else if (msg instanceof Scriptable)
 		{
