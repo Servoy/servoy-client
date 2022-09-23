2389806f9d2606ad62e4bf9be96b72ee394e8cb4
Rob Gansevles <rgansevles@gmail.com>
2022-09-23 15:49:41 +0200

    SVY-17478 WAR Deployment for updating Application Server doesn't work

--- servoy_shared/src/com/servoy/j2db/server/shared/IApplicationServerSingleton.java
+++ servoy_shared/src/com/servoy/j2db/server/shared/IApplicationServerSingleton.java
@@ -155,4 +155,6 @@ public interface IApplicationServerSingleton
 	Pair<Boolean, String> upgradeLicense(String companyName, String licensecode, String licenses);
 
 	String getServerUUID();
+
+	void startServices() throws Exception;
 }
