feae80a3e9edc26eed6d584868941a3b78d49dc6
Johan Compagner <jcompagner@servoy.com>
2023-06-15 18:14:13 +0200

    2023.6.0.3881_RC2 build

--- servoy_shared/src/com/servoy/j2db/ClientVersion.java
+++ servoy_shared/src/com/servoy/j2db/ClientVersion.java
@@ -34,8 +34,8 @@ public abstract class ClientVersion
 	private static final int majorVersion = 2023;
 	private static final int middleVersion = 6;
 	private static final int minorVersion = 0;
-	private static final int releaseNumber = 3880;
-	private static final String versionPostfix = "rc";
+	private static final int releaseNumber = 3881;
+	private static final String versionPostfix = "rc2";
 	private static final boolean lts = false;
 	private static String buildTime = null;
 
