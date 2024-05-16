99cef126cd65ce95fcd65925bcc16e1db112463b
Andrei Costescu <costescuandrei@gmail.com>
2024-05-16 13:14:54 +0300

    For SVY-19262: fullcalendar2 customButtons click doesn't work

--- servoy_ngclient/src/com/servoy/j2db/server/ngclient/property/types/JSNativeJavaObjectType.java
+++ servoy_ngclient/src/com/servoy/j2db/server/ngclient/property/types/JSNativeJavaObjectType.java
@@ -18,20 +18,24 @@
 package com.servoy.j2db.server.ngclient.property.types;
 
 import org.json.JSONException;
+import org.json.JSONString;
 import org.json.JSONWriter;
 import org.mozilla.javascript.NativeJavaObject;
 import org.sablo.specification.PropertyDescription;
 import org.sablo.specification.property.IBrowserConverterContext;
 import org.sablo.specification.property.IClassPropertyType;
+import org.sablo.specification.property.IPropertyConverterForBrowserWithDynamicClientType;
 import org.sablo.specification.property.types.DefaultPropertyType;
 import org.sablo.util.ValueReference;
 import org.sablo.websocket.utils.JSONUtils.FullValueToJSONConverter;
+import org.sablo.websocket.utils.JSONUtils.IJSONStringWithClientSideType;
 
 /**
  * @author gboros
  *
  */
-public class JSNativeJavaObjectType extends DefaultPropertyType<NativeJavaObject> implements IClassPropertyType<NativeJavaObject>
+public class JSNativeJavaObjectType extends DefaultPropertyType<NativeJavaObject>
+	implements IClassPropertyType<NativeJavaObject>, IPropertyConverterForBrowserWithDynamicClientType<NativeJavaObject>
 {
 
 	public static final JSNativeJavaObjectType INSTANCE = new JSNativeJavaObjectType();
@@ -48,6 +52,23 @@ public class JSNativeJavaObjectType extends DefaultPropertyType<NativeJavaObject
 		IBrowserConverterContext dataConverterContext, ValueReference<Boolean> returnValueAdjustedIncommingValue)
 	{
 		// not supported
+		return null;
+	}
+
+	@Override
+	public JSONString toJSONWithDynamicClientSideType(JSONWriter writer, NativeJavaObject sabloValue, PropertyDescription propertyDescription,
+		IBrowserConverterContext dataConverterContext) throws JSONException
+	{
+		if (sabloValue != null) // should always be != null as long as this class is used only as a class type found based on sabloValue.getClass()
+		{
+			IJSONStringWithClientSideType valueAndClientSideType = FullValueToJSONConverter.INSTANCE.getConvertedValueWithClientType(sabloValue.unwrap(),
+				propertyDescription, dataConverterContext, false);
+			if (valueAndClientSideType != null)
+			{
+				writer.value(valueAndClientSideType);
+				return valueAndClientSideType.getClientSideType();
+			} // else wrapped value doesn't want to write anything to the client
+		}
 		return null;
 	}
 
