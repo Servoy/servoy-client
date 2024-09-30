048f2c713b8b070d4e7ebbec953183e35709f0e4
Johan Compagner <jcompagner@servoy.com>
2024-09-30 15:19:28 +0200

    SVY-19496 Screen with 3000+ fields on it (Custom advanced search) will never load in TiNG
    in scripting a null value of visible should always return true (true is
    default)

---
+++ servoy_ngclient/src/com/servoy/j2db/server/ngclient/property/types/NGVisiblePropertyType.java
@@ -0,0 +1,47 @@
+/*
+ This file belongs to the Servoy development and deployment environment, Copyright (C) 1997-2024 Servoy BV
+
+ This program is free software; you can redistribute it and/or modify it under
+ the terms of the GNU Affero General Public License as published by the Free
+ Software Foundation; either version 3 of the License, or (at your option) any
+ later version.
+
+ This program is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+
+ You should have received a copy of the GNU Affero General Public License along
+ with this program; if not, see http://www.gnu.org/licenses or write to the Free
+ Software Foundation,Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301
+*/
+
+package com.servoy.j2db.server.ngclient.property.types;
+
+import org.mozilla.javascript.Scriptable;
+import org.sablo.IWebObjectContext;
+import org.sablo.specification.PropertyDescription;
+import org.sablo.specification.property.types.VisiblePropertyType;
+
+import com.servoy.j2db.server.ngclient.property.types.NGConversions.ISabloComponentToRhino;
+
+/**
+ * @author jcompagner
+ *
+ */
+public class NGVisiblePropertyType extends VisiblePropertyType implements ISabloComponentToRhino<Boolean>
+{
+	public static final NGVisiblePropertyType NG_INSTANCE = new NGVisiblePropertyType();
+
+	@Override
+	public boolean isValueAvailableInRhino(Boolean webComponentValue, PropertyDescription pd, IWebObjectContext webObjectContext)
+	{
+		return true;
+	}
+
+	@Override
+	public Object toRhinoValue(Boolean webComponentValue, PropertyDescription pd, IWebObjectContext webObjectContext, Scriptable startScriptable)
+	{
+		return webComponentValue == null ? Boolean.TRUE : webComponentValue;
+	}
+
+}
