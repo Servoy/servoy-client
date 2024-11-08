5f0fbb13e3eed07bcde486ce37533d2d18418357
Johan Compagner <jcompagner@servoy.com>
2024-11-08 12:12:00 +0100

    version bumb to 2023.3.9.3851_LTS

--- servoy_shared/src/com/servoy/j2db/ClientVersion.java
+++ servoy_shared/src/com/servoy/j2db/ClientVersion.java
@@ -28,8 +28,8 @@ public abstract class ClientVersion
 	// these fields are private intentionally, to prevent final class member copy in other classes!
 	private static final int majorVersion = 2023;
 	private static final int middleVersion = 3;
-	private static final int minorVersion = 8;
-	private static final int releaseNumber = 3850;
+	private static final int minorVersion = 9;
+	private static final int releaseNumber = 3851;
 	private static final String versionPostfix = "";
 	private static final boolean lts = true;
 
