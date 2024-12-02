4b3d9c37b0acbe258f41c5d8a72c57545acff7f0
Johan Compagner <jcompagner@servoy.com>
2024-12-02 12:29:09 +0100

    SVY-19678 Add Event Logger to the Core
    test if string is lager the 30 before doing substrings

--- servoy_ngclient/src/com/servoy/j2db/server/ngclient/component/EventExecutor.java
+++ servoy_ngclient/src/com/servoy/j2db/server/ngclient/component/EventExecutor.java
@@ -237,7 +237,7 @@ public class EventExecutor
 			String argsAsString = Arrays.asList(newargs).stream()
 				.map(value -> value instanceof JSEvent ? "JSEvent" //$NON-NLS-1$
 					: value instanceof Record ? "JSRecord" //$NON-NLS-1$
-						: value instanceof String str ? str.substring(0, 30)
+						: value instanceof String str && str.length() > 30 ? str.substring(0, 30)
 							: value instanceof Scriptable s ? Utils.getScriptableString(s) : String.valueOf(value))
 				.collect(Collectors.joining(",")); //$NON-NLS-1$
 			EVENT_TRACING_LOG.info(application.getUserUID() + '|' + application.getClientID() + '|' + Arrays.toString(tenantValue) + '|' +
