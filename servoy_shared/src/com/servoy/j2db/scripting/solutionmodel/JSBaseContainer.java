cdcb5f169c138654d3c674e04899511a6dbbcf70
emera <emera@servoy.com>
2025-11-03 16:11:58 +0200

    SVY-20548 Add support for JSComponent.<componentName> and componentProperties / handlers - update docs for JSComponent.package.component

--- servoy_shared/src/com/servoy/j2db/scripting/solutionmodel/JSBaseContainer.java
+++ servoy_shared/src/com/servoy/j2db/scripting/solutionmodel/JSBaseContainer.java
@@ -508,7 +508,7 @@ public abstract class JSBaseContainer<T extends AbstractContainer> implements IJ
 	 * var container = myForm.getLayoutContainer("row1")
 	 * var bean = container.newWebComponent('bean','mypackage-testcomponent');
 	 *
-	 * var webcomponent = form.newWebComponent('mywebcomponent',JSComponent.mypackage_testcomponent);
+	 * var webcomponent = form.newWebComponent('mywebcomponent',JSComponent.mypackage.testcomponent);
 	 *
 	 * @param name the specified name of the JSWebComponent object
 	 * @param type the webcomponent name as it appears in the spec or the JSComponent constant representing the webcomponent type
--- servoy_shared/src/com/servoy/j2db/scripting/solutionmodel/JSForm.java
+++ servoy_shared/src/com/servoy/j2db/scripting/solutionmodel/JSForm.java
@@ -4071,7 +4071,7 @@ public class JSForm extends JSBaseContainer<Form> implements IJSScriptParent<For
 	 * form.useCssPosition = true
 	 * var webcomponent = form.newWebComponent('mywebcomponent','mypackage-testcomponent');
 	 *
-	 * var web_component = form.newWebComponent('mywebcomponent',JSComponent.mypackage_testcomponent);
+	 * var web_component = form.newWebComponent('mywebcomponent',JSComponent.mypackage.testcomponent);
 	 *
 	 * @param name the specified name of the JSWebComponent object
 	 * @param type the webcomponent name as it appears in the spec or the JSComponent constant representing the webcomponent type
