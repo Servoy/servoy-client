2049086eae1e84eeb0c8ea5528612a2cc16061ec
Johan Compagner <jcompagner@servoy.com>
2026-07-03 15:46:31 -0500

    versionbumb tot 4182

--- servoy_shared/src/com/servoy/j2db/ClientVersion.java
+++ servoy_shared/src/com/servoy/j2db/ClientVersion.java
@@ -34,8 +34,8 @@ public abstract class ClientVersion
 	private static final int majorVersion = 2026;
 	private static final int middleVersion = 6;
 	private static final int minorVersion = 0;
-	private static final int releaseNumber = 4181;
-	private static final String versionPostfix = "rc2";
+	private static final int releaseNumber = 4182;
+	private static final String versionPostfix = "";
 	private static final boolean lts = false;
 	private static String buildTime = null;
 
